984cc7a52a6cd2818e7803078f9e52597152c10c
Jose Luis Blanco <joseluisblancoc@gmail.com>
2010-12-19 16:42:08 +0000

    updated version numbers to start of 0.9.4

--- doc/doxygen-pages/changeLog_doc.h
+++ doc/doxygen-pages/changeLog_doc.h
@@ -31,6 +31,13 @@
 
 <p> <b>Note:</b> <i>If you are displaying a local version of this page and you have not built the whole HTML documentation, the links above will be broken. Either build the documentation invoking <code>make documentation_html</code> or browse it on-line <a href="http://www.mrpt.org/" target="_blank">here</a>.</i></p>
 
+  <a name="0.9.4">
+  <h2>Version 0.9.4 - (Version 1.0.0-Release_Candidate_2): UNDER DEVELOPMENT </h2></a>
+        - ...
+
+
+
+<br> <hr>
   <a name="0.9.3">
   <h2>Version 0.9.3 - (Version 1.0.0-Release_Candidate_1): Released 18-DEC-2010 (SVN 2338) </h2></a>
 	- <b>Most important changes:</b>
