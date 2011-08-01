589e87df8fce59d0f5094346b26b5e5945fd5099
Jose Luis Blanco <joseluisblancoc@gmail.com>
2011-08-01 15:55:23 +0000

    removed the TColor anonymous union since it was not ISO C++ compliant

--- libs/base/include/mrpt/utils/TColor.h
+++ libs/base/include/mrpt/utils/TColor.h
@@ -37,19 +37,14 @@ namespace mrpt
 	namespace utils
 	{
 
-#pragma pack(push,1)  // Required for the union fields below
-
-		/** A RGB color - 8bit (R,B,G,A are a C++ union with RGBA) */
+		/** A RGB color - 8bit */
 		struct BASE_IMPEXP TColor
 		{
 			inline TColor() : R(0),G(0),B(0),A(255) { }
 			inline TColor(uint8_t r,uint8_t g,uint8_t b, uint8_t alpha=255) : R(r),G(g),B(b),A(alpha) { }
 			inline explicit TColor(const unsigned int color_RGB_24bit) : R(uint8_t(color_RGB_24bit>>16)),G(uint8_t(color_RGB_24bit>>8)),B(uint8_t(color_RGB_24bit)),A(255) { }
-			union 
-			{
-				struct { uint8_t  R,G,B,A; };
-				uint32_t RGBA;
-			};
+			uint8_t  R,G,B,A;
+
 			/** Operator for implicit conversion into an int binary representation 0xRRGGBB */
 			inline operator unsigned int(void) const { return (((unsigned int)R)<<16) | (((unsigned int)G)<<8) | B; }
 
@@ -60,7 +55,6 @@ namespace mrpt
 			static TColor black;//!< Predefined colors
 			static TColor gray;	//!< Predefined colors
 		};
-#pragma pack(pop)
 
 		/** A RGB color - floats in the range [0,1] */
 		struct BASE_IMPEXP TColorf
