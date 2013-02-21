07b3b2695ad6ae2487b49e9cf17e79d5e64cc5f5
Jose Luis Blanco <joseluisblancoc@gmail.com>
2013-02-21 17:34:15 +0000

    Added a new build flag to force not using PCL.

--- cmakemodules/script_pcl.cmake
+++ cmakemodules/script_pcl.cmake
@@ -1,26 +1,33 @@
-# It seems the PCL find_package changes some wxWidgets variables (wtf!), so make a backup:
-#set(BCK_wxWidgets_LIB_DIR ${wxWidgets_LIB_DIR})
-#set(BCK_wxWidgets_LIBRARIES ${wxWidgets_LIBRARIES})
 
-# PCL library:
-# --------------------------------------------
 SET(CMAKE_MRPT_HAS_PCL 0)
 SET(CMAKE_MRPT_HAS_PCL_SYSTEM 0)
 
-find_package(PCL COMPONENTS io common registration QUIET)
-if (PCL_FOUND)
-	SET(CMAKE_MRPT_HAS_PCL 1)
-	SET(CMAKE_MRPT_HAS_PCL_SYSTEM 1)
+# Leave at the user's choice to disable the SWR libs:
+OPTION(DISABLE_PCL "Forces NOT using PCL, even if it could be found by CMake" "OFF")
+MARK_AS_ADVANCED(DISABLE_PCL)
+IF(NOT DISABLE_PCL)
+	# It seems the PCL find_package changes some wxWidgets variables (wtf!), so make a backup:
+	#set(BCK_wxWidgets_LIB_DIR ${wxWidgets_LIB_DIR})
+	#set(BCK_wxWidgets_LIBRARIES ${wxWidgets_LIBRARIES})
 
-	INCLUDE_DIRECTORIES(${PCL_INCLUDE_DIRS})
-	link_directories(${PCL_LIBRARY_DIRS})
-	add_definitions(${PCL_DEFINITIONS})
+	# PCL library:
+	# --------------------------------------------
+	find_package(PCL COMPONENTS io common registration QUIET)
+	if (PCL_FOUND)
+		SET(CMAKE_MRPT_HAS_PCL 1)
+		SET(CMAKE_MRPT_HAS_PCL_SYSTEM 1)
 
-	# Add PCL directories as "-isystem" to avoid warnings:
-	ADD_DIRECTORIES_AS_ISYSTEM(PCL_INCLUDE_DIRS)
+		INCLUDE_DIRECTORIES(${PCL_INCLUDE_DIRS})
+		link_directories(${PCL_LIBRARY_DIRS})
+		add_definitions(${PCL_DEFINITIONS})
 
-endif(PCL_FOUND)
+		# Add PCL directories as "-isystem" to avoid warnings:
+		ADD_DIRECTORIES_AS_ISYSTEM(PCL_INCLUDE_DIRS)
 
-# Undo backups: (read above)
-#set(wxWidgets_LIB_DIR ${BCK_wxWidgets_LIB_DIR})
-#set(wxWidgets_LIBRARIES ${BCK_wxWidgets_LIBRARIES})
+	endif(PCL_FOUND)
+
+	# Undo backups: (read above)
+	#set(wxWidgets_LIB_DIR ${BCK_wxWidgets_LIB_DIR})
+	#set(wxWidgets_LIBRARIES ${BCK_wxWidgets_LIBRARIES})
+	
+ENDIF(NOT DISABLE_PCL)
