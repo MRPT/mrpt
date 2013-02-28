c642f533d305f5981b5804482e6e036cd045e8df
Jose Luis Blanco <joseluisblancoc@gmail.com>
2013-02-28 19:59:25 +0000

    - Progress with the srba-guide - Added a monoSLAM implementation to srba-slam

---
+++ apps/srba-slam/CDatasetParser_Monocular.h
@@ -0,0 +1,75 @@
+/* +---------------------------------------------------------------------------+
+   |                 The Mobile Robot Programming Toolkit (MRPT)               |
+   |                                                                           |
+   |                          http://www.mrpt.org/                             |
+   |                                                                           |
+   | Copyright (c) 2005-2013, Individual contributors, see AUTHORS file        |
+   | Copyright (c) 2005-2013, MAPIR group, University of Malaga                |
+   | Copyright (c) 2012-2013, University of Almeria                            |
+   | All rights reserved.                                                      |
+   |                                                                           |
+   | Redistribution and use in source and binary forms, with or without        |
+   | modification, are permitted provided that the following conditions are    |
+   | met:                                                                      |
+   |    * Redistributions of source code must retain the above copyright       |
+   |      notice, this list of conditions and the following disclaimer.        |
+   |    * Redistributions in binary form must reproduce the above copyright    |
+   |      notice, this list of conditions and the following disclaimer in the  |
+   |      documentation and/or other materials provided with the distribution. |
+   |    * Neither the name of the copyright holders nor the                    |
+   |      names of its contributors may be used to endorse or promote products |
+   |      derived from this software without specific prior written permission.|
+   |                                                                           |
+   | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       |
+   | 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED |
+   | TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR|
+   | PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE |
+   | FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL|
+   | DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR|
+   |  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)       |
+   | HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,       |
+   | STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  |
+   | ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           |
+   | POSSIBILITY OF SUCH DAMAGE.                                               |
+   +---------------------------------------------------------------------------+ */
+
+#pragma once
+
+#include "CDatasetParserBase.h"
+
+template <>
+struct CDatasetParserTempl<mrpt::srba::observations::MonocularCamera> : public CDatasetParserBase
+{
+	double m_noise_std_px;
+
+	CDatasetParserTempl(RBASLAM_Params &cfg) :
+		CDatasetParserBase(cfg),
+		m_noise_std_px(1e-4)
+	{
+		if (cfg.arg_noise.isSet())
+			m_noise_std_px=cfg.arg_noise.getValue();
+	}
+
+	virtual void checkObsProperSize() const
+	{
+		// Columns: KeyframeIndex  LandmarkID | px.x px.y
+		ASSERT_(m_OBS.getColCount()==(2+2))
+	}
+
+	void getObs(
+		size_t idx,
+		mrpt::srba::observation_traits<mrpt::srba::observations::MonocularCamera>::observation_t & o
+		) const
+	{
+		o.feat_id = m_OBS(idx,1);
+		o.obs_data.px.x  = m_OBS(idx,2) + (!m_add_noise ? .0 : mrpt::random::randomGenerator.drawGaussian1D(0, m_noise_std_px));
+		o.obs_data.px.y  = m_OBS(idx,3) + (!m_add_noise ? .0 : mrpt::random::randomGenerator.drawGaussian1D(0, m_noise_std_px));
+	}
+
+	void loadNoiseParamsInto( mrpt::srba::options::observation_noise_identity::parameters_t & p )
+	{
+		p.std_noise_observations = m_noise_std_px;
+	}
+
+
+};
---
+++ apps/srba-slam/instance_se3_lm3d_monocular.cpp
@@ -0,0 +1,96 @@
+/* +---------------------------------------------------------------------------+
+   |                 The Mobile Robot Programming Toolkit (MRPT)               |
+   |                                                                           |
+   |                          http://www.mrpt.org/                             |
+   |                                                                           |
+   | Copyright (c) 2005-2013, Individual contributors, see AUTHORS file        |
+   | Copyright (c) 2005-2013, MAPIR group, University of Malaga                |
+   | Copyright (c) 2012-2013, University of Almeria                            |
+   | All rights reserved.                                                      |
+   |                                                                           |
+   | Redistribution and use in source and binary forms, with or without        |
+   | modification, are permitted provided that the following conditions are    |
+   | met:                                                                      |
+   |    * Redistributions of source code must retain the above copyright       |
+   |      notice, this list of conditions and the following disclaimer.        |
+   |    * Redistributions in binary form must reproduce the above copyright    |
+   |      notice, this list of conditions and the following disclaimer in the  |
+   |      documentation and/or other materials provided with the distribution. |
+   |    * Neither the name of the copyright holders nor the                    |
+   |      names of its contributors may be used to endorse or promote products |
+   |      derived from this software without specific prior written permission.|
+   |                                                                           |
+   | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       |
+   | 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED |
+   | TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR|
+   | PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE |
+   | FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL|
+   | DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR|
+   |  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)       |
+   | HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,       |
+   | STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  |
+   | ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           |
+   | POSSIBILITY OF SUCH DAMAGE.                                               |
+   +---------------------------------------------------------------------------+ */
+
+/* See srba-slam_main.cpp for docs */
+
+#include "srba-run-generic-impl.h"
+
+#include "CDatasetParser_Monocular.h"
+
+template <>
+struct InitializerSensorParams<mrpt::srba::observations::MonocularCamera>
+{
+	template <class RBA>
+	static void init(RBA &rba, RBASLAM_Params &config)
+	{
+		// Load params from file:
+
+		if (!config.arg_sensor_params.isSet())
+			throw std::runtime_error("Error: --sensor-params-cfg-file is mandatory for this type of observations.");
+
+		const std::string sCfgFile = config.arg_sensor_params.getValue();
+		rba.parameters.sensor.camera_calib.loadFromConfigFile("CAMERA",mrpt::utils::CConfigFile(sCfgFile) );
+	}
+};
+
+// Specializations:
+template <>
+struct problem_options_traits_t<kf2kf_poses::SE3,landmarks::Euclidean3D,observations::MonocularCamera>
+{
+	// Camera sensors have a different coordinate system wrt the robot (rotated yaw=-90, pitch=0, roll=-90)
+	struct srba_options_t
+	{
+		typedef options::sensor_pose_on_robot_se3     sensor_pose_on_robot_t;
+		typedef options::observation_noise_identity   obs_noise_matrix_t;      // The sensor noise matrix is the same for all observations and equal to \sigma * I(identity)
+		typedef options::solver_LM_schur_dense_cholesky      solver_t;
+	};
+};
+
+// Explicit instantiation:
+template struct RBA_Run_Factory<kf2kf_poses::SE3,landmarks::Euclidean3D,observations::MonocularCamera>;
+
+
+// Register this RBA problem:
+RBA_Run_BasePtr my_creator_se3_lm3d_monocular(RBASLAM_Params &config)
+{
+	if (config.arg_se3.isSet() && config.arg_lm3d.isSet() && config.arg_obs.getValue()=="MonocularCamera")
+		return RBA_Run_Factory<kf2kf_poses::SE3,landmarks::Euclidean3D,observations::MonocularCamera>::create();
+
+	return RBA_Run_BasePtr();
+}
+
+struct TMyRegister
+{
+	TMyRegister()
+	{
+		RBA_implemented_registry & reg = RBA_implemented_registry::getInstance();
+		reg.doRegister( &my_creator_se3_lm3d_monocular, "--se3 --lm-3d --obs MonocularCamera" );
+	}
+};
+
+static TMyRegister my_initializer;
+
+
+
