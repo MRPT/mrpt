98a6e0d78eea64b9d5e70d55ad15830fd884b843
Jose Luis Blanco <joseluisblancoc@gmail.com>
2013-03-03 00:28:00 +0000

    mark start of version 1.0.1

--- doc/doxygen-pages/changeLog_doc.h
+++ doc/doxygen-pages/changeLog_doc.h
@@ -38,6 +38,12 @@
 
 <p> <b>Note:</b> <i>If you are displaying a local version of this page and you have not built the whole HTML documentation, the links above will be broken. Either build the documentation invoking <code>make documentation_html</code> or browse it on-line <a href="http://www.mrpt.org/" target="_blank">here</a>.</i></p>
 
+
+ <hr>
+ <a name="1.0.1">
+  <h2>Version 1.0.1 - (Under development) </h2></a>
+	- (no changes yet)
+
  <hr>
  <a name="1.0.0">
   <h2>Version 1.0.0: Released 1-MAR-2013 (SVN 3287)  </h2></a>
