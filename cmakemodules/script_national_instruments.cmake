43139814a5d45ba012baa5d9127bf665d6211580
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2013-10-29 00:31:49 +0100

    Better cmake detection of NI DAQmx libs/headers

--- cmakemodules/script_national_instruments.cmake
+++ cmakemodules/script_national_instruments.cmake
@@ -9,28 +9,33 @@ MARK_AS_ADVANCED(DISABLE_NationalInstruments)
 IF(NOT DISABLE_NationalInstruments)	
 	IF(MRPT_HAS_NI_DAQmxBASE)
 		# Look for DAQmxBase libs & headers:
-		SET(NI_DAQmxBASE_ROOT_DIR "" CACHE PATH "Path to NI root directory. Typ: WIN=C:\\Program Files\\National Instruments\\NI-DAQmx Base . LIN=/usr/local/")
-		SET(CMAKE_MRPT_HAS_NIDAQMXBASE 0) # The variable that actually ends up in config.h
+		find_library(NI_DAQmxBASE_LIB_FILE
+			NAMES nidaqmxbase libnidaqmxbase
+			PATHS 
+				"/usr/local/natinst/nidaqmxbase"
+				"$ENV{ProgramFiles}/National Instruments/NI-DAQmx Base/Lib"
+				"$ENV{ProgramFiles(x86)}/National Instruments/NI-DAQmx Base/Lib"
+				"$ENV{ProgramW6432}/National Instruments/NI-DAQmx Base/Lib"
+			DOC "Full path of library file 'libnidaqmxbase' or 'nidaqmxbase.lib'"
+		)
 		
-		# Checks:
-		SET(NI_DAQmxBASE_LIB CACHE FILE "" "Library file nidaqmxbase.{lib|so}")
-		IF (EXISTS "${NI_DAQmxBASE_ROOT_DIR}/lib/nidaqmxbase.lib")
-			SET(NI_DAQmxBASE_LIB "${NI_DAQmxBASE_ROOT_DIR}/lib/nidaqmxbase.lib")
-		ENDIF (EXISTS "${NI_DAQmxBASE_ROOT_DIR}/lib/nidaqmxbase.lib")
-
-		SET(NI_DAQmxBASE_INCLUDE_DIR CACHE PATH "" "Path to NIDAQmxBase.h")
-		IF (EXISTS "${NI_DAQmxBASE_ROOT_DIR}/include/NIDAQmxBase.h")
-			SET(NI_DAQmxBASE_INCLUDE_DIR "${NI_DAQmxBASE_ROOT_DIR}/include/")
-		ENDIF (EXISTS "${NI_DAQmxBASE_ROOT_DIR}/include/NIDAQmxBase.h")
-						
+		find_path(NI_DAQmxBASE_INCLUDE_DIR
+			NAMES NIDAQmxBase.h nidaqmxbase.h
+			PATHS 
+				"/usr/local/natinst/nidaqmxbase"
+				"$ENV{ProgramFiles}/National Instruments/NI-DAQmx Base/Include"
+				"$ENV{ProgramFiles(x86)}/National Instruments/NI-DAQmx Base/Include"
+				"$ENV{ProgramW6432}/National Instruments/NI-DAQmx Base/Include"
+			DOC "Path to [PATH]/NIDAQmxBase.h"
+		)
+		
 		# The variable that actually ends up in config.h
-		IF (EXISTS "${NI_DAQmxBASE_LIB}" AND EXISTS "${NI_DAQmxBASE_INCLUDE_DIR}/NIDAQmxBase.h")
+		IF (NI_DAQmxBASE_INCLUDE_DIR AND NI_DAQmxBASE_LIB_FILE)
 			SET(CMAKE_MRPT_HAS_NIDAQMXBASE 1) 
-		ENDIF (EXISTS "${NI_DAQmxBASE_LIB}" AND EXISTS "${NI_DAQmxBASE_INCLUDE_DIR}/NIDAQmxBase.h")
+		ELSE(NI_DAQmxBASE_INCLUDE_DIR AND NI_DAQmxBASE_LIB_FILE)
+			SET(CMAKE_MRPT_HAS_NIDAQMXBASE 0)
+			MESSAGE("Error: Correct NI_DAQmxBASE_LIB_FILE and NI_DAQmxBASE_INCLUDE_DIR, or uncheck MRPT_HAS_NI_DAQmxBASE")
+		ENDIF (NI_DAQmxBASE_INCLUDE_DIR AND NI_DAQmxBASE_LIB_FILE)
 			
-		IF(NOT CMAKE_MRPT_HAS_NIDAQMXBASE)
-			MESSAGE("Error: Set NI_DAQmxBASE_ROOT_DIR correctly (to contain 'NI_DAQmxBASE_ROOT_DIR/include/NIDAQmxBase.h'), or uncheck MRPT_HAS_NI_DAQmxBASE")
-		ENDIF(NOT CMAKE_MRPT_HAS_NIDAQMXBASE)
-		
 	ENDIF(MRPT_HAS_NI_DAQmxBASE)
 ENDIF(NOT DISABLE_NationalInstruments)	
