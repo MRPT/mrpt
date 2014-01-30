9dc99a2a2639ed06b7faacdfb35e0c75c0748019
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2014-01-30 17:23:51 +0100

    suitesparse sync

--- cmakemodules/FindSuiteSparse.cmake
+++ cmakemodules/FindSuiteSparse.cmake
@@ -68,9 +68,12 @@ if(SuiteSparse_DIR)
 endif()
 
 ## set default verbosity
-if(NOT SuiteSparse_VERBOSE)
+## Process the CMake automatically-generated var: SuiteSparse_FIND_QUIETLY: supersedes *_VERBOSE.
+if(NOT SuiteSparse_VERBOSE OR SuiteSparse_FIND_QUIETLY)
 	set(SuiteSparse_VERBOSE OFF)
-else()
+endif()
+
+if(SuiteSparse_VERBOSE)
 	message(STATUS "Start to FindSuiteSparse.cmake :")
 endif()
 
@@ -256,6 +259,7 @@ macro(SuiteSparse_FIND_COMPONENTS )
 	## set the final SuiteSparse_FOUND based on all previous components found (status)
 	foreach(componentToCheck ${SuiteSparse_FOUND_LIST})
 		set(SuiteSparse_FOUND ON)
+		MESSAGE(STATUS "final check: ${componentToCheck}")
 		if(NOT ${componentToCheck})
 			set(SuiteSparse_FOUND OFF)
 			break() ## one component not found is enought to failed
@@ -299,8 +303,11 @@ if(SuiteSparse_USE_LAPACK_BLAS)
 		PATH_SUFFIXES	Release Debug
 	)
 	if(NOT SuiteSparse_BLAS_LIBRARY)
-		message(SEND_ERROR "   Failed to find SuiteSparse_BLAS_LIBRARY.Set it manually or set the SuiteSparse_BLAS_DIR to looking for it inside.")
-		set(SuiteSparse_BLAS_DIR "$ENV{SuiteSparse_BLAS_DIR}" CACHE PATH "blas root directory")
+		if (SuiteSparse_VERBOSE)
+			# Send all msgs as "STATUS": We'll send an error at the bottom, only if "REQUIRED" is set.
+			message(STATUS "   Failed to find SuiteSparse_BLAS_LIBRARY.Set it manually or set the SuiteSparse_BLAS_DIR to looking for it inside.")
+		endif()
+			set(SuiteSparse_BLAS_DIR "$ENV{SuiteSparse_BLAS_DIR}" CACHE PATH "blas root directory")
 	else()
 		if(DEFINED SuiteSparse_BLAS_DIR)
 			mark_as_advanced(SuiteSparse_BLAS_DIR)
@@ -320,7 +327,10 @@ if(SuiteSparse_USE_LAPACK_BLAS)
 		PATH_SUFFIXES	Release Debug
 	)
 	if(NOT SuiteSparse_LAPACK_LIBRARY)
-		message(SEND_ERROR "   Failed to find SuiteSparse_LAPACK_LIBRARY.Set it manually or set the SuiteSparse_LAPACK_DIR to looking for it inside.")
+		if (SuiteSparse_VERBOSE)
+			# Send all msgs as "STATUS": We'll send an error at the bottom, only if "REQUIRED" is set.
+			message(STATUS "   Failed to find SuiteSparse_LAPACK_LIBRARY.Set it manually or set the SuiteSparse_LAPACK_DIR to looking for it inside.")
+		endif()
 		set(SuiteSparse_LAPACK_DIR "$ENV{SuiteSparse_LAPACK_DIR}" CACHE PATH "lapack root directory")
 	else()
 		if(DEFINED SuiteSparse_LAPACK_DIR)
@@ -411,3 +421,16 @@ endif()
 if(SuiteSparse_VERBOSE)
 	message(STATUS "Finish to FindSuiteSparse.cmake => SuiteSparse_FOUND=${SuiteSparse_FOUND}")
 endif()
+
+## Show error if not found and _REQUIRED
+IF(NOT SuiteSparse_FOUND)
+  # make FIND_PACKAGE friendly
+  IF(NOT SuiteSparse_FIND_QUIETLY)
+    IF(SuiteSparse_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR
+        "SuiteSparse required but some headers or libs not found.")
+    ELSE()
+      MESSAGE(STATUS "ERROR: SuiteSparse was not found.")
+    ENDIF()
+  ENDIF()
+ENDIF()
