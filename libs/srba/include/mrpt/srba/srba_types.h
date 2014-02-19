cb671647304f5eb4991d4adb30f8e3b035da447a
famoreno <famoreno@uma.es>
2014-02-19 03:14:19 -0800

    - Fixed Copyright notes for the last updated files.

--- libs/srba/include/mrpt/srba/srba_types.h
+++ libs/srba/include/mrpt/srba/srba_types.h
@@ -1,36 +1,10 @@
 /* +---------------------------------------------------------------------------+
-   |                 The Mobile Robot Programming Toolkit (MRPT)               |
-   |                                                                           |
+   |                     Mobile Robot Programming Toolkit (MRPT)               |
    |                          http://www.mrpt.org/                             |
    |                                                                           |
-   | Copyright (c) 2005-2013, Individual contributors, see AUTHORS file        |
-   | Copyright (c) 2005-2013, MAPIR group, University of Malaga                |
-   | Copyright (c) 2012-2013, University of Almeria                            |
-   | All rights reserved.                                                      |
-   |                                                                           |
-   | Redistribution and use in source and binary forms, with or without        |
-   | modification, are permitted provided that the following conditions are    |
-   | met:                                                                      |
-   |    * Redistributions of source code must retain the above copyright       |
-   |      notice, this list of conditions and the following disclaimer.        |
-   |    * Redistributions in binary form must reproduce the above copyright    |
-   |      notice, this list of conditions and the following disclaimer in the  |
-   |      documentation and/or other materials provided with the distribution. |
-   |    * Neither the name of the copyright holders nor the                    |
-   |      names of its contributors may be used to endorse or promote products |
-   |      derived from this software without specific prior written permission.|
-   |                                                                           |
-   | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       |
-   | 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED |
-   | TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR|
-   | PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE |
-   | FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL|
-   | DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR|
-   |  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)       |
-   | HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,       |
-   | STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  |
-   | ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           |
-   | POSSIBILITY OF SUCH DAMAGE.                                               |
+   | Copyright (c) 2005-2014, Individual contributors, see AUTHORS file        |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                   |
+   | Released under BSD License. See details in http://www.mrpt.org/License    |
    +---------------------------------------------------------------------------+ */
 
 #pragma once
@@ -511,8 +485,10 @@ namespace srba
 
 #ifdef SRBA_WORKAROUND_MSVC9_DEQUE_BUG
 		typedef typename std::deque< stlplus::smart_ptr<k2k_edge_t> > k2k_edges_deque_t;  // Note: A std::deque() does not invalidate pointers/references, as we always insert elements at the end; we'll exploit this...
+		typedef std::deque< stlplus::smart_ptr<k2f_edge_t> >   all_observations_deque_t;  
 #else
 		typedef typename mrpt::aligned_containers<k2k_edge_t>::deque_t  k2k_edges_deque_t;  // Note: A std::deque() does not invalidate pointers/references, as we always insert elements at the end; we'll exploit this...
+		typedef typename mrpt::aligned_containers<k2f_edge_t>::deque_t  all_observations_deque_t;
 #endif
 
 		typedef std::deque<keyframe_info>  keyframe_vector_t;  //!< Index are "TKeyFrameID" IDs. There's no NEED to make this a deque<> for preservation of references, but is an efficiency improvement
@@ -661,16 +637,11 @@ namespace srba
 
 		/** Index (by feat ID) of ALL landmarks stored in \a unknown_lms and \a known_lms.
 		  * Note that if gaps occur in the observed feature IDs, some pointers here will be NULL and some mem will be wasted, but in turn we have a O(1) search mechanism for all LMs. */
-		std::deque<TLandmarkEntry>   all_lms;
+		typename mrpt::aligned_containers<TLandmarkEntry>::deque_t   all_lms;
 
-		TSpanningTree           spanning_tree;
-
-#ifdef SRBA_WORKAROUND_MSVC9_DEQUE_BUG
-		std::deque< stlplus::smart_ptr<k2f_edge_t> >   all_observations;  //!< All raw observation data (k2f edges)
-#else
-		std::deque<k2f_edge_t>  all_observations;  //!< All raw observation data (k2f edges)
-#endif
-		TLinearSystem           lin_system;        //!< The sparse linear system of equations
+		TSpanningTree            spanning_tree;
+		all_observations_deque_t all_observations;  //!< All raw observation data (k2f edges)
+		TLinearSystem            lin_system;        //!< The sparse linear system of equations
 
 		/** Its size grows simultaneously to all_observations, its values are updated during optimization to
 		  *  reflect invalid conditions in some Jacobians so we can completely discard their information
