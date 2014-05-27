ed0f71c422b35d4e6b85508ec8c6cfee8299d2f5
Edu <edu@edu-desktop.(none)>
2014-05-27 16:21:15 +0200

    Modifications in OpenNI2 driver wrapper to support more resolutions

--- libs/hwdrivers/include/mrpt/hwdrivers/COpenNI2Generic.h
+++ libs/hwdrivers/include/mrpt/hwdrivers/COpenNI2Generic.h
@@ -35,7 +35,7 @@ namespace mrpt
 
       /** Get a list of the connected OpenNI2 sensors.
       */
-      void getConnectedDevices();
+      int getConnectedDevices();
 
 			/** Kill the OpenNI2 driver
 			  */
@@ -88,6 +88,15 @@ namespace mrpt
 			/** @name Sensor parameters (alternative to \a loadConfig ) and manual control
 			    @{ */
 
+//			/** Set the stream mode (resolution, fps and pixel format) for the OpenNI2 device */
+//      bool setONI2StreamMode(openni::VideoStream& stream, int w, int h, int fps, openni::PixelFormat format);
+//
+			/** Set the RGB stream mode (resolution, fps and pixel format) for the OpenNI2 device */
+      bool initONI2RGBStream(unsigned sensor_id, int w, int h, int fps, void* pFormat);
+
+			/** Set the Depth stream mode (resolution, fps and pixel format) for the OpenNI2 device */
+      bool initONI2DepthStream(unsigned sensor_id, int w, int h, int fps, void* pFormat);
+
 			/** Try to open the camera (set all the parameters before calling this) - users may also call initialize(), which in turn calls this method.
 			  *  Raises an exception upon error.
 			  * \exception std::exception A textual description of the error.
@@ -118,8 +127,9 @@ namespace mrpt
 			std::vector<void*>	vp_devices; // Opaque pointer to "openni::Device"
 
 			/** The same options (width, height and fps) are set for all the sensors. (This could be changed if necessary) */
-            int width, height;
-            float fps;
+      int width, height;
+      float fps;
+      void *rgb_pFormat, *depth_pFormat; // These parameters will be necesary if several pixel formats are used by this class.
 
 			/** A vector with pointers to the rgb streams of the available devices */
 			std::vector<void*> vp_depth_stream; // Opaque pointer to "openni::VideoStream"
