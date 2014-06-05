682136aebb1b4564752301f5b30356b5f4144a3e
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2014-06-05 23:17:59 +0200

    Fix undefined CMAKE_MRPT_HAS_DUO3D

--- cmakemodules/script_duo3d.cmake
+++ cmakemodules/script_duo3d.cmake
@@ -1,17 +1,16 @@
 # Support for DUO3D stereo camera
 # ==========================================================================================
-IF(UNIX)
-	SET( MRPT_HAS_DUO3D OFF CACHE INTERNAL "Has DUO3D libraries?" FORCE)
-ELSE(UNIX)
-	SET( MRPT_HAS_DUO3D OFF CACHE BOOL "Has DUO3D libraries?")
+SET(CMAKE_MRPT_HAS_DUO3D 0)  # Set default value (it cannot be empty!)
 
-	IF( MRPT_HAS_DUO3D )
-		SET( DUO3D_ROOT_DIR "" CACHE PATH "Path to DUO3D library root directory")
+SET( MRPT_HAS_DUO3D OFF CACHE BOOL "Build with support for DUO3D libraries?")
 
-		IF(UNIX)
-			MESSAGE("Sorry! DUO3D camera is supported only for Windows yet. Set MRPT_HAS_DUO3D to OFF")
-			SET(CMAKE_MRPT_HAS_DUO3D 0)
-		ELSE(UNIX)
+IF( MRPT_HAS_DUO3D )
+	IF(UNIX)
+		MESSAGE("Sorry! DUO3D camera is supported only for Windows yet. Set MRPT_HAS_DUO3D to OFF")
+	ELSE(UNIX)
+	
+			SET( DUO3D_ROOT_DIR "" CACHE PATH "Path to DUO3D library root directory")
+
 			# Set to 1, next check for missing things and set to 0 on any error & report message:
 			SET(CMAKE_MRPT_HAS_DUO3D 1)
 
@@ -29,8 +28,6 @@ ELSE(UNIX)
 				SET(CMAKE_MRPT_HAS_DUO3D 0)
 				MESSAGE("The directory 'DUO3D_ROOT_DIR' does not contain lib/DUOLib.lib. Turn off DUO3D support or provide the correct path.")
 			ENDIF(NOT EXISTS ${DUO3D_ROOT_DIR}/lib/DUOLib.lib)
-		ENDIF(UNIX)
-
-	ENDIF(MRPT_HAS_DUO3D)
-ENDIF(UNIX)
-
+	ENDIF(UNIX)
+			
+ENDIF(MRPT_HAS_DUO3D)
