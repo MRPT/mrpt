8481d81c23787894ae898d3aa6c8c05df9249e1e
Jose Luis Blanco <joseluisblancoc@gmail.com>
2014-06-12 17:10:03 +0200

    travis: after last cmit, all tests should pass

--- .travis.sh
+++ .travis.sh
@@ -26,16 +26,7 @@ function test ()
 {
   mkdir $BUILD_DIR && cd $BUILD_DIR
   cmake $MRPT_DIR -DBUILD_APPLICATIONS=FALSE
-  #make tests
-  make run_tests_mrpt_base -j2
-  make run_tests_mrpt_slam -j2
-  make run_tests_mrpt_graphslam -j2
-  make run_tests_mrpt_topography -j2
-  make run_tests_mrpt_srba -j2
-  make run_tests_mrpt_scanmatching -j2
-  make run_tests_mrpt_opengl -j2
-  make run_tests_mrpt_obs -j2
-  make run_tests_mrpt_maps -j2
+  make test
 }
 
 function testhwdrivers ()
