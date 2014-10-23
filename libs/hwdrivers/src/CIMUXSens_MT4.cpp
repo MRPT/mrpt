8599e3f8e5dc88f7231326c0b0e86c4b4b0cb245
Joe Burmeister <joe.a.burmeister@gmail.com>
2014-10-23 16:10:51 +0100

    Make the xsens also return altitude.

--- libs/hwdrivers/src/CIMUXSens_MT4.cpp
+++ libs/hwdrivers/src/CIMUXSens_MT4.cpp
@@ -635,6 +635,7 @@ void CIMUXSens_MT4::initialize()
 			configArray.push_back( XsOutputConfiguration(XDI_StatusByte, m_sampleFreq) );
 			configArray.push_back( XsOutputConfiguration(XDI_LatLon, m_sampleFreq) );
 			configArray.push_back( XsOutputConfiguration(XDI_GpsTimeUtc, m_sampleFreq) );
+			configArray.push_back( XsOutputConfiguration(XDI_AltitudeMsl, m_sampleFreq) );
 
 			if (!my_xsens_device.setOutputConfiguration(configArray))
 				throw std::runtime_error("Could not configure MTmk4 device. Aborting.");
