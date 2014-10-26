8a2bd2fe5764870cfbfeae66c7e5ab5c805eebfe
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2014-10-26 16:26:05 +0100

    start solving compat with opencv 3.0

--- libs/base/include/mrpt/otherlibs/do_opencv_includes.h
+++ libs/base/include/mrpt/otherlibs/do_opencv_includes.h
@@ -39,8 +39,10 @@
 #		include <opencv2/calib3d/calib3d.hpp>
 #		include <opencv2/objdetect/objdetect.hpp>
 
-#		include <opencv2/legacy/legacy.hpp>  // CvImage
-#		include <opencv2/legacy/compat.hpp>
+#		if MRPT_OPENCV_VERSION_NUM<0x300  // These ones dissapeared in 3.0.0
+#			include <opencv2/legacy/legacy.hpp>  // CvImage
+#			include <opencv2/legacy/compat.hpp>
+#		endif
 #		if (MRPT_OPENCV_VERSION_NUM>=0x240) && MRPT_HAS_OPENCV_NONFREE
 #			include <opencv2/nonfree/nonfree.hpp>
 #		endif
