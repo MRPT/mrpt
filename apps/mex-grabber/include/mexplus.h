0adbaec8fafaba9a579f66600e8b88c11197a3c0
Jesus Briales <jesusbriales@gmail.com>
2014-12-04 23:08:53 +0100

    Basic files for mexplus project
    The basic files are added in a app-like folder

---
+++ apps/mex-grabber/include/mexplus.h
@@ -0,0 +1,12 @@
+/** MEXPLUS: C++ Matlab MEX development kit.
+ *
+ * Kota Yamaguchi 2014
+ */
+
+#ifndef __MEXPLUS_H__
+#define __MEXPLUS_H__
+
+#include <mexplus/arguments.h>
+#include <mexplus/dispatch.h>
+
+#endif // __MEXPLUS_H__
