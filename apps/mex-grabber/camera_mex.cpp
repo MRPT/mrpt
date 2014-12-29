d111d93bd1298caab6f639b43ad3bcf255812282
Jesus Briales <jesusbriales@gmail.com>
2014-12-29 02:27:11 +0100

    Add ::from method for cv::Mat copied from MxArray.hpp of mexopencv

--- apps/mex-grabber/camera_mex.cpp
+++ apps/mex-grabber/camera_mex.cpp
@@ -43,6 +43,9 @@
 // Matlab MEX interface headers
 #include <mexplus.h>
 
+// Matlab-OpenCV interface from mexopencv: headers
+//#include <MxArray.hpp>
+
 using namespace mrpt;
 using namespace mrpt::system;
 using namespace mrpt::hwdrivers;
@@ -50,6 +53,154 @@ using namespace mrpt::utils;
 using namespace mrpt::slam;
 using namespace std;
 using namespace mexplus;
+
+// Add cv::Mat method
+namespace mexplus {
+/** mxArray* importer methods.
+ */
+//template <typename T>
+//static mxArray* from(const T& value) { return fromInternal<T>(value); }
+//static mxArray* from(const char* value) {
+//  mxArray* array = mxCreateString(value);
+//  MEXPLUS_CHECK_NOTNULL(array);
+//  return array;
+//}
+//static mxArray* from(int32_t value) {
+//  mxArray* array = mxCreateNumericMatrix(1, 1, mxINT32_CLASS, mxREAL);
+//  MEXPLUS_CHECK_NOTNULL(array);
+//  *reinterpret_cast<int32_t*>(mxGetData(array)) = value;
+//  return array;
+//}
+
+/** std::map wrapper with one-line initialization and lookup method.
+ * Initialization
+ * @code
+ * const ConstMap<std::string,int> BorderType = ConstMap<std::string,int>
+ *     ("Replicate",  cv::BORDER_REPLICATE)
+ *     ("Constant",   cv::BORDER_CONSTANT)
+ *     ("Reflect",    cv::BORDER_REFLECT);
+ * @endcode
+ * Lookup
+ * @code
+ * BorderType["Constant"] // => cv::BORDER_CONSTANT
+ * @endcode
+ */
+template <typename T, typename U>
+class ConstMap
+{
+  public:
+    /// Constructor with a single key-value pair
+    ConstMap(const T& key, const U& val)
+    {
+        m_[key] = val;
+    }
+    /// Consecutive insertion operator
+    ConstMap<T, U>& operator()(const T& key, const U& val)
+    {
+        m_[key] = val;
+        return *this;
+    }
+    /// Implicit converter to std::map
+    operator std::map<T, U>() { return m_; }
+    /// Lookup operator; fail if not found
+    U operator [](const T& key) const
+    {
+        typename std::map<T,U>::const_iterator it = m_.find(key);
+        if (it==m_.end())
+            mexErrMsgIdAndTxt("mexopencv:error", "Value not found");
+        return (*it).second;
+    }
+  private:
+    std::map<T, U> m_;
+};
+
+/** Translates data type definition used in Matlab to that of OpenCV.
+ * @param depth data depth of opencv's Mat class. e.g., CV_32F.
+ * @return data type of matlab's mxArray. e.g., mxDOUBLE_CLASS.
+ */
+const ConstMap<int,mxClassID> ClassIDOf = ConstMap<int,mxClassID>
+    (CV_64F,    mxDOUBLE_CLASS)
+    (CV_32F,    mxSINGLE_CLASS)
+    (CV_8S,     mxINT8_CLASS)
+    (CV_8U,     mxUINT8_CLASS)
+    (CV_16S,    mxINT16_CLASS)
+    (CV_16U,    mxUINT16_CLASS)
+    (CV_32S,    mxINT32_CLASS);
+
+/** Translates data type definition used in OpenCV to that of Matlab.
+ * @param classid data type of matlab's mxArray. e.g., mxDOUBLE_CLASS.
+ * @return opencv's data type. e.g., CV_8U.
+ */
+const ConstMap<mxClassID, int> DepthOf = ConstMap<mxClassID, int>
+    (mxDOUBLE_CLASS,   CV_64F)
+    (mxSINGLE_CLASS,   CV_32F)
+    (mxINT8_CLASS,     CV_8S)
+    (mxUINT8_CLASS,    CV_8U)
+    (mxINT16_CLASS,    CV_16S)
+    (mxUINT16_CLASS,   CV_16U)
+    (mxINT32_CLASS,    CV_32S)
+    (mxUINT32_CLASS,   CV_32S)
+    (mxLOGICAL_CLASS,  CV_8U);
+
+template <>
+mxArray* MxArray::from(const cv::Mat& mat)
+{
+    mxArray* p_; // Create pointer
+    if (mat.empty())
+    {
+        p_ = mxCreateNumericArray(0, 0, mxDOUBLE_CLASS, mxREAL);
+        if (!p_)
+            mexErrMsgIdAndTxt("mexopencv:error", "Allocation error");
+        return p_;
+    }
+    // Optional arguments:
+    mxClassID classid = mxUNKNOWN_CLASS;
+    bool transpose = true;
+
+    cv::Mat input = (mat.dims == 2 && transpose) ? mat.t() : mat;
+    // Create a new mxArray.
+    const int nchannels = input.channels();
+    const int* dims_ = input.size;
+    std::vector<mwSize> d(dims_, dims_ + input.dims);
+    d.push_back(nchannels);
+    classid = (classid == mxUNKNOWN_CLASS)
+        ? ClassIDOf[input.depth()] : classid;
+    std::swap(d[0], d[1]);
+    if (classid == mxLOGICAL_CLASS)
+    {
+        // OpenCV's logical true is any nonzero while matlab's true is 1.
+        cv::compare(input, 0, input, cv::CMP_NE);
+        input.setTo(1, input);
+        p_ = mxCreateLogicalArray(d.size(), &d[0]);
+    }
+    else {
+        p_ = mxCreateNumericArray(d.size(), &d[0], classid, mxREAL);
+    }
+    if (!p_)
+        mexErrMsgIdAndTxt("mexopencv:error", "Allocation error");
+    // Copy each channel.
+    std::vector<cv::Mat> channels;
+    split(input, channels);
+    std::vector<mwSize> si(d.size(), 0); // subscript index.
+    int type = CV_MAKETYPE(DepthOf[classid], 1); // destination type.
+    for (int i = 0; i < nchannels; ++i)
+    {
+        si[si.size() - 1] = i; // last dim is a channel index.
+
+        mwIndex subs_si = mxCalcSingleSubscript(p_, si.size(), &si[0]);
+//        void *ptr = reinterpret_cast<void*>(
+//                reinterpret_cast<size_t>(mxGetData(p_)) +
+//                mxGetElementSize(p_) * subs(si));
+        void *ptr = reinterpret_cast<void*>(
+                reinterpret_cast<size_t>(mxGetData(p_)) +
+                mxGetElementSize(p_) * subs_si);
+        cv::Mat m(input.dims, dims_, type, ptr);
+        channels[i].convertTo(m, type); // Write to mxArray through m.
+    }
+    return p_;
+}
+
+} // namespace mexplus
 
 //#define CLASS CHokuyoURG
 #define CLASS CGenericSensor
@@ -243,7 +394,7 @@ MEX_DEFINE(read) (int nlhs, mxArray* plhs[],
   }
   else if( IS_CLASS(obj, CObservationImage) )
   {
-      mexPrintf("Reading image\n");
+      //mexPrintf("Reading image\n");
 
       CObservationImagePtr cam_obs = CObservationImagePtr(obj);
 
@@ -251,10 +402,15 @@ MEX_DEFINE(read) (int nlhs, mxArray* plhs[],
       // to use cvarrToMat and IplImage, respectively
       cv::Mat cvImg = cv::cvarrToMat( cam_obs->image.getAs<IplImage>() );
 
+//      cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
+//      cv::imshow( "Display window", cvImg );                   // Show our image inside it.
+//      cv::waitKey(0);                                          // Wait for a keystroke in the window
+
       //CMatrixFloat mat;
       //cam_obs->image.grayscale().getAsMatrix( mat );
 
-      cam_obs->image.saveToFile("/home/jesus/output_image.jpg");
+      //cam_obs->image.saveToFile("/home/jesus/output_image.jpg");
+
       /*
       mexPrintf( "Rows %d and columns %d\n", mat.rows(), mat.cols() );
       int num_of_pixels = mat.rows() * mat.cols();
@@ -263,9 +419,11 @@ MEX_DEFINE(read) (int nlhs, mxArray* plhs[],
       output.set(0, vector<float>( mat.data(), mat.data()+num_of_pixels ) );
       */
 
-      output.set(0, 0);
+      output.set(0, cvImg);
       output.set(1, 1);
       output.set(2, 2);
+
+      //plhs[0] = mexopencv::MxArray(cvImg);
 
       // Delete useless objects
   }
