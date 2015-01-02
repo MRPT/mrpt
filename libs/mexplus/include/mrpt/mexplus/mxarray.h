46ec2610b4f5f43382220d7b0dbb0e54120ebae8
Jesus Briales <jesusbriales@gmail.com>
2015-01-02 03:39:53 +0100

    Add extra OpenCV code to mxarray.h file
    NOTE: This should be temporal, or added only if has OpenCV

--- libs/mexplus/include/mrpt/mexplus/mxarray.h
+++ libs/mexplus/include/mrpt/mexplus/mxarray.h
@@ -57,6 +57,8 @@
 #include <typeinfo>
 #include <vector>
 
+#include "mxarray_extra.h"
+
 /** Macro definitions.
  */
 #define MEXPLUS_CHECK_NOTNULL(pointer) \
@@ -270,6 +272,68 @@ public:
     *reinterpret_cast<int32_t*>(mxGetData(array)) = value;
     return array;
   }
+
+  // Extra code for OpenCV classes
+  static mxArray* from(const cv::Mat& mat)
+  {
+      mxArray* p_; // Create pointer
+      if (mat.empty())
+      {
+          p_ = mxCreateNumericArray(0, 0, mxDOUBLE_CLASS, mxREAL);
+          if (!p_)
+              mexErrMsgIdAndTxt("mexopencv:error", "Allocation error");
+          return p_;
+      }
+      // Optional arguments:
+      mxClassID classid = mxUNKNOWN_CLASS;
+      bool transpose = true;
+
+      cv::Mat input = (mat.dims == 2 && transpose) ? mat.t() : mat;
+      // Create a new mxArray.
+      const int nchannels = input.channels();
+      const int* dims_ = input.size;
+      std::vector<mwSize> d(dims_, dims_ + input.dims);
+      d.push_back(nchannels);
+      classid = (classid == mxUNKNOWN_CLASS)
+          ? ClassIDOf[input.depth()] : classid;
+      std::swap(d[0], d[1]);
+      if (classid == mxLOGICAL_CLASS)
+      {
+          // OpenCV's logical true is any nonzero while matlab's true is 1.
+          cv::compare(input, 0, input, cv::CMP_NE);
+          input.setTo(1, input);
+          p_ = mxCreateLogicalArray(d.size(), &d[0]);
+      }
+      else {
+          p_ = mxCreateNumericArray(d.size(), &d[0], classid, mxREAL);
+      }
+      if (!p_)
+          mexErrMsgIdAndTxt("mexopencv:error", "Allocation error");
+      // Copy each channel.
+      std::vector<cv::Mat> channels;
+      split(input, channels);
+      std::vector<mwSize> si(d.size(), 0); // subscript index.
+      int type = CV_MAKETYPE(DepthOf[classid], 1); // destination type.
+      for (int i = 0; i < nchannels; ++i)
+      {
+          si[si.size() - 1] = i; // last dim is a channel index.
+
+          mwIndex subs_si = mxCalcSingleSubscript(p_, si.size(), &si[0]);
+  //        void *ptr = reinterpret_cast<void*>(
+  //                reinterpret_cast<size_t>(mxGetData(p_)) +
+  //                mxGetElementSize(p_) * subs(si));
+          void *ptr = reinterpret_cast<void*>(
+                  reinterpret_cast<size_t>(mxGetData(p_)) +
+                  mxGetElementSize(p_) * subs_si);
+          cv::Mat m(input.dims, dims_, type, ptr);
+          //channels[i].convertTo(m, type); // Write to mxArray through m.
+          // Swap R and B channels
+          MRPT_TODO("Do in other place where it is more clear")
+          channels[nchannels-1-i].convertTo(m, type); // Write to mxArray through m.
+      }
+      return p_;
+  }
+
   /** mxArray* exporter methods.
    */
   template <typename T>
@@ -531,7 +595,7 @@ public:
   std::vector<std::string> fieldNames() const {
     MEXPLUS_ASSERT(isStruct(), "Expected a struct array.");
     std::vector<std::string> fields(fieldSize());
-    for (int i = 0; i < fields.size(); ++i)
+    for (size_t i = 0; i < fields.size(); ++i)
       fields[i] = fieldName(i);
     return fields;
   }
