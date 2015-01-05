bd43d9b1e421ac440d024dec1590a0984ca69f96
Jesus Briales <jesusbriales@gmail.com>
2015-01-05 02:42:35 +0100

    Some changes to DeclareMEXLib
    Increase code simplicity by writting one single SET_TARGET_PROPERTIES for which:
    - Lib PREFIX is empty
    - Lib SUFFIX is chosen according to OS
    - The DEBUG_POSTFIX is removed to keep MEX lib name unique
    
    Besides the general macro (internal) two macros are defined:
    - define_mex_lib for MEX functions (e.g. fun.mexglx, called as a simple Matlab function)
    - define_mex_lib_private for MEX library of class methods (e.g. Cfoo_.mexglx, handled by Cfoo.m class)

--- cmakemodules/DeclareMEXLib.cmake
+++ cmakemodules/DeclareMEXLib.cmake
@@ -1,36 +1,55 @@
+# define_mex_lib(): Declares a usual MEX function:
+#-----------------------------------------------------------------------
 macro(define_mex_lib name)
-        # 32-bit or 64-bit mex
-        if(WIN32)
-          if (CMAKE_CL_64)
-                  SET_TARGET_PROPERTIES(${name} 
-					PROPERTIES SUFFIX _.mexw64 
-					LIBRARY_OUTPUT_DIRECTORY "${MEX_LIBRARY_OUTPUT_PATH}${ARGN}"
-					RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/"
-					)
-          else(CMAKE_CL_64)
-                  SET_TARGET_PROPERTIES(${name} 
-				  PROPERTIES SUFFIX _.mexw32 
-				  LIBRARY_OUTPUT_DIRECTORY "${MEX_LIBRARY_OUTPUT_PATH}${ARGN}"
-				  RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/"
-				  )
-          endif(CMAKE_CL_64)
-        else(WIN32)
-          if (CMAKE_SIZEOF_VOID_P MATCHES "8")
-                  SET_TARGET_PROPERTIES(${name} PROPERTIES 
-				  SUFFIX _.mexa64 PREFIX "" 
-				  LIBRARY_OUTPUT_DIRECTORY "${MEX_LIBRARY_OUTPUT_PATH}${ARGN}"
-				  RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/"
-				  )
-          else(CMAKE_SIZEOF_VOID_P MATCHES "8")
-                  SET_TARGET_PROPERTIES(${name} PROPERTIES 
-				  SUFFIX _.mexglx PREFIX "" 
-				  LIBRARY_OUTPUT_DIRECTORY "${MEX_LIBRARY_OUTPUT_PATH}${ARGN}"
-				  RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/"
-				  )
-          endif (CMAKE_SIZEOF_VOID_P MATCHES "8")
-        endif(WIN32)
+	internal_define_mex_lib(${name} 0 ) # is_private = 0
 endmacro(define_mex_lib)
 
+# define_mex_lib_private(): Declares a private MEX library, which is stored in /private subfolder
+# private libraries are typically used for MEX classes (MEX libraries containing several methods
+# which are accessed through a Matlab class in a .m file)
+# See: http://es.mathworks.com/help/matlab/matlab_prog/private-functions.html
+#-----------------------------------------------------------------------
+macro(define_mex_lib_private name)
+	internal_define_mex_lib(${name} 1 ) # is_private = 1
+endmacro(define_mex_lib_private)
+
+# Implementation of both define_mex_lib() and define_mex_lib_private():
+#-----------------------------------------------------------------------------
+macro(internal_define_mex_lib   name   is_private)
+	INCLUDE(../../../cmakemodules/AssureCMakeRootFile.cmake) # Avoid user mistake in CMake source directory
+
+	IF(${is_private})
+		SET(subfolder  private) # Special subfolder private in Matlab (restricted access)
+		SET(underscore "_")		# Suffix _ (syntax chosen for private libraries)
+	ENDIF(${is_private})
+
+	# 32-bit or 64-bit mex: choose correct suffix depending on system
+	if(WIN32)
+		if (CMAKE_CL_64)
+			SET(MEX_SUFFIX .mexw64)
+		else(CMAKE_CL_64)
+			SET(MEX_SUFFIX .mexw32)
+		endif(CMAKE_CL_64)
+	else(WIN32)
+		if (CMAKE_SIZEOF_VOID_P MATCHES "8")
+			SET(MEX_SUFFIX .mexa64)
+		else(CMAKE_SIZEOF_VOID_P MATCHES "8")
+			SET(MEX_SUFFIX .mexglx)
+		endif (CMAKE_SIZEOF_VOID_P MATCHES "8")
+	endif(WIN32)
+
+	# Define MEX library target with specific options
+	SET_TARGET_PROPERTIES(${name} PROPERTIES
+	PREFIX ""
+	SUFFIX "${underscore}${MEX_SUFFIX}"
+	DEBUG_POSTFIX "" # Remove DEBUG_POSTFIX in MEX libraries (usually -dbg)
+	LIBRARY_OUTPUT_DIRECTORY "${MEX_LIBRARY_OUTPUT_PATH}/${subfolder}"
+	RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/"
+	)
+endmacro(internal_define_mex_lib)
+
+# define_mex_test(): Declares a MEX executable which allows debug of mexFunction() through main() function:
+#-----------------------------------------------------------------------
 macro(define_mex_test name)
-        SET_TARGET_PROPERTIES(${name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${MEX_EXECUTABLE_OUTPUT_PATH})
+SET_TARGET_PROPERTIES(${name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${MEX_EXECUTABLE_OUTPUT_PATH})
 endmacro(define_mex_test name)
