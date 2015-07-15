a3f469634ad44956b8c94698d4e73f6c733ff412
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2015-07-16 01:15:06 +0200

    IMU observation doc fixes

--- libs/obs/include/mrpt/obs/CObservationIMU.h
+++ libs/obs/include/mrpt/obs/CObservationIMU.h
@@ -65,39 +65,8 @@ namespace obs
 
 	/** This class stores measurements from an Inertial Measurement Unit (IMU) (attitude estimation, raw gyroscope and accelerometer values), altimeters or magnetometers.
 	 *
-	 *  The order of the 21 raw values in each entry of mrpt::obs::CObservationIMU::rawMeasurements is (you can use the TIMUDataIndex "enum" symbolic names):
-		<table>
-		<tr> <td> 0 </td> <td>IMU_X_ACC</td> <td> x-axis acceleration (m/sec<sup>2</sup>)</td> </tr>
-		<tr> <td> 1 </td> <td>IMU_Y_ACC</td> <td> y-axis acceleration (m/sec<sup>2</sup>)</td> </tr>
-		<tr> <td> 2 </td> <td>IMU_Z_ACC</td> <td> z-axis acceleration (m/sec<sup>2</sup>)</td> </tr>
-		<tr> <td> 3 </td> <td>IMU_YAW_VEL</td> <td> yaw angular velocity (rad/sec)</td> </tr>
-		<tr> <td> 4 </td> <td>IMU_PITCH_VEL</td> <td> pitch angular velocity (rad/sec)</td> </tr>
-		<tr> <td> 5 </td> <td>IMU_ROLL_VEL</td> <td> roll angular velocity (rad/sec)</td> </tr>
-		<tr> <td> 6 </td> <td>IMU_X_VEL</td> <td> x-axis velocity (m/sec)</td> </tr>
-		<tr> <td> 7 </td> <td>IMU_Y_VEL</td> <td> y-axis velocity (m/sec)</td> </tr>
-		<tr> <td> 8 </td> <td>IMU_Z_VEL</td> <td> z-axis velocity (m/sec)</td> </tr>
-		<tr> <td> 9 </td> <td>IMU_YAW</td> <td> yaw absolute value (rad)</td> </tr>
-		<tr> <td> 10 </td> <td>IMU_PITCH</td> <td> pitch absolute value (rad)</td> </tr>
-		<tr> <td> 11 </td> <td>IMU_ROLL</td> <td> roll absolute value (rad)</td> </tr>
-		<tr> <td> 12 </td> <td>IMU_X</td> <td> x absolute value (meters)</td> </tr>
-		<tr> <td> 13 </td> <td>IMU_Y</td> <td> y absolute value (meters)</td> </tr>
-		<tr> <td> 14 </td> <td>IMU_Z</td> <td> z absolute value (meters)</td> </tr>
-		<tr> <td> 15 </td> <td>IMU_MAG_X</td> <td> x magnetic field value (gauss)</td> </tr>
-		<tr> <td> 16 </td> <td>IMU_MAG_Y</td> <td> y magnetic field value (gauss)</td> </tr>
-		<tr> <td> 17 </td> <td>IMU_MAG_Z</td> <td> z magnetic field value (gauss)</td> </tr>
-		<tr> <td> 18 </td> <td>IMU_PRESSURE</td> <td> air pressure (Pascals)</td> </tr>
-		<tr> <td> 19 </td> <td>IMU_ALTITUDE</td> <td>altitude from an altimeter (meters)</td> </tr>
-		<tr> <td> 20 </td> <td>IMU_TEMPERATURE</td> <td> temperature (degrees Celsius)</td> </tr>
-		<tr> <td> 21 </td> <td>IMU_ORI_QUAT_X</td> <td> Orientation Quaternion X value </td> </tr>
-		<tr> <td> 22 </td> <td>IMU_ORI_QUAT_Y</td> <td> Orientation Quaternion Y value </td> </tr>
-		<tr> <td> 23 </td> <td>IMU_ORI_QUAT_Z</td> <td> Orientation Quaternion Z value </td> </tr>
-		<tr> <td> 24 </td> <td>IMU_ORI_QUAT_W</td> <td> Orientation Quaternion W value </td> </tr>
-		</table>
-	 *
-	 *  Values from 0 to 5 are direct measurements measured by accelerometers & gyroscopes. 
-	 *  Values at indices from 6 to 14, if present, are estimates (dead reckoning) from the IMU unit.
-	 *  Values at indices 15 to 20, if present, are from additional sensors.
-	 *  Values at indices 20 to 24, if present, are a quaternion representation of the orientation.
+	 *  The order of the values in each entry of mrpt::obs::CObservationIMU::rawMeasurements is defined as symbolic names in the enum mrpt::obs::TIMUDataIndex. 
+	 *  Check it out also for reference on the unit and the coordinate frame used for each value.
 	 *
 	 * \sa CObservation
 	 * \ingroup mrpt_obs_grp
