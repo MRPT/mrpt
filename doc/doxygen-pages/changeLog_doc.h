745cafb86df54e7cd0ed7855918e9a7bf0ad603e
Jose Luis Blanco <joseluisblancoc@gmail.com>
2015-11-03 16:13:37 +0100

    Update changelog: release of 1.3.2

--- doc/doxygen-pages/changeLog_doc.h
+++ doc/doxygen-pages/changeLog_doc.h
@@ -13,7 +13,7 @@
 <p> <b>Note:</b> <i>If you are displaying a local version of this page and you have not built the whole HTML documentation, the links above will be broken. Either build the documentation invoking <code>make documentation_html</code> or [browse it on-line](http://www.mrpt.org/).</i></p>
 
 <a name="1.3.2">
-  <h2>Version 1.3.2: (Under development) </h2></a>
+  <h2>Version 1.3.2: Released 3-NOV-2015 </h2></a>
   	- Changes in Apps:
 		- [rawlog-edit](http://www.mrpt.org/Application%3Arawlog-edit):
 			- New operation: `--list-poses`
@@ -31,6 +31,7 @@
 	- BUG FIXES: 
 		- Fix excessive width of paths drawn by CMetricMapBuilderRBPF::drawCurrentEstimationToImage()
 		- Fix image distortion: k3 may be ignored. (Thanks to CBaiz)
+		- Fix Debian bugs.
 
 <hr>
 <a name="1.3.1">
