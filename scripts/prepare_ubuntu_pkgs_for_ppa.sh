bc5e7d3716605176f429022e7700947e8e1d9da8
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2016-01-30 19:17:26 +0100

    Build PPA for Xenial

--- scripts/prepare_ubuntu_pkgs_for_ppa.sh
+++ scripts/prepare_ubuntu_pkgs_for_ppa.sh
@@ -40,8 +40,8 @@ rm -fr $MRPT_UBUNTU_OUT_DIR/
 # -------------------------------------------------------------------
 # And now create the custom packages for each Ubuntu distribution:
 # -------------------------------------------------------------------
-LST_DISTROS=(wily vivid trusty precise )
-LST_EBDEIGN=( 0      0   0        1    )
+LST_DISTROS=(xenial wily vivid trusty precise )
+LST_EBDEIGN=( 0       0      0   0        1   )
 
 count=${#LST_DISTROS[@]}
 IDXS=$(seq 0 $(expr $count - 1))
