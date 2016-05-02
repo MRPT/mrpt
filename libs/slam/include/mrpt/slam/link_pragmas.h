22748e9bfe524541f79d26b8526964fbb61bbd7d
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2016-05-02 23:02:29 +0200

    complete the clean up of link pragma usages

--- libs/slam/include/mrpt/slam/link_pragmas.h
+++ libs/slam/include/mrpt/slam/link_pragmas.h
@@ -18,18 +18,6 @@
 //  MRPT_XXX_EXPORT, MRPT_XXX_IMPORT
 //  SLAM_IMPEXP, mrpt_xxx_EXPORTS
 
-// If we are building the DLL (_EXPORTS), do not link against the .lib files:
-#if !defined(mrpt_slam_EXPORTS) && (defined(_MSC_VER) || defined(__BORLANDC__))
-#	if defined(_DEBUG)
-#		pragma comment (lib, BOOST_JOIN( BOOST_JOIN("libmrpt-slam",MRPT_VERSION_POSTFIX),"-dbg.lib"))
-#	else
-#		pragma comment (lib, BOOST_JOIN( BOOST_JOIN("libmrpt-slam",MRPT_VERSION_POSTFIX),".lib"))
-#	endif
-#endif
-
-
-
-
 /*   The macros below for DLL import/export are required for Windows only.
     Mostly all the definitions in this file are copied or at least slamd
      on the file wx/dlimpexp.h, written by Vadim Zeitlin and published
@@ -87,7 +75,7 @@
 #		define SLAM_IMPEXP MRPT_SLAM_IMPORT
 #	endif
 #else /* not making nor using DLL */
-#    define SLAM_IMPEXP 
+#    define SLAM_IMPEXP
 #endif
 
 
