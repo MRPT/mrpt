c1bba63713eb449260e76aa9fca1e6d7c8a68fa0
Jose Luis Blanco <joseluisblancoc@gmail.com>
2016-05-12 12:14:02 +0200

    Fixed #249

--- libs/maps/src/maps/CPointsMap_liblas.cpp
+++ libs/maps/src/maps/CPointsMap_liblas.cpp
@@ -114,7 +114,11 @@ bool CPointsMap::loadLASFile(const std::string &filename, LAS_HeaderInfo &out_he
 	out_headerInfo.FileSignature      = header.GetFileSignature();
 	out_headerInfo.SystemIdentifier   = header.GetSystemId();
 	out_headerInfo.SoftwareIdentifier = header.GetSoftwareId();
+#if LIBLAS_VERSION_NUM < 1800
 	out_headerInfo.project_guid       = header.GetProjectId().to_string();
+#else
+	out_headerInfo.project_guid       = boost::uuids::to_string(header.GetProjectId());
+#endif
 	out_headerInfo.spatial_reference_proj4 = header.GetSRS().GetProj4();
 	out_headerInfo.creation_year = header.GetCreationYear();
 	out_headerInfo.creation_DOY  = header.GetCreationDOY();
