4e7621932634b9d5fbc3ead54bde63d90e10da42
Nikos Koukis <nickkouk@gmail.com>
2016-05-13 13:36:14 +0300

    Add documentation fixes

--- graphslam-engine/graphslam_engine_app.cpp
+++ graphslam-engine/graphslam_engine_app.cpp
@@ -1,3 +1,13 @@
+/* +---------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)               |
+   |                          http://www.mrpt.org/                             |
+   |                                                                           |
+   | Copyright (c) 2005-2016, Individual contributors, see AUTHORS file        |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                   |
+   | Released under BSD License. See details in http://www.mrpt.org/License    |
+   +---------------------------------------------------------------------------+ */
+
+
 #include <mrpt/obs/CRawlog.h>
 #include <mrpt/graphslam.h>
 #include <mrpt/graphs.h>
@@ -33,8 +43,16 @@ int main(int argc, char **argv)
   
   try {
     const string laser_scans_fname = "../dataset_malaga20060121/20060121-Teleco_Faculty_Malaga_laser_only.rawlog";
+    const string config_fname = "../default_config.ini";
     GraphSlamEngine_t<CNetworkOfPoses2DInf> g_engine;
     g_engine.parseLaserScansFile(laser_scans_fname);
+
+    g_engine.readConfigFile(config_fname);
+    cout << "Config file read." << endl;
+
+    g_engine.printProblemParams();
+    cout << "Config file printed." << endl;
+    
 
 
   }
