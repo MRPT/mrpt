17f99f6365c7c914976d511f1ad143116b207fc0
Nikos Koukis <nickkouk@gmail.com>
2016-06-01 00:49:56 +0300

    Make format changes for mrpt compatibility
    - Tabs over spaces for indentation, alignment
    - Remove trailing whitespace
    - Convert all non-single spaces to tabs (tabstop=2 in vim)

--- graphslam-engine/GraphSlamEngine.h
+++ graphslam-engine/GraphSlamEngine.h
@@ -21,7 +21,6 @@
 #include <mrpt/system/os.h>
 #include <mrpt/poses/CPoses2DSequence.h>
 #include <mrpt/poses/CPosePDF.h>
-//#include <mrpt/utils.h>
 #include <mrpt/utils/CLoadableOptions.h>
 #include <mrpt/utils/mrpt_stdint.h>
 #include <mrpt/utils/mrpt_macros.h>
@@ -34,7 +33,6 @@
 #include <mrpt/slam/CICP.h>
 #include <mrpt/slam/CMetricMapBuilder.h>
 #include <mrpt/slam/CMetricMapBuilderICP.h>
-//#include <mrpt/graphs.h>
 #include <mrpt/graphs/CNetworkOfPoses.h>
 #include <mrpt/graphslam.h>
 #include <mrpt/gui/CDisplayWindow3D.h>
@@ -65,276 +63,276 @@ using namespace mrpt::graphslam;
 using namespace std;
 
 bool verbose = true;
-#define VERBOSE_COUT  if (verbose) std::cout << "[graphslam_engine] "
+#define VERBOSE_COUT	if (verbose) std::cout << "[graphslam_engine] "
 
 template <class GRAPH_t>
 class GraphSlamEngine_t {
-  public:
+	public:
 
-    typedef std::map<string, CFileOutputStream*> fstreams;
-    typedef std::map<string, CFileOutputStream*>::iterator fstreams_it;
-    typedef std::map<string, CFileOutputStream*>::const_iterator fstreams_cit;
+		typedef std::map<string, CFileOutputStream*> fstreams;
+		typedef std::map<string, CFileOutputStream*>::iterator fstreams_it;
+		typedef std::map<string, CFileOutputStream*>::const_iterator fstreams_cit;
 
-    typedef typename GRAPH_t::constraint_t constraint_t;
-    typedef typename GRAPH_t::constraint_t::type_value pose_t; // type of underlying poses (2D/3D)
-    typedef CMatrixFixedNumeric<double,
-            constraint_t::state_length, constraint_t::state_length> InfMat;
-
-
-    // Ctors, Dtors
-    //////////////////////////////////////////////////////////////
-    GraphSlamEngine_t(const string& config_file,
-        CDisplayWindow3D* win = NULL,
-        string rawlog_fname = "");
-    ~GraphSlamEngine_t();
-
-    // Public function definitions
-    //////////////////////////////////////////////////////////////
-    /**
-     * Wrapper fun around the GRAPH_t corresponding method
-     */
-    void saveGraph() const {
-      MRPT_START
-
-      if (!m_has_read_config) {
-        THROW_EXCEPTION("Config file has not been provided yet.\nExiting...");
-      }
-      string fname = m_output_dir_fname + "/" + m_save_graph_fname;
-      saveGraph(fname);
-
-      MRPT_END
-    }
-    /**
-     * Wrapper fun around the GRAPH_t corresponding method
-     */
-    void saveGraph(const string& fname) const {
-      MRPT_START
-
-      m_graph.saveToTextFile(fname);
-      VERBOSE_COUT << "Saved graph to text file: " << fname <<
-        " successfully." << endl;
-
-      MRPT_END
-    }
-    /** 
-     * Read the configuration file specified and fill in the corresponding
-     * class members
-     */
-   void readConfigFile(const string& fname);
-    /**
-     * Print the problem parameters (usually fetched from a configuration file)
-     * to the console for verification
-     *
-     * \sa GraphSlamEngine_t::parseRawlogFile
-     */
-    void printProblemParams() const;
-    /**
-     * TODO - Make this a function template so that it can handle camera
-     * images, laser scan files, etc.
-     * Reads the file provided and builds the graph 
-     */
-    void parseRawlogFile();
-    /**
-     * GraphslamEngine_t::optmizeGraph
-     *
-     * Optimize the under-construction graph
-     */
-    inline void optimizeGraph(GRAPH_t graph) {
-      MRPT_START
-
-      cout << "Executing the graph optimization" << endl;
-
-      // TODO: read these throught the .ini file
-      // params for the optimization of the graph
-      TParametersDouble  optimization_params;
-      optimization_params["verbose"]  = 0;
-      optimization_params["profiler"] = 0;
-      optimization_params["max_iterations"] = 500;
-      optimization_params["scale_hessian"] = 0.1;  // If <1, will "exagerate" the scale of the gradient and, normally, will converge much faster.
-      optimization_params["tau"] = 1e-3;
-      // e2: Lev-marq algorithm iteration stopping criterion #2: |delta_incr| < e2*(x_norm+e2)
-      //optimization_params["e1"] = 1e-6;
-      //optimization_params["e2"] = 1e-6;
-
-      graphslam::TResultInfoSpaLevMarq  levmarq_info;
-
-      // Execute the optimization
-      graphslam::optimize_graph_spa_levmarq(
-          graph,
-          levmarq_info,
-          NULL,  // List of nodes to optimize. NULL -> all but the root node.
-          optimization_params,
-          NULL); // functor feedback
-          //&optimization_feedback);
-
-      MRPT_END
-    }
-
-    /**
-     * GraphSlamEngine_t::visualizeGraph
-     *
-     * Called internally for updating the vizualization scene for the graph
-     * building procedure
-     */
-    inline void visualizeGraph(const GRAPH_t& gr);
-    /**
-     * GRAPH_t getter function - return reference to own graph
-     * Handy function for visualization, printing purposes
-     */
-    const GRAPH_t& getGraph() const { return m_graph; } 
-
-  private:
-    // Private function definitions
-    //////////////////////////////////////////////////////////////
-
-    //static void optimization_feedback(
-        //const GRAPH_t& graph,
-        //const size_t iter,
-        //const size_t max_iter,
-        //const double cur_sq_error )
-    //{
-      //m_log_sq_err_evolution.push_back(std::log(cur_sq_error));
-      //if ((iter % 100)==0)
-        //cout << "Progress: " << iter << " / " << max_iter << ", total sq err = " << cur_sq_error << endl;
-    //}
+		typedef typename GRAPH_t::constraint_t constraint_t;
+		typedef typename GRAPH_t::constraint_t::type_value pose_t; // type of underlying poses (2D/3D)
+		typedef CMatrixFixedNumeric<double,
+						constraint_t::state_length, constraint_t::state_length> InfMat;
 
 
-    /**
-     * General initialization method to call from the different Ctors
-     */
-    void initGraphSlamEngine();
-    /**
-     * Initialize (clean up and create new files) the output directory
-     * Also provides cmd line arguements for the user to choose the desired
-     * action.
-     * \sa GraphSlamEngine_t::initResultsFile
-     */
-    void initOutputDir();
-    /**
-     * Method to automate the creation of the output result files
-     * Open and write an introductory message using the provided fname
-     */
-    void initResultsFile(const string& fname);
-    /**
-     * GraphSlamEngine_t::getICPEdge
-     *
-     * Align the laser scans of the given poses and compute a constraint
-     * between them. Return the goodness of the ICP operation and let the user
-     * decide if they want to keep it as an edge or not
-     */
-    inline double getICPEdge(const TNodeID& from, const TNodeID& to, constraint_t *rel_edge );
-    /**
-     * assignTextMessageParameters
-     *
-     * Assign the next available offset_y and text_index for the textMessage under
-     * construction. Then increment the respective current counters
-     */
-    inline void assignTextMessageParameters(double* offset_y, int* text_index) {
-      *offset_y = m_curr_offset_y;
-      m_curr_offset_y += kOffsetYStep;
+		// Ctors, Dtors
+		//////////////////////////////////////////////////////////////
+		GraphSlamEngine_t(const string& config_file,
+				CDisplayWindow3D* win = NULL,
+				string rawlog_fname = "");
+		~GraphSlamEngine_t();
 
-      *text_index = m_curr_text_index;
-      m_curr_text_index += kIndexTextStep;
-    }
-    inline void updateCurPosViewport(const GRAPH_t& gr);
-    inline void updateTotalOdometryDistance() const;
+		// Public function definitions
+		//////////////////////////////////////////////////////////////
+		/**
+		 * Wrapper fun around the GRAPH_t corresponding method
+		 */
+		void saveGraph() const {
+			MRPT_START
 
-    // VARIABLES
-    //////////////////////////////////////////////////////////////
+			if (!m_has_read_config) {
+				THROW_EXCEPTION("Config file has not been provided yet.\nExiting...");
+			}
+			string fname = m_output_dir_fname + "/" + m_save_graph_fname;
+			saveGraph(fname);
 
-    // the graph object to be built and optimized
-    GRAPH_t m_graph;
+			MRPT_END
+		}
+		/**
+		 * Wrapper fun around the GRAPH_t corresponding method
+		 */
+		void saveGraph(const string& fname) const {
+			MRPT_START
 
-    /**
-     * Problem parameters.
-     * Most are imported from a .ini config file
-     * \sa GraphSlamEngine_t::readConfigFile
-     */
-    string  m_config_fname;
+			m_graph.saveToTextFile(fname);
+			VERBOSE_COUT << "Saved graph to text file: " << fname <<
+				" successfully." << endl;
 
-    string  m_rawlog_fname;
-    string  m_output_dir_fname;
-    bool    m_user_decides_about_output_dir;
-    bool    m_do_debug;
-    string  m_debug_fname;
-    string  m_save_graph_fname;
+			MRPT_END
+		}
+		/**
+		 * Read the configuration file specified and fill in the corresponding
+		 * class members
+		 */
+	 void readConfigFile(const string& fname);
+		/**
+		 * Print the problem parameters (usually fetched from a configuration file)
+		 * to the console for verification
+		 *
+		 * \sa GraphSlamEngine_t::parseRawlogFile
+		 */
+		void printProblemParams() const;
+		/**
+		 * TODO - Make this a function template so that it can handle camera
+		 * images, laser scan files, etc.
+		 * Reads the file provided and builds the graph
+		 */
+		void parseRawlogFile();
+		/**
+		 * GraphslamEngine_t::optmizeGraph
+		 *
+		 * Optimize the under-construction graph
+		 */
+		inline void optimizeGraph(GRAPH_t graph) {
+			MRPT_START
 
-    bool    m_do_pose_graph_only;
-    string  m_optimizer;
+			cout << "Executing the graph optimization" << endl;
 
-    string  m_loop_closing_alg;
-    double  m_loop_closing_min_nodeid_diff;
+			// TODO: read these throught the .ini file
+			// params for the optimization of the graph
+			TParametersDouble  optimization_params;
+			optimization_params["verbose"]	= 0;
+			optimization_params["profiler"] = 0;
+			optimization_params["max_iterations"] = 500;
+			optimization_params["scale_hessian"] = 0.1;  // If <1, will "exagerate" the scale of the gradient and, normally, will converge much faster.
+			optimization_params["tau"] = 1e-3;
+			// e2: Lev-marq algorithm iteration stopping criterion #2: |delta_incr| < e2*(x_norm+e2)
+			//optimization_params["e1"] = 1e-6;
+			//optimization_params["e2"] = 1e-6;
 
-    string  m_decider_alg;
-    double  m_distance_threshold;
-    double  m_angle_threshold;
+			graphslam::TResultInfoSpaLevMarq	levmarq_info;
 
-    bool    m_has_read_config;
+			// Execute the optimization
+			graphslam::optimize_graph_spa_levmarq(
+					graph,
+					levmarq_info,
+					NULL,  // List of nodes to optimize. NULL -> all but the root node.
+					optimization_params,
+					NULL); // functor feedback
+					//&optimization_feedback);
 
-    // use lookup table to hold the optimizer codes
-    map<string, int> optimizer_codes; 
+			MRPT_END
+		}
 
-    /** 
-     * FileStreams
-     * variable that keeps track of the out fstreams so that they can be closed 
-     * (if still open) in the class Dtor.
-     */
-    fstreams m_out_streams;
+		/**
+		 * GraphSlamEngine_t::visualizeGraph
+		 *
+		 * Called internally for updating the vizualization scene for the graph
+		 * building procedure
+		 */
+		inline void visualizeGraph(const GRAPH_t& gr);
+		/**
+		 * GRAPH_t getter function - return reference to own graph
+		 * Handy function for visualization, printing purposes
+		 */
+		const GRAPH_t& getGraph() const { return m_graph; }
 
-    // visualization objects
-    CDisplayWindow3D* m_win;
+	private:
+		// Private function definitions
+		//////////////////////////////////////////////////////////////
 
-    TParametersDouble m_optimized_graph_viz_params;
-    bool m_visualize_optimized_graph;
-    bool m_visualize_odometry_poses;
+		//static void optimization_feedback(
+				//const GRAPH_t& graph,
+				//const size_t iter,
+				//const size_t max_iter,
+				//const double cur_sq_error )
+		//{
+			//m_log_sq_err_evolution.push_back(std::log(cur_sq_error));
+			//if ((iter % 100)==0)
+				//cout << "Progress: " << iter << " / " << max_iter << ", total sq err = " << cur_sq_error << endl;
+		//}
 
-    /**
-     * textMessage Parameters 
-     *
-     */
-    string m_font_name;
-    int m_font_size;
 
-    // textMessage vertical text position
-    const double kOffsetYStep;
-    double m_curr_offset_y;
-    double m_offset_y_graph;
-    double m_offset_y_odometry;
-    double m_offset_y_timestamp;
+		/**
+		 * General initialization method to call from the different Ctors
+		 */
+		void initGraphSlamEngine();
+		/**
+		 * Initialize (clean up and create new files) the output directory
+		 * Also provides cmd line arguements for the user to choose the desired
+		 * action.
+		 * \sa GraphSlamEngine_t::initResultsFile
+		 */
+		void initOutputDir();
+		/**
+		 * Method to automate the creation of the output result files
+		 * Open and write an introductory message using the provided fname
+		 */
+		void initResultsFile(const string& fname);
+		/**
+		 * GraphSlamEngine_t::getICPEdge
+		 *
+		 * Align the laser scans of the given poses and compute a constraint
+		 * between them. Return the goodness of the ICP operation and let the user
+		 * decide if they want to keep it as an edge or not
+		 */
+		inline double getICPEdge(const TNodeID& from, const TNodeID& to, constraint_t *rel_edge );
+		/**
+		 * assignTextMessageParameters
+		 *
+		 * Assign the next available offset_y and text_index for the textMessage under
+		 * construction. Then increment the respective current counters
+		 */
+		inline void assignTextMessageParameters(double* offset_y, int* text_index) {
+			*offset_y = m_curr_offset_y;
+			m_curr_offset_y += kOffsetYStep;
 
-    // textMessage index
-    const int kIndexTextStep;
-    int m_curr_text_index;
-    int m_text_index_graph;
-    int m_text_index_odometry;
-    int m_text_index_timestamp;
+			*text_index = m_curr_text_index;
+			m_curr_text_index += kIndexTextStep;
+		}
+		inline void updateCurPosViewport(const GRAPH_t& gr);
+		inline void updateTotalOdometryDistance() const;
 
-    // instance to keep track of all the edges + visualization related
-    // functions
-    EdgeCounter_t m_edge_counter;
+		// VARIABLES
+		//////////////////////////////////////////////////////////////
 
-    // std::maps to store information about the graph(s)
-    map<const GRAPH_t*, string> graph_to_name;
-    map<const GRAPH_t*, TParametersDouble*> graph_to_viz_params;
+		// the graph object to be built and optimized
+		GRAPH_t m_graph;
 
-    // odometry visualization
-    vector<pose_t*> m_odometry_poses;
+		/**
+		 * Problem parameters.
+		 * Most are imported from a .ini config file
+		 * \sa GraphSlamEngine_t::readConfigFile
+		 */
+		string	m_config_fname;
 
-    bool m_is3D;
-    TNodeID m_nodeID_max;
+		string	m_rawlog_fname;
+		string	m_output_dir_fname;
+		bool		m_user_decides_about_output_dir;
+		bool		m_do_debug;
+		string	m_debug_fname;
+		string	m_save_graph_fname;
 
-    // ICP configuration
-    float m_ICP_goodness_thres;
-    int m_prev_nodes_for_ICP; // add ICP constraints with m_prev_nodes_for_ICP nodes back
-    map<TNodeID, CObservation2DRangeScanPtr> m_nodes_to_laser_scans;
-    CICP m_ICP;
+		bool		m_do_pose_graph_only;
+		string	m_optimizer;
 
-    // graph optimization
+		string	m_loop_closing_alg;
+		double	m_loop_closing_min_nodeid_diff;
 
-    // Container to handle the propagation of the square root error of the problem
-    vector<double> m_log_sq_err_evolution;
+		string	m_decider_alg;
+		double	m_distance_threshold;
+		double	m_angle_threshold;
 
-    pose_t m_curr_estimated_pose;
+		bool		m_has_read_config;
+
+		// use lookup table to hold the optimizer codes
+		map<string, int> optimizer_codes;
+
+		/**
+		 * FileStreams
+		 * variable that keeps track of the out fstreams so that they can be closed
+		 * (if still open) in the class Dtor.
+		 */
+		fstreams m_out_streams;
+
+		// visualization objects
+		CDisplayWindow3D* m_win;
+
+		TParametersDouble m_optimized_graph_viz_params;
+		bool m_visualize_optimized_graph;
+		bool m_visualize_odometry_poses;
+
+		/**
+		 * textMessage Parameters
+		 *
+		 */
+		string m_font_name;
+		int m_font_size;
+
+		// textMessage vertical text position
+		const double kOffsetYStep;
+		double m_curr_offset_y;
+		double m_offset_y_graph;
+		double m_offset_y_odometry;
+		double m_offset_y_timestamp;
+
+		// textMessage index
+		const int kIndexTextStep;
+		int m_curr_text_index;
+		int m_text_index_graph;
+		int m_text_index_odometry;
+		int m_text_index_timestamp;
+
+		// instance to keep track of all the edges + visualization related
+		// functions
+		EdgeCounter_t m_edge_counter;
+
+		// std::maps to store information about the graph(s)
+		map<const GRAPH_t*, string> graph_to_name;
+		map<const GRAPH_t*, TParametersDouble*> graph_to_viz_params;
+
+		// odometry visualization
+		vector<pose_t*> m_odometry_poses;
+
+		bool m_is3D;
+		TNodeID m_nodeID_max;
+
+		// ICP configuration
+		float m_ICP_goodness_thres;
+		int m_prev_nodes_for_ICP; // add ICP constraints with m_prev_nodes_for_ICP nodes back
+		map<TNodeID, CObservation2DRangeScanPtr> m_nodes_to_laser_scans;
+		CICP m_ICP;
+
+		// graph optimization
+
+		// Container to handle the propagation of the square root error of the problem
+		vector<double> m_log_sq_err_evolution;
+
+		pose_t m_curr_estimated_pose;
 
 };
 
--- graphslam-engine/graphslam-engine_app.cpp
+++ graphslam-engine/graphslam-engine_app.cpp
@@ -10,12 +10,11 @@
 
 #include <mrpt/obs/CRawlog.h>
 #include <mrpt/graphslam.h>
-//#include <mrpt/graphs.h>
 #include <mrpt/gui/CDisplayWindow3D.h>
 #include <mrpt/system/filesystem.h>
 #include <mrpt/system/os.h>
 #include <mrpt/utils/CLoadableOptions.h>
-#include <mrpt/opengl/CPlanarLaserScan.h> 
+#include <mrpt/opengl/CPlanarLaserScan.h>
 #include <mrpt/poses/CPoses2DSequence.h>
 #include <mrpt/poses/CPosePDF.h>
 #include <mrpt/graphs/CNetworkOfPoses.h>
@@ -38,31 +37,31 @@ using namespace mrpt::utils;
 
 using namespace std;
 
-#define VERBOSE_COUT  if (verbose) std::cout << "[graphslam_engine] "
+#define VERBOSE_COUT	if (verbose) std::cout << "[graphslam_engine] "
 
-/** 
+/**
  * Forward declaration
  * display_graph function template provided in display_graph.cpp
  */
 template <class GRAPHTYPE> void display_graph(const GRAPHTYPE & g);
 
 
-/* 
+/*
  * Command line options initialization
  */
 
-// 
-TCLAP::CmdLine cmd(/*output message = */ " graphslam-engine - Part of the MRPT\n", 
-    /* delimeter = */ ' ', /* version = */ MRPT_getVersion().c_str());
+//
+TCLAP::CmdLine cmd(/*output message = */ " graphslam-engine - Part of the MRPT\n",
+		/* delimeter = */ ' ', /* version = */ MRPT_getVersion().c_str());
 
-TCLAP::ValueArg<string> arg_ini_file(/*flag = */ "i", /*name = */ "ini_file", 
-    /*desc = */ ".ini configuration file", /* required = */ false,
-    /* default value = */ "", /*typeDesc = */ "config.ini", /*parser = */ cmd);
-TCLAP::ValueArg<string> arg_rawlog_file( "r", "rawlog", 
-    "Rawlog dataset file",  false, "", "captured_observations.rawlog", cmd);
+TCLAP::ValueArg<string> arg_ini_file(/*flag = */ "i", /*name = */ "ini_file",
+		/*desc = */ ".ini configuration file", /* required = */ false,
+		/* default value = */ "", /*typeDesc = */ "config.ini", /*parser = */ cmd);
+TCLAP::ValueArg<string> arg_rawlog_file( "r", "rawlog",
+		"Rawlog dataset file",	false, "", "captured_observations.rawlog", cmd);
 TCLAP::SwitchArg arg_do_demo(/*flag = */ "d", /*name = */ "demo",
-    /*desc = */ "Default file for demonstration purposes", 
-    /* parser = */ cmd, /* default = */ false);
+		/*desc = */ "Default file for demonstration purposes",
+		/* parser = */ cmd, /* default = */ false);
 
 
 
@@ -71,90 +70,89 @@ TCLAP::SwitchArg arg_do_demo(/*flag = */ "d", /*name = */ "demo",
  */
 int main(int argc, char **argv)
 {
-  bool showHelp    = argc>1 && !os::_strcmp(argv[1],"--help");
-  bool showVersion = argc>1 && !os::_strcmp(argv[1],"--version");
- 
-  try {
+	bool showHelp		 = argc>1 && !os::_strcmp(argv[1],"--help");
+	bool showVersion = argc>1 && !os::_strcmp(argv[1],"--version");
 
-    /** 
-     * Command line arguments parsing
-     */
-    // validation
-    if (!cmd.parse( argc, argv ) ||  showVersion || showHelp) {
-      return 0;
-    }
-    else if (argc == 1 || (!arg_do_demo.isSet() && !arg_ini_file.isSet())) {
-      THROW_EXCEPTION("Neither .ini file or demo option was specified."
-          << "Use -h [--help] flag for list of available options"
-          << "Exiting..");
-    }
-    else if (arg_do_demo.isSet()) {
-      if (arg_rawlog_file.isSet() || arg_ini_file.isSet()) {
-        THROW_EXCEPTION("-d [--demo] flag cannot be specified alongside other flags"
-            << "Use -h [--help] flag for list of available options"
-            << "Exiting..");
-      }
-    }
-    // fetching the .ini file
-    string config_fname;
-    if ( arg_do_demo.isSet() ) {
-      VERBOSE_COUT << "Using the demo file: " << config_fname << endl;
-      config_fname = "../default_config.ini";
-    }
-    else {
-      config_fname = arg_ini_file.getValue();
-      //VERBOSE_COUT << "Rawlog file: " << config_fname << endl;
-    }
+	try {
 
-    /**
-     * Objects initialization
-     */
+		/**
+		 * Command line arguments parsing
+		 */
+		// validation
+		if (!cmd.parse( argc, argv ) ||  showVersion || showHelp) {
+			return 0;
+		}
+		else if (argc == 1 || (!arg_do_demo.isSet() && !arg_ini_file.isSet())) {
+			THROW_EXCEPTION("Neither .ini file or demo option was specified."
+					<< "Use -h [--help] flag for list of available options"
+					<< "Exiting..");
+		}
+		else if (arg_do_demo.isSet()) {
+			if (arg_rawlog_file.isSet() || arg_ini_file.isSet()) {
+				THROW_EXCEPTION("-d [--demo] flag cannot be specified alongside other flags"
+						<< "Use -h [--help] flag for list of available options"
+						<< "Exiting..");
+			}
+		}
+		// fetching the .ini file
+		string config_fname;
+		if ( arg_do_demo.isSet() ) {
+			VERBOSE_COUT << "Using the demo file: " << config_fname << endl;
+			config_fname = "../default_config.ini";
+		}
+		else {
+			config_fname = arg_ini_file.getValue();
+			//VERBOSE_COUT << "Rawlog file: " << config_fname << endl;
+		}
 
-    // Initialize the visualization objects
-    CDisplayWindow3D	win("Graphslam building procedure",800, 600);
-    win.setCameraElevationDeg(75);
+		/**
+		 * Objects initialization
+		 */
 
-
-    // Initialize the class
-    string rawlog_fname;
-    if (arg_rawlog_file.isSet()) {
-      rawlog_fname = arg_rawlog_file.getValue();
-      //VERBOSE_COUT << "Explicitly specified rawlog file:" << rawlog_fname << endl;;
-    }
-    GraphSlamEngine_t<CNetworkOfPoses2DInf> g_engine(config_fname, &win, rawlog_fname);
-
-    
-    g_engine.parseRawlogFile();
-
-    // saving the graph to external file
-    g_engine.saveGraph();
-
-    // TODO add "keylogger" function to know when to exit..
-
-    while (win.isOpen()) {
-      ;;
-    }
+		// Initialize the visualization objects
+		CDisplayWindow3D	win("Graphslam building procedure",800, 600);
+		win.setCameraElevationDeg(75);
 
 
-  }
-  catch (exception& e) {
-    setConsoleColor(CONCOL_RED, true);
-    cerr << "Program finished with an exception!" << endl;
-    setConsoleColor(CONCOL_NORMAL, true);
+		// Initialize the class
+		string rawlog_fname;
+		if (arg_rawlog_file.isSet()) {
+			rawlog_fname = arg_rawlog_file.getValue();
+			//VERBOSE_COUT << "Explicitly specified rawlog file:" << rawlog_fname << endl;;
+		}
+		GraphSlamEngine_t<CNetworkOfPoses2DInf> g_engine(config_fname, &win, rawlog_fname);
 
-    cerr << e.what() << endl;
+		g_engine.parseRawlogFile();
 
-    mrpt::system::pause();
-    return -1;
-  }
-  catch (...) {
-    setConsoleColor(CONCOL_RED, true);
-    cerr << "Program finished for an untyped exception!!" << endl;
-    setConsoleColor(CONCOL_NORMAL, true);
+		// saving the graph to external file
+		g_engine.saveGraph();
 
-    mrpt::system::pause();
-    return -1;
-  }
+		// TODO add "keylogger" function to know when to exit..
 
-  return 0;
+		while (win.isOpen()) {
+			;;
+		}
+
+
+	}
+	catch (exception& e) {
+		setConsoleColor(CONCOL_RED, true);
+		cerr << "Program finished with an exception!" << endl;
+		setConsoleColor(CONCOL_NORMAL, true);
+
+		cerr << e.what() << endl;
+
+		mrpt::system::pause();
+		return -1;
+	}
+	catch (...) {
+		setConsoleColor(CONCOL_RED, true);
+		cerr << "Program finished for an untyped exception!!" << endl;
+		setConsoleColor(CONCOL_NORMAL, true);
+
+		mrpt::system::pause();
+		return -1;
+	}
+
+	return 0;
 }
