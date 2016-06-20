bb8ae9ead9892ca17968fcf67562fa6942d874a0
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2016-06-20 09:19:31 +0200

    Progress with holo PTG; further refactoring of common PTG methods

--- libs/nav/src/tpspace/CPTG_Holo_Blend.cpp
+++ libs/nav/src/tpspace/CPTG_Holo_Blend.cpp
@@ -19,6 +19,22 @@ using namespace mrpt::system;
 
 IMPLEMENTS_SERIALIZABLE(CPTG_Holo_Blend,CParameterizedTrajectoryGenerator,mrpt::nav)
 
+/*
+Closed-form PTG. Parameters: 
+- Initial velocity vector (xip, yip)
+- Target velocity vector depends on \alpha: xfp = V_MAX*cos(alpha), yfp = V_MAX*sin(alpha)
+- T_ramp time for velocity interpolation (xip,yip) -> (xfp, yfp)
+- W_MAX: Rotational velocity for robot heading forwards.
+
+Number of steps "d" for each PTG path "k": 
+- Step = time increment PATH_TIME_STEP
+
+
+*/
+
+const double PATH_TIME_STEP = 10e-3;   // 10 ms
+
+
 CPTG_Holo_Blend::CPTG_Holo_Blend() : 
 	T_ramp(-1.0),
 	V_MAX(-1.0), 
@@ -46,6 +62,12 @@ void CPTG_Holo_Blend::setParams(const mrpt::utils::CConfigFileBase &cfg,const st
 	this->V_MAX   = cfg.read_double  (sSection, sKeyPrefix+std::string("v_max_mps"), .0, true );
 	this->W_MAX   = mrpt::utils::DEG2RAD( cfg.read_double  (sSection, sKeyPrefix+std::string("w_max_dps"), .0, true ) );
 	this->turningRadiusReference   = cfg.read_double  (sSection, sKeyPrefix+std::string("turningRadiusReference"), turningRadiusReference);
+
+	// Initial velocity, for debugging (in runtime this will be update to the current robot vel)
+	curVelLocal.vx = cfg.read_double  (sSection, sKeyPrefix+std::string("vxi"), curVelLocal.vx);
+	curVelLocal.vy = cfg.read_double  (sSection, sKeyPrefix+std::string("vyi"), curVelLocal.vy);
+
+	CParameterizedTrajectoryGenerator::setParamsCommon(cfg,sSection,sKeyPrefix);
 }
 
 std::string CPTG_Holo_Blend::getDescription() const
@@ -100,6 +122,11 @@ void CPTG_Holo_Blend::initialize(const std::string & cacheFilename, const bool v
 	ASSERT_(T_ramp>0);
 	ASSERT_(V_MAX>0);
 	ASSERT_(W_MAX>0);
+	ASSERT_(m_alphaValuesCount>0);
+
+	// DEBUG:
+	//debugDumpInFiles("1");
+
 }
 
 void CPTG_Holo_Blend::deinitialize()
@@ -119,34 +146,89 @@ void CPTG_Holo_Blend::directionToMotionCommand( uint16_t k, std::vector<double> 
 	cmd_vel[3] = W_MAX;
 }
 
-void CPTG_Holo_Blend::renderPathAsSimpleLine(const uint16_t k,mrpt::opengl::CSetOfLines &gl_obj,const float decimate_distance,const float max_path_distance) const
-{
-	THROW_EXCEPTION("todo");
-	MRPT_TODO("impl");
-}
-
 size_t CPTG_Holo_Blend::getPathStepCount(uint16_t k) const
 {
-	THROW_EXCEPTION("todo");
 	MRPT_TODO("impl");
+	return 600;
 }
 
 void CPTG_Holo_Blend::getPathPose(uint16_t k, uint16_t step, mrpt::math::TPose2D &p) const
 {
-	THROW_EXCEPTION("todo");
-	MRPT_TODO("impl");
+	const double t = PATH_TIME_STEP*step;
+	const double dir = CParameterizedTrajectoryGenerator::index2alpha(k);
+
+	const double TR2_ = 1.0/(2*T_ramp);
+	const double vxf = V_MAX * cos(dir), vyf = V_MAX * sin(dir);
+	const double vxi = curVelLocal.vx, vyi = curVelLocal.vy;
+
+	// Translational part:
+	if (t<T_ramp)
+	{
+		p.x   = vxi * t + t*t * TR2_ * (vxf-vxi);
+		p.y   = vyi * t + t*t * TR2_ * (vyf-vyi);
+	}
+	else
+	{
+		p.x   = T_ramp *0.5*(vxi+vxf) + (t-T_ramp) * vxf;
+		p.y   = T_ramp *0.5*(vyi+vyf) + (t-T_ramp) * vyf;
+	}
+
+	// Rotational part:
+	const double T_rot = std::abs(dir)/W_MAX;
+	if (t<T_rot)
+	     p.phi = t*dir/T_rot;
+	else p.phi = dir;
 }
 
 double CPTG_Holo_Blend::getPathDist(uint16_t k, uint16_t step) const
 {
-	THROW_EXCEPTION("todo");
-	MRPT_TODO("impl");
+	const double t = PATH_TIME_STEP*step;
+	const double dir = CParameterizedTrajectoryGenerator::index2alpha(k);
+
+	const double TR2_ = 1.0/(2*T_ramp);
+	const double vxf = V_MAX * cos(dir), vyf = V_MAX * sin(dir);
+	const double vxi = curVelLocal.vx, vyi = curVelLocal.vy;
+
+	const double k2 = (vxf-vxi)*TR2_;
+	const double k4 = (vyf-vyi)*TR2_;
+
+	if (t<T_ramp)
+	{
+		const double num1 = vxi+2*t*k2;
+		const double num2 = vyi+2*t*k4;
+		const double dist_trans = std::sqrt( ( num1*num1*num1/k2 + num2*num2*num2/k4 )/6.0  );
+		return dist_trans;
+	}
+	else
+	{
+		const double num1 = vxi+2*T_ramp*k2;
+		const double num2 = vyi+2*T_ramp*k4;
+		const double dist_trans = std::sqrt( ( num1*num1*num1/k2 + num2*num2*num2/k4 )/6.0  ) + (t-T_ramp) * V_MAX;
+		return dist_trans;
+	}
 }
 
 bool CPTG_Holo_Blend::getPathStepForDist(uint16_t k, double dist, uint16_t &out_step) const
 {
-	THROW_EXCEPTION("todo");
-	MRPT_TODO("impl");
+	const double dir = CParameterizedTrajectoryGenerator::index2alpha(k);
+
+	const double TR2_ = 1.0/(2*T_ramp);
+	const double vxf = V_MAX * cos(dir), vyf = V_MAX * sin(dir);
+	const double vxi = curVelLocal.vx, vyi = curVelLocal.vy;
+
+	const double k2 = (vxf-vxi)*TR2_;
+	const double k4 = (vyf-vyi)*TR2_;
+
+	// 3 roots for possible solutions within 0<t<t_ramp:
+	{
+		double r[3];
+		const double d = dist;
+		r[0] = (pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0))*1.0/pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(-1.0/2.7E1)+sqrt(-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0),3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(1.0/2.7E1)+(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)-(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),2.0))-(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)+(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),1.0/3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(-1.0/2.7E1)+sqrt(-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0),3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(1.0/2.7E1)+(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)-(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),2.0))-(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)+(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),1.0/3.0)-((k2*k2)*k4*vxi*4.0+k2*(k4*k4)*vyi*4.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0);
+		r[1] = (pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0))*1.0/pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(-1.0/2.7E1)+sqrt(-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0),3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(1.0/2.7E1)+(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)-(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),2.0))-(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)+(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),1.0/3.0)*(-1.0/2.0)-sqrt(3.0)*((pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0))*1.0/pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(-1.0/2.7E1)+sqrt(-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0),3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(1.0/2.7E1)+(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)-(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),2.0))-(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)+(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),1.0/3.0)-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(-1.0/2.7E1)+sqrt(-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0),3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(1.0/2.7E1)+(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)-(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),2.0))-(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)+(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),1.0/3.0))*5.0E-1*sqrt(-1.0)-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(-1.0/2.7E1)+sqrt(-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0),3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(1.0/2.7E1)+(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)-(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),2.0))-(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)+(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),1.0/3.0)*(1.0/2.0)-((k2*k2)*k4*vxi*4.0+k2*(k4*k4)*vyi*4.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0);
+		r[2] = (pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0))*1.0/pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(-1.0/2.7E1)+sqrt(-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0),3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(1.0/2.7E1)+(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)-(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),2.0))-(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)+(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),1.0/3.0)*(-1.0/2.0)+sqrt(3.0)*((pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0))*1.0/pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(-1.0/2.7E1)+sqrt(-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0),3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(1.0/2.7E1)+(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)-(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),2.0))-(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)+(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),1.0/3.0)-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(-1.0/2.7E1)+sqrt(-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0),3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(1.0/2.7E1)+(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)-(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),2.0))-(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)+(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),1.0/3.0))*5.0E-1*sqrt(-1.0)-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(-1.0/2.7E1)+sqrt(-pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,2.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/9.0)-(k2*k4*(vxi*vxi)*2.0+k2*k4*(vyi*vyi)*2.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0),3.0)+pow(pow((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1,3.0)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,3.0)*(1.0/2.7E1)+(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)-(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),2.0))-(k4*(vxi*vxi*vxi)*(1.0/2.0)+k2*(vyi*vyi*vyi)*(1.0/2.0)-(d*d)*k2*k4*3.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0)+(k2*k4*(vxi*vxi)*6.0+k2*k4*(vyi*vyi)*6.0)*((k2*k2)*k4*vxi*1.2E1+k2*(k4*k4)*vyi*1.2E1)*1.0/pow(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0,2.0)*(1.0/6.0),1.0/3.0)*(1.0/2.0)-((k2*k2)*k4*vxi*4.0+k2*(k4*k4)*vyi*4.0)/(k2*(k4*k4*k4)*8.0+(k2*k2*k2)*k4*8.0);
+	}
+ 
+	return 0;
 }
 
 
--- libs/nav/src/tpspace/CParameterizedTrajectoryGenerator.cpp
+++ libs/nav/src/tpspace/CParameterizedTrajectoryGenerator.cpp
@@ -11,6 +11,9 @@
 
 #include <mrpt/utils/CStream.h>
 #include <mrpt/nav/tpspace/CParameterizedTrajectoryGenerator.h>
+#include <mrpt/opengl/CSetOfLines.h>
+#include <mrpt/system/filesystem.h>
+#include <mrpt/system/os.h>
 
 using namespace mrpt::nav;
 
@@ -62,4 +65,93 @@ uint16_t CParameterizedTrajectoryGenerator::alpha2index( double alpha ) const
 	if (k<0) k=0;
 	if (k>=m_alphaValuesCount) k=m_alphaValuesCount-1;
 	return (uint16_t)k;
+}
+
+void CParameterizedTrajectoryGenerator::renderPathAsSimpleLine(
+	const uint16_t k, 
+	mrpt::opengl::CSetOfLines &gl_obj, 
+	const float decimate_distance, 
+	const float max_path_distance) const
+{
+	const size_t nPointsInPath = getPathStepCount(k);
+
+	// Decimate trajectories: we don't need centimeter resolution!
+	float last_added_dist = 0.0f;
+	for (size_t n=0;n<nPointsInPath;n++)
+	{
+		const float d = this->getPathDist(k, n); // distance thru path "k" until timestep "n"
+
+		if (d<last_added_dist+decimate_distance && n!=0)
+			continue; // skip: decimation
+
+		last_added_dist = d;
+
+		mrpt::math::TPose2D p;
+		this->getPathPose(k, n, p);
+
+		if (gl_obj.empty())
+		     gl_obj.appendLine(0,0,0, p.x, p.y,0);
+		else gl_obj.appendLineStrip(p.x, p.y,0);
+
+		// Draw the TP only until we reach the target of the "motion" segment:
+		if (max_path_distance!=0.0f && d>=max_path_distance) break;
+	}
+}
+
+bool CParameterizedTrajectoryGenerator::debugDumpInFiles( const std::string &ptg_name ) const 
+{
+	using namespace mrpt::system;
+	using namespace std;
+
+	const char *sPath = CParameterizedTrajectoryGenerator::OUTPUT_DEBUG_PATH_PREFIX.c_str();
+	
+	mrpt::system::createDirectory( sPath );
+	mrpt::system::createDirectory( mrpt::format("%s/PTGs",sPath) );
+
+	const string sFilTxt_x   = mrpt::format("%s/PTGs/PTG%s_x.txt",sPath,ptg_name.c_str() );
+	const string sFilTxt_y   = mrpt::format("%s/PTGs/PTG%s_y.txt",sPath,ptg_name.c_str() );
+	const string sFilTxt_phi = mrpt::format("%s/PTGs/PTG%s_phi.txt",sPath,ptg_name.c_str() );
+	const string sFilTxt_t   = mrpt::format("%s/PTGs/PTG%s_t.txt",sPath,ptg_name.c_str() );
+	const string sFilTxt_d   = mrpt::format("%s/PTGs/PTG%s_d.txt",sPath,ptg_name.c_str() );
+
+	ofstream fx(sFilTxt_x.c_str());  if (!fx.is_open()) return false;
+	ofstream fy(sFilTxt_y.c_str());  if (!fy.is_open()) return false;
+	ofstream fp(sFilTxt_phi.c_str());if (!fp.is_open()) return false;
+	ofstream fd(sFilTxt_d.c_str());  if (!fd.is_open()) return false;
+
+	const size_t nPaths = getAlphaValuesCount();
+
+	// Text version:
+	fx << "% PTG data file for 'x'. Each row is the trajectory for a different 'alpha' parameter value." << endl;
+	fy << "% PTG data file for 'y'. Each row is the trajectory for a different 'alpha' parameter value." << endl;
+	fp << "% PTG data file for 'phi'. Each row is the trajectory for a different 'alpha' parameter value." << endl;
+	fd << "% PTG data file for 'd'. Each row is the trajectory for a different 'alpha' parameter value." << endl;
+
+	vector<size_t> path_length(nPaths);
+	for (size_t k=0;k<nPaths;k++)
+		path_length[k] = getPathStepCount(k);
+
+	size_t maxPoints=0;
+	for (size_t k=0;k<nPaths;k++)
+		maxPoints = max( maxPoints, path_length[k] );
+
+	for (size_t k=0;k<nPaths;k++)
+	{
+		for (size_t n=0;n< maxPoints;n++)
+		{
+			const size_t nn = std::min( n, path_length[k]-1 );
+			mrpt::math::TPose2D p;
+			this->getPathPose(k,nn, p);
+			fx << p.x << " ";
+			fy << p.y << " ";
+			fp << p.phi << " ";
+			fd << this->getPathDist(k,nn) << " ";
+		}
+		fx << endl;
+		fy << endl;
+		fp << endl;
+		fd << endl;
+	}
+
+	return true;
 }
