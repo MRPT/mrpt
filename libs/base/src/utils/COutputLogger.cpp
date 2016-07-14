26b3b05bd2b02c11da3cda110cbc4216700cac26
bergercookie <nickkouk@gmail.com>
2016-07-14 20:15:52 +0300

    Revert back to UTC time getCurrentTime
    Local time is not yet implemented in MRPT for MacOS.

--- libs/base/src/utils/COutputLogger.cpp
+++ libs/base/src/utils/COutputLogger.cpp
@@ -211,7 +211,7 @@ COutputLogger_t::TMsg::TMsg(const std::string& msg_str, const COutputLogger_t& l
 
 	name = logger.getName();
 	level = logger.getCurrentLoggingLevel();
-	timestamp = mrpt::system::getCurrentLocalTime(); // fill with the current time
+	timestamp = mrpt::system::getCurrentTime(); // fill with the current time
 	body = msg_str;
 }
 COutputLogger_t::TMsg::~TMsg() { }
@@ -273,7 +273,7 @@ std::string COutputLogger_t::TMsg::getAsString() const {
 	stringstream out;
 	out.str("");
 	out << "[" << name <<  " | " << this->getLoggingLevelName(level) << " | " 
-		<< mrpt::system::timeLocalToString(timestamp) 
+		<< mrpt::system::timeToString(timestamp) 
 		<< "] " << body;
 
 	return out.str();
