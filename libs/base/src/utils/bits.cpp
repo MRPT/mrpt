150e661b6a6b59c2046b8f18076f1360a255b146
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2016-07-22 01:25:49 +0200

    use bswap compiler intrinsics if available

--- libs/base/src/utils/bits.cpp
+++ libs/base/src/utils/bits.cpp
@@ -13,6 +13,19 @@
 #include <mrpt/utils/core_defs.h>
 #include <mrpt/utils/bits.h>
 #include <cstring>
+#include <cstdlib>
+
+// These #defines from: https://github.com/boostorg/endian/blob/master/include/boost/endian/detail/intrinsic.hpp
+#ifndef __has_builtin
+  #define __has_builtin(x) 0  // Compatibility with non-clang compilers
+#endif
+#if (defined(__clang__) && __has_builtin(__builtin_bswap32) && __has_builtin(__builtin_bswap64)) \
+  || (defined(__GNUC__ ) && \
+  (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 3)))
+	#define HAVE_BSWAP_INTRINSICS
+#endif
+
+
 
 template <typename T>
 void reverseBytesInPlace_2b(T& v_in_out)
@@ -28,7 +41,14 @@ void reverseBytesInPlace_4b(T& v_in_out)
 {
 	uint32_t org;
 	::memcpy(&org, &v_in_out, sizeof(T));  // Was: = = *reinterpret_cast<uint32_t*>(&v_in_out); but caused SIGBUS in some archs
-	const uint32_t val_rev = ((org & 0xff000000) >> (3*8)) | ((org & 0x00ff0000) >> (1*8)) | ((org & 0x0000ff00) << (1*8)) | ((org & 0x000000ff) << (3*8));
+	const uint32_t val_rev =
+#if defined(_MSC_VER)
+	_byteswap_ulong(org);
+#elif defined(HAVE_BSWAP_INTRINSICS)
+	__builtin_bswap32(org);
+#else
+		((org & 0xff000000) >> (3*8)) | ((org & 0x00ff0000) >> (1*8)) | ((org & 0x0000ff00) << (1*8)) | ((org & 0x000000ff) << (3*8));
+#endif
 	::memcpy(&v_in_out, &val_rev, sizeof(T)); // This avoid deref. puned pointer warning with:   *reinterpret_cast<const T*>(&val_rev);
 }
 
@@ -37,13 +57,18 @@ void reverseBytesInPlace_8b(T& v_in_out)
 {
 	uint64_t org;
 	::memcpy(&org, &v_in_out, sizeof(T));  // Was: = *reinterpret_cast<uint64_t*>(&v_in_out); but caused SIGBUS in some archs
+#if defined(_MSC_VER)
+	uint64_t val_rev =_byteswap_uint64(org);
+#elif defined(HAVE_BSWAP_INTRINSICS)
+	uint64_t val_rev =__builtin_bswap64(org);
+#else
 	uint64_t val_rev = 0;
 	int i,j;
 	for (i=7,j=7;i>=4;i--,j-=2)
 		val_rev |= ((org & ( UINT64_C(0xff) << (i*8))) >> (j*8));
 	for (i=3,j=1;i>=0;i--,j+=2)
 		val_rev |= ((org & ( UINT64_C(0xff) << (i*8))) << (j*8));
-
+#endif
 	::memcpy(&v_in_out, &val_rev, sizeof(T)); // This avoid deref. puned pointer warning with:   *reinterpret_cast<const T*>(&val_rev);
 }
 
@@ -97,7 +122,7 @@ void mrpt::utils::reverseBytesInPlace(long double& v_in_out) {
 	std::swap(dat[0],dat[1]);
 	reverseBytesInPlace(dat[0]);
 	reverseBytesInPlace(dat[1]);
-	::memcpy(&v_in_out, &dat[0], sizeof(long double)); // This avoid deref. puned pointer warning with:   *reinterpret_cast<const T*>(&val_rev);
+	::memcpy(&v_in_out, &dat[0], sizeof(long double));
 }
 #endif
 
