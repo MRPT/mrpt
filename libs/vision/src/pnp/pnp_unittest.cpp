55e340757b0cff73fb8ad499df8eb6d1ee053f9f
mangi020 <mangi020@umn.edu>
2016-07-27 23:10:42 -0500

    Added r-pnp algorithm

--- libs/vision/src/pnp/pnp_unittest.cpp
+++ libs/vision/src/pnp/pnp_unittest.cpp
@@ -130,6 +130,48 @@ TEST(CPnP_Test, ppnp_ALGO_TEST)
     
 }
 
+TEST(CPnP_Test, rpnp_ALGO_TEST)
+{
+    int n = 6;
+
+	Eigen::Matrix3d R(3, 3);
+	Eigen::Vector3d t;
+	R << -0.3536, 0.3536, -0.8660,
+		0.9330, 0.0670, -0.3536,
+		-0.0670, -0.9330, -0.3536;
+
+	t << 20, -30, 100;
+
+	Eigen::MatrixXd obj_pts(3, n), img_pts(3, n);
+
+	obj_pts << 20, 0,  10, 15, 14, 16,
+                0, 0, -10, -5, -2, -5,
+               40, 0,   0, 24, 21, 13;
+
+	for (int i = 0; i < n; i++)
+	{
+		img_pts.col(i) = (R * obj_pts.col(i) + t);
+		img_pts.col(i) /= img_pts(2,i);
+	}
+
+	Eigen::Matrix3d R_est;
+	Eigen::Vector3d t_est;
+    
+    Eigen::MatrixXd I3 = Eigen::MatrixXd::Identity(3, 3);
+    
+    pnp::CPnP p;
+
+    Eigen::MatrixXd pose_est(6,1);
+    p.CPnP_rpnp(obj_pts, img_pts, n, I3, pose_est);
+    
+    t_est<<pose_est(0), pose_est(1), pose_est(2);
+    
+    double err_t = (t-t_est).norm();
+    
+    EXPECT_LE(err_t, 0.5);
+    
+}
+
 TEST(CPnP_Test, posit_ALGO_TEST)
 {
     int n = 6;
