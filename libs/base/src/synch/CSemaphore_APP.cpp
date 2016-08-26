49f37b00740aa95caeb2361fe4e384d814848d2a
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2016-08-27 01:56:21 +0200

    Remove support for named semaphores
    Named semaphores were actually not used in any MRPT app, and they will
    complicate the future porting of semaphores implementation towards a
    purely C++11, platform independent, implementation.

--- libs/base/src/synch/CSemaphore_APP.cpp
+++ libs/base/src/synch/CSemaphore_APP.cpp
@@ -40,33 +40,16 @@ typedef struct
 // ######################################################################
 CSemaphore::CSemaphore(
     unsigned int    initialCount,
-    unsigned int    maxCount,
-    const std::string &name )
-    :
-    m_name(name)
+    unsigned int    maxCount)
 {
   MRPT_START
 
-  if (isNamed())
-  {
-    m_data.resize( sizeof(sem_private_struct) );
-    sem_private token = m_data.getAs<sem_private>();
+	m_data.resize(sizeof(semaphore_t));
+	semaphore_t * sem = m_data.getAs<semaphore_t *>();
 
-    // Open it or create if not existing:
-    token->semid = sem_open(m_name.c_str(), O_CREAT, 0644 /* permisions */, initialCount );
-
-    if (token->semid==SEM_FAILED)
-      THROW_EXCEPTION( format("Creating semaphore (name='%s') raised error: %s",m_name.c_str(),strerror(errno) ) )
-  }
-  else
-  {
-    m_data.resize(sizeof(semaphore_t));
-    semaphore_t * sem = m_data.getAs<semaphore_t *>();
-
-    kern_return_t error = semaphore_create(mach_task_self(), sem, SYNC_POLICY_FIFO, initialCount);
-    if(error != KERN_SUCCESS)
-      THROW_EXCEPTION( format("Creating unnamed semaphore raised error: %d", error ) );
-  }
+	kern_return_t error = semaphore_create(mach_task_self(), sem, SYNC_POLICY_FIFO, initialCount);
+	if(error != KERN_SUCCESS)
+		THROW_EXCEPTION( format("Creating semaphore raised error: %d", error ) );
 
   MRPT_END
 }
@@ -78,16 +61,8 @@ CSemaphore::~CSemaphore()
 
   if (m_data.alias_count()==1)
   {
-    if(isNamed())
-    {
-      sem_private token = m_data.getAs<sem_private>();
-      sem_close(token->semid);
-    }
-    else
-    {
-      semaphore_t * sem = m_data.getAs<semaphore_t *>();
-      semaphore_destroy(mach_task_self(), *sem);
-    }
+	semaphore_t * sem = m_data.getAs<semaphore_t *>();
+	semaphore_destroy(mach_task_self(), *sem);
   }
 
   MRPT_END
@@ -98,65 +73,24 @@ bool CSemaphore::waitForSignal( unsigned int timelimit )
 {
   MRPT_START
 
-  if(isNamed())
-  {
-    struct timeb endtime;
+	semaphore_t * sem = m_data.getAs<semaphore_t *>();
 
-    const long sec = timelimit / 1000;
-    const long millisec = timelimit % 1000;
-    ftime( &endtime );
-    endtime.time += sec;
-    endtime.millitm += millisec;
-    if( endtime.millitm > 999 )
-    {
-        endtime.millitm -= 1000;
-        endtime.time++;
-    }
+	if(timelimit == 0)
+	{
+		kern_return_t result = semaphore_wait(*sem);
 
-    sem_private token = m_data.getAs<sem_private>();
+		return (result == KERN_SUCCESS);
+	}
+	else
+	{
+		mach_timespec_t ts;
+		ts.tv_sec = timelimit / 1000;
+		ts.tv_nsec = (timelimit % 1000) * 1000000;
 
-    struct timeb nowtime;
-    ftime( &nowtime );
-    int rc;
-    // Mac version: we don't have sem_timedwait()
-    if(timelimit==0)
-    {
-      // No timeout
-      rc = sem_wait(token->semid);
-      return (rc == 0); // true: all ok.
-    }
+		kern_return_t result = semaphore_timedwait(*sem, ts);
 
-    do
-    {
-      rc = sem_trywait(token->semid);
-      mrpt::system::sleep(1);
-      ftime( &nowtime );
-    }
-    while(rc != 0 && (endtime.time > nowtime.time && endtime.millitm > nowtime.millitm));
-
-    return (rc == 0); // true: all ok.
-  }
-  else
-  {
-    semaphore_t * sem = m_data.getAs<semaphore_t *>();
-
-    if(timelimit == 0)
-    {
-      kern_return_t result = semaphore_wait(*sem);
-
-      return (result == KERN_SUCCESS);
-    }
-    else
-    {
-      mach_timespec_t ts;
-      ts.tv_sec = timelimit / 1000;
-      ts.tv_nsec = (timelimit % 1000) * 1000000;
-
-      kern_return_t result = semaphore_timedwait(*sem, ts);
-
-      return (result == KERN_SUCCESS);
-    }
-  }
+		return (result == KERN_SUCCESS);
+	}
 
   MRPT_END
 }
@@ -166,24 +100,13 @@ void CSemaphore::release(unsigned int increaseCount )
 {
   MRPT_START
 
-  if(isNamed())
-  {
-    sem_private token = m_data.getAs<sem_private>();
-
-    for (unsigned int i=0;i<increaseCount;i++)
-      if (sem_post(token->semid))
-      THROW_EXCEPTION( format("Increasing count of semaphore (name='%s') raised error: %s",m_name.c_str(),strerror(errno) ) )
-  }
-  else
-  {
-    semaphore_t * sem = m_data.getAs<semaphore_t *>();
-    for(unsigned int i=0; i<increaseCount; ++i)
-    {
-      kern_return_t result = semaphore_signal(*sem);
-      if(result != KERN_SUCCESS)
-        THROW_EXCEPTION( format("Increasing count of unnamed semaphore raised error: %d", result ) );
-    }
-  }
+	semaphore_t * sem = m_data.getAs<semaphore_t *>();
+	for(unsigned int i=0; i<increaseCount; ++i)
+	{
+		kern_return_t result = semaphore_signal(*sem);
+		if(result != KERN_SUCCESS)
+		THROW_EXCEPTION( format("Increasing count of semaphore raised error: %d", result ) );
+	}
 
   MRPT_END
 }
