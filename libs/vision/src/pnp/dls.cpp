f99004387a54b18553f07280459126e3d37d9db6
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2016-08-27 10:57:50 +0200

    Fix PNP build in msvc; add standard copyright notice
    cc: @mangi020

--- libs/vision/src/pnp/dls.cpp
+++ libs/vision/src/pnp/dls.cpp
@@ -1,3 +1,14 @@
+/* +---------------------------------------------------------------------------+
+|                     Mobile Robot Programming Toolkit (MRPT)               |
+|                          http://www.mrpt.org/                             |
+|                                                                           |
+| Copyright (c) 2005-2016, Individual contributors, see AUTHORS file        |
+| See: http://www.mrpt.org/Authors - All rights reserved.                   |
+| Released under BSD License. See details in http://www.mrpt.org/License    |
++---------------------------------------------------------------------------+ */
+
+#include "vision-precomp.h"   // Precompiled headers
+
 #include <iostream>
 #include <mrpt/config.h>
 
