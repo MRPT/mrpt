ad0e70f450936cd8c411e9d6b9efc2a614cf0423
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2016-10-05 08:35:05 +0200

    CStream: Fix memory leak if an exception (e.g. EOF) is found during object deserialization. Fixes #342

--- libs/base/src/utils/CStream.cpp
+++ libs/base/src/utils/CStream.cpp
@@ -343,7 +343,7 @@ CStream& utils::operator>>(mrpt::utils::CStream&in, char *s)
 #define CSTREAM_VERBOSE     0
 
 template <bool EXISTING_OBJ>
-CSerializable* CStream::internal_ReadObject(CSerializable *existingObj)
+void CStream::internal_ReadObject(CSerializablePtr &newObj,CSerializable *existingObj)
 {
 	// Automatically register all classes when the first one is registered.
 	registerAllPendingClasses();
@@ -429,6 +429,7 @@ CSerializable* CStream::internal_ReadObject(CSerializable *existingObj)
 				THROW_EXCEPTION(msg)
 			}
 			obj = static_cast<CSerializable*>(classId->createObject());
+			newObj = CSerializablePtr(obj);
 		}
 
 		// Go on, read it:
@@ -441,8 +442,6 @@ CSerializable* CStream::internal_ReadObject(CSerializable *existingObj)
 			if (sizeof(endFlag)!=ReadBuffer( (void*)&endFlag, sizeof(endFlag) )) THROW_EXCEPTION("Cannot read object streaming version from stream!");
 			if (endFlag!=SERIALIZATION_END_FLAG) THROW_EXCEPTION_CUSTOM_MSG1("end-flag missing: There is a bug in the deserialization method of class: '%s'",strClassName.c_str());
 		}
-
-		return obj;
 	}
 	catch (std::bad_alloc &)
 	{
@@ -470,7 +469,9 @@ CSerializable* CStream::internal_ReadObject(CSerializable *existingObj)
  ---------------------------------------------------------------*/
 CSerializablePtr CStream::ReadObject()
 {
-	return CSerializablePtr( internal_ReadObject<false>() );
+	CSerializablePtr ret;
+	internal_ReadObject<false>(ret, NULL);
+	return ret;
 }
 
 /*---------------------------------------------------------------
@@ -480,7 +481,8 @@ CSerializablePtr CStream::ReadObject()
  ---------------------------------------------------------------*/
 void CStream::ReadObject(CSerializable *existingObj)
 {
-	internal_ReadObject<true>(existingObj);
+	CSerializablePtr dummy;
+	internal_ReadObject<true>(dummy,existingObj);
 }
 
 /*---------------------------------------------------------------
