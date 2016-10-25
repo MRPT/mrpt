ea7bafdfaeaed6433bf5ee8dc646230f65793f46
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2016-10-25 11:12:12 +0200

    Errors and warnings fixes for MSVC 2015

--- libs/hmtslam/src/hmtslam-precomp.cpp
+++ libs/hmtslam/src/hmtslam-precomp.cpp
@@ -7,6 +7,7 @@
    | Released under BSD License. See details in http://www.mrpt.org/License    |
    +---------------------------------------------------------------------------+ */
 
+#define MRPT_NO_WARN_BIG_HDR // Including all hdrs here is not a problem
 #include "hmtslam-precomp.h" // Precomp header
 
 
