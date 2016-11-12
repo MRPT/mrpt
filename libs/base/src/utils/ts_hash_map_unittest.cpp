97236b1349a5d62b23725d52bcc79336dd22bea8
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2016-11-12 20:04:11 +0100

    done implementation of ts_hash_map<>

--- libs/base/src/utils/ts_hash_map_unittest.cpp
+++ libs/base/src/utils/ts_hash_map_unittest.cpp
@@ -32,3 +32,42 @@ TEST(ts_hash_map, string_hash_u64) {
 	simple_test_hash_string<uint64_t>();
 }
 
+
+TEST(ts_hash_map, stdstring_key)
+{
+	mrpt::utils::ts_hash_map<std::string, double> m;
+
+	EXPECT_TRUE(m.empty());
+
+	m["numero"] = 2.3;
+	EXPECT_FALSE(m.empty());
+	m.clear();
+	EXPECT_TRUE(m.empty());
+
+	m["uno"] = 1.0;
+	m["dos"] = 2.0;
+	m["tres"] = 3.0;
+
+	EXPECT_EQ(1.0, m["uno"]);
+	EXPECT_EQ(2.0, m["dos"]);
+	EXPECT_EQ(3.0, m["tres"]);
+
+	m["tres"]++;
+	EXPECT_EQ(4.0, m["tres"]) << "Fail after ++ operator applied to reference [].";
+
+	double num = .0;
+	for (const auto &e : m)
+		num += e.second;
+	EXPECT_NEAR(num, 7.0, 1e-10) << "Fail after visiting and summing all entries";
+
+	{
+		const auto &it = m.find("pepe");
+		EXPECT_TRUE(it == m.end());
+	}
+
+	{
+		auto &it = m.find("uno");
+		EXPECT_TRUE( it->second==1.0 );
+	}
+}
+
