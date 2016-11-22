786fc8901a6eec1ef6d3947478be184833c6f544
Jose Luis Blanco <joseluisblancoc@gmail.com>
2016-11-22 15:04:11 +0100

    Upgrade pymrpt (don't use deprecated classes)

--- python/src/utils_bindings.cpp
+++ python/src/utils_bindings.cpp
@@ -11,35 +11,12 @@
 #include <mrpt/utils/CConfigFileBase.h>
 #include <mrpt/utils/CConfigFile.h>
 #include <mrpt/utils/CLoadableOptions.h>
-#include <mrpt/utils/CRobotSimulator.h>
 
 /* STD */
 #include <stdint.h>
 
 using namespace boost::python;
 using namespace mrpt::utils;
-using namespace mrpt::poses;
-
-
-// CRobotSimulator
-CPose2D CRobotSimulator_getOdometry(CRobotSimulator &self)
-{
-    CPose2D pose;
-    self.getOdometry(pose);
-    return pose;
-}
-
-CPose2D CRobotSimulator_getRealPose(CRobotSimulator &self)
-{
-    CPose2D pose;
-    self.getRealPose(pose);
-    return pose;
-}
-
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(CRobotSimulator_setDelayModelParams_overloads, setDelayModelParams, 0, 2)
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(CRobotSimulator_setOdometryErrors_overloads, setOdometryErrors, 1, 7)
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(CRobotSimulator_resetOdometry_overloads, resetOdometry, 0, 1)
-// end of CRobotSimulator
 
 // CStream
 void CStream_ReadObject(CStream& self, CSerializablePtr& obj)
@@ -139,30 +116,6 @@ void export_utils()
     // CConfigFile
     {
         class_<CConfigFile, bases<CConfigFileBase> >("CConfigFile", init<optional<std::string> >())
-        ;
-    }
-
-    // CRobotSimulator
-    {
-        class_<CRobotSimulator>("CRobotSimulator", "This class can be used to simulate the kinematics and dynamics of a differential driven planar mobile robot, including odometry errors and dynamics limitations.", init< optional<float, float> >(args("TAU", "DELAY"), "Constructor with default dynamic model-parameters"))
-            .def("setDelayModelParams", &CRobotSimulator::setDelayModelParams, CRobotSimulator_setDelayModelParams_overloads(args("TAU_delay_sec=1.8f", "CMD_delay_sec=0.3f"), "Change the model of delays used for the orders sent to the robot"))
-            .def("setOdometryErrors", &CRobotSimulator::setOdometryErrors, CRobotSimulator_setOdometryErrors_overloads( args("enabled", "Ax_err_bias", "Ax_err_std", "Ay_err_bias", "Ay_err_std", "Aphi_err_bias", "Aphi_err_std"), "Enable/Disable odometry errors. Errors in odometry are introduced per millisecond."))
-            .def("setRealPose", &CRobotSimulator::setRealPose, args("pose"), "Reset actual robot pose (inmediately, without simulating the movement along time).")
-            .def("getX", &CRobotSimulator::getX, "Read the instantaneous, error-free status of the simulated robot.")
-            .def("getY", &CRobotSimulator::getY, "Read the instantaneous, error-free status of the simulated robot.")
-            .def("getPHI", &CRobotSimulator::getPHI, "Read the instantaneous, error-free status of the simulated robot.")
-            .def("getT", &CRobotSimulator::getT, "Read the instantaneous, error-free status of the simulated robot.")
-            .def("getV", &CRobotSimulator::getV, "Read the instantaneous, error-free status of the simulated robot.")
-            .def("getW", &CRobotSimulator::getW, "Read the instantaneous, error-free status of the simulated robot.")
-            .def("setV", &CRobotSimulator::setV, args("v"), "Set actual robot velocity, error-free status of the simulated robot.")
-            .def("setW", &CRobotSimulator::setW, args("w"), "Set actual robot turnrate, error-free status of the simulated robot.")
-            .def("movementCommand", &CRobotSimulator::movementCommand, args("lin_vel", "ang_vel"), "Used to command the robot a desired movement (velocities).")
-            .def("resetStatus", &CRobotSimulator::resetStatus, "Reset all the simulator variables to 0 (All but current simulator time).")
-            .def("resetTime", &CRobotSimulator::resetTime, "Reset time counter.")
-            .def("simulateInterval", &CRobotSimulator::simulateInterval, args("dt"), "This method must be called periodically to simulate discrete time intervals.")
-            .def("resetOdometry", &CRobotSimulator::resetOdometry,  CRobotSimulator_resetOdometry_overloads(args("newOdo"), "Forces odometry to be set to a specified values."))
-            .def("getOdometry", &CRobotSimulator_getOdometry, "Reads the simulated robot odometry (this is NOT equal to the actual error-free robot coordinates).")
-            .def("getRealPose", &CRobotSimulator_getRealPose, "Reads the real robot pose.")
         ;
     }
 
