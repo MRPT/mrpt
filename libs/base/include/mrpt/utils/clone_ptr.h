f02b903d3e04065babcb011af9cbab8f2b60be71
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2016-12-14 02:29:58 +0100

    New clone_ptr_ptr<> template

--- libs/base/include/mrpt/utils/clone_ptr.h
+++ libs/base/include/mrpt/utils/clone_ptr.h
@@ -43,6 +43,63 @@ namespace mrpt
 			}
 		};
 
+		/** Wrapper to a stlplus clone smart pointer to polymorphic classes, capable of handling 
+		* copy operator, etc. making deep copies. 
+		* Example use: `clone_ptr_ptr<mrpt::poses::CPosePDFPtr>`
+		* \sa copy_ptr<T>
+		*/
+		template <typename T>
+		class clone_ptr_ptr
+		{
+		public:
+			/** Ctor from a smart pointer; makes deep copy. */
+			explicit clone_ptr_ptr(const T &ptr) {
+				m_smartptr = ptr;
+				m_smartptr.make_unique();
+			}
+			/** Default ctor; init to nullptr. */
+			clone_ptr_ptr() {}
+			/** copy ctor: makes a copy of the object via `clone()` */
+			clone_ptr_ptr(const clone_ptr_ptr & o) {
+				m_smartptr = o.m_smartptr;
+				m_smartptr.make_unique();
+			}
+			clone_ptr_ptr<T> & operator =(const clone_ptr_ptr<T> &o) {
+				if (this == &o) return *this;
+				m_smartptr = o.m_smartptr;
+				m_smartptr.make_unique();
+				return *this;
+			}
+			clone_ptr_ptr<T> & operator =(const T &o_ptr) {
+				m_smartptr = o_ptr;
+				m_smartptr.make_unique();
+				return *this;
+			}
+
+			~clone_ptr_ptr() {}
+
+			typename T::value_type * pointer() {
+				if (m_smartptr) return m_smartptr.pointer();
+				else throw std::runtime_error("dereferencing NULL clone_ptr");
+			}
+			const typename T::value_type * pointer() const {
+				if (m_smartptr) return m_smartptr.pointer();
+				else throw std::runtime_error("dereferencing NULL clone_ptr");
+			}
+
+			typename T::value_type * operator->() { return pointer(); }
+			const typename T::value_type * operator->() const { return pointer(); }
+
+			typename T::value_type& operator*(void) { return *pointer(); }
+			const typename T::value_type& operator*(void) const { return *pointer(); }
+
+			operator bool() const { return m_smartptr.present(); }
+			bool operator!(void) const { return !m_smartptr.present(); }
+
+		private:
+			T m_smartptr;
+		};
+
 		/** @} */  // end of grouping
 	} // End of namespace
 } // End of namespace
