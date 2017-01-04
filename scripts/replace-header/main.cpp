863da6fec4689217653529c88d73ec5f674d43b8
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2017-01-04 23:56:02 +0100

    Copyright notice update to 2017

--- scripts/replace-header/main.cpp
+++ scripts/replace-header/main.cpp
@@ -117,7 +117,7 @@ int main(int argc, char **argv)
 				of << "   |                     Mobile Robot Programming Toolkit (MRPT)               |\n";
 				of << "   |                          http://www.mrpt.org/                             |\n";
 				of << "   |                                                                           |\n";
-				of << "   | Copyright (c) 2005-2016, Individual contributors, see AUTHORS file        |\n";
+				of << "   | Copyright (c) 2005-2017, Individual contributors, see AUTHORS file        |\n";
 				of << "   | See: http://www.mrpt.org/Authors - All rights reserved.                   |\n";
 				of << "   | Released under BSD License. See details in http://www.mrpt.org/License    |\n";
 				of << "   +---------------------------------------------------------------------------+ */\n";
