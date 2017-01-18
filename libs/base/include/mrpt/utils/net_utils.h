097ca9778b636089a4e8c54ab229aac7f06136c9
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2017-01-18 18:00:21 +0100

    Velodyne C++ API: support HTTP post-based settings

--- libs/base/include/mrpt/utils/net_utils.h
+++ libs/base/include/mrpt/utils/net_utils.h
@@ -49,7 +49,7 @@ namespace mrpt
 			  * \param auth_pass Send a basic HTTP authorization request with the given user & password.
 			  *
 			  * \return The error or success code.
-			  * \sa mrpt::utils::vectorToBinaryFile
+			  * \sa mrpt::utils::vectorToBinaryFile, http_request()
 			  */
 			ERRORCODE_HTTP BASE_IMPEXP
 			http_get(
@@ -65,24 +65,28 @@ namespace mrpt
 				int  timeout_ms = 1000
 				);
 
-			/** Perform an HTTP GET operation (version for retrieving the data as text)
-			  * \param url Must be a simple string of the form "http://<servername>/<relative-address>".
-			  * \param port The server port, if different from 80.
-			  * \param extra_headers If provided, the given extra HTTP headers will be sent.
-			  * \param out_errormsg On exit will contain a description of the error or "Ok".
-			  * \param out_content The buffer with the retrieved data.
-			  * \param out_http_responsecode If provided, will hold the HTTP code, eg: 200, 404...
-			  * \param out_headers If provided, a copy of all the headers returned by the server will be saved here.
-			  * \param auth_user Send a basic HTTP authorization request with the given user & password.
-			  * \param auth_pass Send a basic HTTP authorization request with the given user & password.
-			  *
-			  * \return The error or success code.
-			  * \sa mrpt::utils::vectorToBinaryFile
-			  */
+			/**\ overload */
 			ERRORCODE_HTTP  BASE_IMPEXP
 			http_get(
 				const string	&url,
 				string			&out_content,
+				string			&out_errormsg,
+				int				port = 80,
+				const string	&auth_user = string(),
+				const string	&auth_pass = string(),
+				int				*out_http_responsecode = NULL,
+				mrpt::utils::TParameters<string>  *extra_headers = NULL,
+				mrpt::utils::TParameters<string>  *out_headers = NULL,
+				int  timeout_ms = 1000
+				);
+
+			/** Generic function for HTTP GET & POST methods. \sa http_get */
+			ERRORCODE_HTTP BASE_IMPEXP
+			http_request(
+				const string   & http_method,
+				const string   & http_send_content,
+				const string	&url,
+				vector_byte		&out_content,
 				string			&out_errormsg,
 				int				port = 80,
 				const string	&auth_user = string(),
