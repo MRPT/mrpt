cbfebdc5aaf6a0130bbf043c384a9e090f10784c
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2017-01-19 13:55:22 +0100

    fix bug in dyngrid 3D

--- libs/base/include/mrpt/utils/CDynamicGrid3D.h
+++ libs/base/include/mrpt/utils/CDynamicGrid3D.h
@@ -185,7 +185,7 @@ namespace mrpt
 			inline size_t cellAbsIndexFromCXCYCZ(const int cx, const int cy, const int cz) const {
 				if (cx<0 || cx >= static_cast<int>(m_size_x)) return INVALID_VOXEL_IDX;
 				if (cy<0 || cy >= static_cast<int>(m_size_y)) return INVALID_VOXEL_IDX;
-				if (cz<0 || cy >= static_cast<int>(m_size_z)) return INVALID_VOXEL_IDX;
+				if (cz<0 || cz >= static_cast<int>(m_size_z)) return INVALID_VOXEL_IDX;
 				return cx + cy*m_size_x + cz * m_size_x_times_y;
 			}
 				
