214e1fc0803c6652aeaef9c3e201ea199f9711af
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2017-02-19 04:56:50 +0100

    API extended

--- libs/base/src/math/CRuntimeCompiledExpression.cpp
+++ libs/base/src/math/CRuntimeCompiledExpression.cpp
@@ -27,8 +27,9 @@ CRuntimeCompiledExpression::CRuntimeCompiledExpression()
 }
 
 void CRuntimeCompiledExpression::compile(
-	const std::string &expression,                  //!< [in] The expression to be compiled.
-	const std::map<std::string, double> &variables  //!< [in] Map of variables/constants by `name` ->  `value`. The references to the values in this map **must** be ensured to be valid thoughout all the life of the compiled expression.
+	const std::string &expression,                    //!< [in] The expression to be compiled.
+	const std::map<std::string, double> &variables,   //!< [in] Map of variables/constants by `name` ->  `value`. The references to the values in this map **must** be ensured to be valid thoughout all the life of the compiled expression.
+	const std::string &expr_name_for_error_reporting  //!< A descriptive name of this formula, to be used when generating error reports via an  exception, if needed
 )
 {
 	exprtk::symbol_table<double> symbol_table;
@@ -43,13 +44,25 @@ void CRuntimeCompiledExpression::compile(
 	// Compile user-given expressions:
 	exprtk::parser<double> parser;
 	if (!parser.compile(expression, PIMPL_GET_REF(exprtk::expression<double>, m_compiled_formula)))
-		THROW_EXCEPTION_FMT("Error compiling expression: `%s`. Error: `%s`", expression.c_str(), parser.error().c_str());
+		THROW_EXCEPTION_FMT("Error compiling expression (name=`%s`): `%s`. Error: `%s`", expr_name_for_error_reporting.c_str(), expression.c_str(), parser.error().c_str());
 }
 
 double CRuntimeCompiledExpression::eval() const
 {
 	ASSERT_(m_compiled_formula.ptr.get() != nullptr);
 	return PIMPL_GET_CONSTREF(exprtk::expression<double>, m_compiled_formula).value();
+}
+
+void CRuntimeCompiledExpression::register_symbol_table(
+	const std::map<std::string, double *> &variables  //!< [in] Map of variables/constants by `name` ->  `value`. The references to the values in this map **must** be ensured to be valid thoughout all the life of the compiled expression.
+)
+{
+	exprtk::symbol_table<double> symbol_table;
+	for (const auto &v : variables) {
+		double * var = const_cast<double*>(v.second);
+		symbol_table.add_variable(v.first, *var);
+	}
+	PIMPL_GET_REF(exprtk::expression<double>, m_compiled_formula).register_symbol_table(symbol_table);
 }
 
 exprtk::expression<double> & CRuntimeCompiledExpression::get_raw_exprtk_expr() {
