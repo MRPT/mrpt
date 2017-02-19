dbea31a1e678f3a483f1ce76ecd3c0afe2de43ac
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2017-02-19 04:57:37 +0100

    replace direct use of exprtk by wrapper class

--- libs/nav/src/reactive/CMultiObjMotionOpt_Scalarization.cpp
+++ libs/nav/src/reactive/CMultiObjMotionOpt_Scalarization.cpp
@@ -11,14 +11,10 @@
 
 #include <mrpt/nav/reactive/CMultiObjMotionOpt_Scalarization.h>
 
-#define exprtk_disable_string_capabilities   // Workaround a bug in Ubuntu precise's GCC+libstdc++
-#include <mrpt/otherlibs/exprtk.hpp>
-
 using namespace mrpt::nav;
 using namespace mrpt::utils;
 
 IMPLEMENTS_MRPT_OBJECT(CMultiObjMotionOpt_Scalarization, CMultiObjectiveMotionOptimizerBase, mrpt::nav)
-
 
 CMultiObjMotionOpt_Scalarization::CMultiObjMotionOpt_Scalarization() :
 	CMultiObjectiveMotionOptimizerBase(parameters)
@@ -28,7 +24,7 @@ CMultiObjMotionOpt_Scalarization::CMultiObjMotionOpt_Scalarization() :
 void CMultiObjMotionOpt_Scalarization::clear()
 {
 	CMultiObjectiveMotionOptimizerBase::clear();
-	m_expr_scalar_formula = TCompiledFormulaWrapper();
+	m_expr_scalar_formula = mrpt::math::CRuntimeCompiledExpression();
 	m_expr_scalar_vars.clear();
 }
 
@@ -56,23 +52,16 @@ int CMultiObjMotionOpt_Scalarization::impl_decide(const std::vector<mrpt::nav::T
 	if (m_expr_scalar_vars.empty())
 	{
 		// vars:
-		exprtk::symbol_table<double> symbol_table;
 		for (const auto &c : extra_info.score_values) {
 			for (const auto &score : c) {
 				double & var = m_expr_scalar_vars[score.first]; // create or reuse placeholder
-				symbol_table.add_variable(score.first, var);
 			}
 		}
-		symbol_table.add_constant("M_PI", M_PI);
-		symbol_table.add_constants();
 
 		// formula:
-		m_expr_scalar_formula = TCompiledFormulaWrapper();
-		PIMPL_GET_REF(exprtk::expression<double>, m_expr_scalar_formula.compiled_formula).register_symbol_table(symbol_table);
+		m_expr_scalar_formula = mrpt::math::CRuntimeCompiledExpression();
 		// Compile user-given expressions:
-		exprtk::parser<double> parser;
-		if (!parser.compile(parameters.scalar_score_formula, PIMPL_GET_REF(exprtk::expression<double>, m_expr_scalar_formula.compiled_formula)))
-			THROW_EXCEPTION_FMT("Error compiling `scalar_score_formula` expression: `%s`. Error: `%s`", parameters.scalar_score_formula.c_str(), parser.error().c_str());
+		m_expr_scalar_formula.compile(parameters.scalar_score_formula, m_expr_scalar_vars);
 	}
 
 	// Evaluate the formula for all candidates:
@@ -94,7 +83,7 @@ int CMultiObjMotionOpt_Scalarization::impl_decide(const std::vector<mrpt::nav::T
 			double & var = it->second;
 			var = score.second;
 		}
-		const double val = PIMPL_GET_CONSTREF(exprtk::expression<double>, m_expr_scalar_formula.compiled_formula).value();
+		const double val = m_expr_scalar_formula.eval();
 		extra_info.final_evaluation[i] = val;
 
 		if (val > 0 && (best_idx == -1 || val > best_val))
