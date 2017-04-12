54435834ee8443f2c8d7aeab8e3772d83ddb1572
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2017-04-12 04:24:31 +0200

    Correct "point - quaternion" operators

--- libs/base/src/poses/CPose3DQuat.cpp
+++ libs/base/src/poses/CPose3DQuat.cpp
@@ -460,3 +460,18 @@ bool mrpt::poses::operator!=(const CPose3DQuat &p1,const CPose3DQuat &p2)
 {
 	return !(p1==p2);
 }
+
+CPoint3D mrpt::poses::operator -(const CPoint3D &G,const CPose3DQuat &p)
+{ 
+	CPoint3D L; 
+	p.inverseComposePoint(G[0], G[1], G[2], L[0], L[1], L[2]); 
+	return L; 
+}
+
+mrpt::math::TPoint3D mrpt::poses::operator -(const mrpt::math::TPoint3D &G, const CPose3DQuat &p)
+{
+	mrpt::math::TPoint3D L; 
+	p.inverseComposePoint(G[0], G[1], G[2], L[0], L[1], L[2]); 
+	return L; 
+}
+
