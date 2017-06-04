2cb53c768a9869fd30fae8d22dcc190c3612fc8c
Raghavender Sahdev <raghavendersahdev@gmail.com>
2017-06-03 21:33:12 -0400

    Update detectordialog.cpp

--- apps/benchmarking-image-features/detectordialog.cpp
+++ apps/benchmarking-image-features/detectordialog.cpp
@@ -1,7 +1,28 @@
-#include "detectordialog.h"
+#ifndef DIALOGDETECTOR_H
+#define DIALOGDETECTOR_H
 
-DialogDetector::DialogDetector()
+#include <QDialog>
+#include <string>
+#include <QString>
+
+
+using namespace std;
+class DetectorDialog : public QDialog
 {
+    Q_OBJECT
+public:
 
-}
+    explicit DetectorDialog(QWidget *parent = 0, QString  str="/home", int detector=0);
+    void computeDetector(int detector_id);
 
+public:
+    QWidget *detector_gui;
+    string file_path;
+
+public slots:
+    void onshow_results_clicked();
+    void oncloseDialog_clicked();
+
+};
+
+#endif // DIALOGDETECTOR_H
