bb7b4423a3f7981f4b30ab8f39a30b6e2ad4a14d
Nikos Koukis <nickkouk@gmail.com>
2017-06-14 01:00:22 +0300

    cmake:Impose minimum version for vtk parts
    vtkStructuredGrid was added in version 5.10.0rc2
    
    https://github.com/Kitware/VTK/releases?after=v6.0.0.rc3
    https://github.com/Kitware/VTK/commits/master/Common/DataModel/vtkStructuredGrid.h

--- cmakemodules/script_vtk.cmake
+++ cmakemodules/script_vtk.cmake
@@ -5,9 +5,14 @@ SET(CMAKE_MRPT_HAS_VTK 0)
 OPTION(DISABLE_VTK "Force not using VTK libs" "OFF")
 MARK_AS_ADVANCED(DISABLE_VTK)
 IF(NOT DISABLE_VTK)
-
 	FIND_PACKAGE(VTK)
 	IF (VTK_FOUND)
+		# vtkStructuredGrid added after 5.10.0rc2 or above
+		# constraint on the version - putting this in FIND_PACKAGE doesn't work.
+		IF (${VTK_MAJOR_VERSION} LESS 5 OR (${VTK_MAJOR_VERSION} EQUAL 5 AND VTK_MINOR_VERSION LESS 10))
+			MESSAGE(FATAL_ERROR "Current VTK version (${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}.${VTK_BUILD_VERSION}) is less than version 5.10 (the required minimum). Exiting.")
+		ENDIF()
+
 		SET(CMAKE_MRPT_HAS_VTK 1)
 	ENDIF()
 ENDIF()
