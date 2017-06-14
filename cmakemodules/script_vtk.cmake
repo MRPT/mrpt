6f02d82093cea4926cf71f8638931972967df583
Nikos Koukis <nickkouk@gmail.com>
2017-06-14 13:30:10 +0300

    Rollback minimum vtk version
    vtkStructuredGrid.h is included in older versions (notably 5.8) after
    all.
    
    Also use the UseVTK.cmake file instead of manually including header dirs
    and linking.

--- cmakemodules/script_vtk.cmake
+++ cmakemodules/script_vtk.cmake
@@ -8,12 +8,5 @@ IF(NOT DISABLE_VTK)
 	FIND_PACKAGE(VTK)
 	IF (VTK_FOUND)
 		SET(CMAKE_MRPT_HAS_VTK 1)
-
-		# vtkStructuredGrid added after 5.10.0rc2 or above
-		# constraint on the version - putting this in FIND_PACKAGE doesn't work.
-		IF (${VTK_MAJOR_VERSION} LESS 5 OR (${VTK_MAJOR_VERSION} EQUAL 5 AND VTK_MINOR_VERSION LESS 10))
-			MESSAGE("Current VTK version (${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}.${VTK_BUILD_VERSION}) is less than version 5.10 (the required minimum). Disabling VTK.")
-			SET(CMAKE_MRPT_HAS_VTK 0)
-		ENDIF()
 	ENDIF()
 ENDIF()
