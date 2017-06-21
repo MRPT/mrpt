215bdf4fc420affd8264698a839406b3e1ebb419
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2017-06-21 03:17:45 +0200

    rnav: More flexible callback behavior
    Added:
    - new INI file parameter
    - callback can now return a bool to reflect whether to cancel navigation
    or not

--- libs/nav/src/reactive/CRobot2NavInterface.cpp
+++ libs/nav/src/reactive/CRobot2NavInterface.cpp
@@ -61,19 +61,19 @@ void CRobot2NavInterface::sendNewWaypointTargetEvent(int waypoint_index)
 }
 void CRobot2NavInterface::sendNavigationEndDueToErrorEvent()
 {
-	MRPT_LOG_INFO("[sendNavigationEndDueToErrorEvent] Doing nothing: not implemented in user's derived class.");
+	MRPT_LOG_THROTTLE_INFO(1.0,"[sendNavigationEndDueToErrorEvent] Doing nothing: not implemented in user's derived class.");
 }
 void CRobot2NavInterface::sendWaySeemsBlockedEvent() 
 {
-	MRPT_LOG_INFO("[sendWaySeemsBlockedEvent] Doing nothing: not implemented in user's derived class.");
+	MRPT_LOG_THROTTLE_INFO(1.0, "[sendWaySeemsBlockedEvent] Doing nothing: not implemented in user's derived class.");
 }
 void CRobot2NavInterface::sendApparentCollisionEvent()
 {
-	MRPT_LOG_INFO("[sendApparentCollisionEvent] Doing nothing: not implemented in user's derived class.");
+	MRPT_LOG_THROTTLE_INFO(1.0,"[sendApparentCollisionEvent] Doing nothing: not implemented in user's derived class.");
 }
-void CRobot2NavInterface::sendCannotGetCloserToBlockedTargetEvent()
+void CRobot2NavInterface::sendCannotGetCloserToBlockedTargetEvent(bool &do_abort_nav)
 {
-	MRPT_LOG_INFO("[sendCannotGetCloserToBlockedTargetEvent] Doing nothing: not implemented in user's derived class.");
+	MRPT_LOG_THROTTLE_INFO(1.0, "[sendCannotGetCloserToBlockedTargetEvent] Doing nothing: not implemented in user's derived class.");
 }
 
 double CRobot2NavInterface::getNavigationTime() {
