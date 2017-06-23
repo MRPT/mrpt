9c7a007a87cfdbc22d2665c774903e2c9152060e
LisGein <lisgein47@gmail.com>
2017-06-23 16:47:47 +0300

    robot-map-gui: added open *.simplemap.gz and copyright notice

--- apps/robot-map-gui/gui/CMainWindow.cpp
+++ apps/robot-map-gui/gui/CMainWindow.cpp
@@ -1,3 +1,11 @@
+/* +---------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)               |
+   |                          http://www.mrpt.org/                             |
+   |                                                                           |
+   | Copyright (c) 2005-2017, Individual contributors, see AUTHORS file        |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                   |
+   | Released under BSD License. See details in http://www.mrpt.org/License    |
+   +---------------------------------------------------------------------------+ */
 #include "CMainWindow.h"
 #include "CGLWidget.h"
 #include "CDocument.h"
@@ -35,7 +43,7 @@ CMainWindow::~CMainWindow()
 
 void CMainWindow::openMap()
 {
-	QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "", tr("Files (*.simplemap)"));
+	QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "", tr("Files (*.simplemap, *.simplemap.gz)"));
 	QString configName = QFileDialog::getOpenFileName(this, tr("Open Config File"), "", tr("Files (*.ini)"));
 
 	if (fileName.size() == 0 || configName.size() == 0)
