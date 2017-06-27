388f28f76fb8508e5d3148ab133ba166c6e1a9ac
Raghavender Sahdev <raghavendersahdev@gmail.com>
2017-06-27 04:22:33 -0400

    work from June 25 to June 26 added descriptor/detector visualization using mouse click_minor bugs_need to be fixed

---
+++ apps/benchmarking-image-features/src/my_qlabel.cpp
@@ -0,0 +1,27 @@
+//
+// Created by raghavender on 26/06/17.
+//
+
+#include "my_qlabel.h"
+
+my_qlabel::my_qlabel(QWidget *parent) : QLabel(parent)
+{
+
+}
+
+void my_qlabel::mouseMoveEvent(QMouseEvent *ev) {
+    this->x = ev->x();
+    this->y = ev->y();
+    emit Mouse_Pos();
+}
+
+void my_qlabel::mousePressEvent(QMouseEvent *ev)
+{
+    emit Mouse_Pressed();
+}
+
+void my_qlabel::leaveEvent(QEvent *)
+{
+    emit Mouse_Left();
+}
+
