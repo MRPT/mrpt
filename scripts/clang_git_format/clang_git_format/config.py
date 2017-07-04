c534d0af4ec575c9da342e41600c31a6ef997a7a
Nikos Koukis <nickkouk@gmail.com>
2017-07-04 18:35:57 +0300

    Add clang_git_format to MRPT codebase
    Up-to-date repo can be found
    [here](https://github.com/bergercookie/clang_git_format).

---
+++ scripts/clang_git_format/clang_git_format/config.py
@@ -0,0 +1,16 @@
+import string
+
+PROGNAME = "clang-format"
+CLANG_FORMAT_VERSION = "3.8.0"
+_last_dot = CLANG_FORMAT_VERSION.rfind(".")
+CLANG_FORMAT_SHORT_VERSION = CLANG_FORMAT_VERSION[:_last_dot]
+
+# URL location of the "cached" copy of clang-format to download
+# for users which do not have clang-format installed
+CLANG_FORMAT_HTTP_LINUX_CACHE = "https://s3.amazonaws.com/boxes.10gen.com/build/clang-format-3.8-rhel55.tar.gz"
+CLANG_FORMAT_HTTP_DARWIN_CACHE = "https://s3.amazonaws.com/boxes.10gen.com/build/clang%2Bllvm-3.8.0-x86_64-apple-darwin.tar.xz"
+
+CLANG_FORMAT_SOURCE_TAR_BASE = string.Template(
+    "clang+llvm-$version-$tar_path/bin/" +
+    PROGNAME)
+
