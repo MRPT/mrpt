6e4004364eca16fa3e860686e10fdbfdb9187597
Nikos Koukis <nickkouk@gmail.com>
2017-07-06 22:26:49 +0300

    Modify convert_dox_comments scripts

--- scripts/clang_git_format/scripts/convert_inline_dox_comments_file.sh
+++ scripts/clang_git_format/scripts/convert_inline_dox_comments_file.sh
@@ -15,11 +15,11 @@ f=$1
 # Modify all //!< ... comments to /** ... */ and place them in the previous line
 # - Move the comment to the prevous line
 # - Keep indentation level
-sed -i''  "s/^\(\s\+\)\(.*\)\s*\/\/\!<\(.*\)$/\1\/\*\*\3 \*\/\\n\1\2/g" ${f}
+# - Do not match comment lines (start with // or /*)
+sed -i''  "/^\s\+[\/\*\|\/\/]/! s/^\(\s*\)\(.*\)\s*\/\/\!<\(.*\)$/\1\/\*\*\3 \*\/\\n\1\2/g" ${f}
 
-# remove trailing spaces - run it only for statements (ending in ";"), not for
-# e.g., macros.
-# clang-format is coming up to do the cleanup...
-sed -i'' "s/;\s*$/;/g" ${f}
+# remove buggy "*/ */" expressions at the end of lines - print for verification
+sed -n "/\*\/\s*\*\//p" ${f}
+sed -i "s/\(\*\/\)\s*\*\//\1/g" ${f}
 
 exit 0
