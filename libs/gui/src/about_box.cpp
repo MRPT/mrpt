a815d62603dce9c55ce9c97a5bc8e3552eb4486e
Jose Luis Blanco <joseluisblancoc@gmail.com>
2017-07-10 13:52:19 +0200

    add missing files

---
+++ libs/gui/src/about_box.cpp
@@ -0,0 +1,36 @@
+/* +------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)            |
+   |                          http://www.mrpt.org/                          |
+   |                                                                        |
+   | Copyright (c) 2005-2017, Individual contributors, see AUTHORS file     |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                |
+   | Released under BSD License. See details in http://www.mrpt.org/License |
+   +------------------------------------------------------------------------+ */
+
+#include "gui-precomp.h"  // Precompiled headers
+
+#include <mrpt/gui/about_box.h>
+
+#if MRPT_HAS_WXWIDGETS
+#include "CAboutBox_wx.h"
+#endif
+
+//#if MRPT_HAS_QT
+// #include "CAboutBox_qt.h" // TODO
+//#endif
+
+void GUI_IMPEXP mrpt::gui::show_mrpt_about_box_wxWidgets(
+	void *parent_wx_window,
+	const std::string &appName,
+	const std::string &additionalInfo,
+	const bool showStandardInfo
+	)
+{
+#if MRPT_HAS_WXWIDGETS
+	wxWindow* parent = reinterpret_cast<wxWindow*>(parent_wx_window);
+	CAboutBox dlg(parent,appName,additionalInfo,showStandardInfo);
+	dlg.ShowModal();
+#endif
+}
+
+
