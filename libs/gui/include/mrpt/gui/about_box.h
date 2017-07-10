2a8e069cf5ce974046b827ecffad070189dff854
Jose Luis Blanco <joseluisblancoc@gmail.com>
2017-07-10 13:52:19 +0200

    add missing files

---
+++ libs/gui/include/mrpt/gui/about_box.h
@@ -0,0 +1,30 @@
+/* +------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)            |
+   |                          http://www.mrpt.org/                          |
+   |                                                                        |
+   | Copyright (c) 2005-2017, Individual contributors, see AUTHORS file     |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                |
+   | Released under BSD License. See details in http://www.mrpt.org/License |
+   +------------------------------------------------------------------------+ */
+
+#pragma once
+
+#include <mrpt/gui/link_pragmas.h>
+#include <string>
+
+namespace mrpt
+{
+namespace gui
+{
+
+/** Shows the standard MRPT GUI "About Box" (wxWidgets version) */
+void GUI_IMPEXP show_mrpt_about_box_wxWidgets(
+	void *parent_wx_window,
+	const std::string &appName,
+	const std::string &additionalInfo = std::string(),
+	const bool showStandardInfo = true
+	);
+
+
+}
+}
