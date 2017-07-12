bee333ce2b1e35def7db483a6f8cf5a6c6f0068b
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2017-07-13 00:23:38 +0200

    CriticalSections: simplified and add try_enter()

--- libs/base/src/synch/CCriticalSection.cpp
+++ libs/base/src/synch/CCriticalSection.cpp
@@ -10,15 +10,59 @@
 #include "base-precomp.h"  // Precompiled headers
 
 #include <mrpt/synch/CCriticalSection.h>
+#include <mrpt/system/threads.h>
 #include <mutex>
 
 using namespace mrpt::utils;
 using namespace mrpt::synch;
 
+// CAbstractMutex
 CAbstractMutex::~CAbstractMutex()
 {
 }
 
+// CCriticalSection
+CCriticalSection::CCriticalSection(const char *name) :
+	m_data(new std::mutex()),
+	m_name(name ? name : "Unnamed"),
+	m_debugOut(nullptr)
+{
+}
+
+CCriticalSection::~CCriticalSection()
+{
+	std::mutex *mut = reinterpret_cast<std::mutex*>(m_data);
+	delete mut;
+	m_data = nullptr;
+}
+
+void  CCriticalSection::enter() const
+{
+	const unsigned long threadid = mrpt::system::getCurrentThreadId();
+	if (m_debugOut) m_debugOut->printf("[CCriticalSection:%s] Entering Thread ID:%lu\n", m_name.c_str(), threadid);
+
+	std::mutex *mut = reinterpret_cast<std::mutex*>(m_data);
+	mut->lock();
+
+	if (m_debugOut) m_debugOut->printf("[CCriticalSection:%s] Entering DONE Thread ID:%lu\n", m_name.c_str(), threadid);
+}
+
+void  CCriticalSection::leave() const
+{
+	const unsigned long threadid = mrpt::system::getCurrentThreadId();
+	if (m_debugOut) m_debugOut->printf("[CCriticalSection:%s] Leaving Thread ID:%lu\n", m_name.c_str(), threadid);
+
+	std::mutex *mut = reinterpret_cast<std::mutex*>(m_data);
+	mut->unlock();
+}
+
+bool CCriticalSection::try_enter() const
+{
+	std::mutex *mut = reinterpret_cast<std::mutex*>(m_data);
+	return mut->try_lock();
+}
+
+// CCriticalSectionRecursive
 CCriticalSectionRecursive::CCriticalSectionRecursive() :
 	m_data( new std::recursive_mutex() )
 {
@@ -41,6 +85,13 @@ void  CCriticalSectionRecursive::leave() const
 	std::recursive_mutex *mut = reinterpret_cast<std::recursive_mutex*>(m_data);
 	mut->unlock();
 }
+
+bool CCriticalSectionRecursive::try_enter() const
+{
+	std::recursive_mutex *mut = reinterpret_cast<std::recursive_mutex*>(m_data);
+	return mut->try_lock();
+}
+
 
 
 
