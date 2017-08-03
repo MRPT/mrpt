fa16ca9d21b2fba4d54d726406e847f8d2036fd7
Jose Luis Blanco <joseluisblancoc@gmail.com>
2017-08-03 16:46:52 +0200

    fix clang -Wextra warnings

--- libs/slam/include/mrpt/slam/CMonteCarloLocalization2D.h
+++ libs/slam/include/mrpt/slam/CMonteCarloLocalization2D.h
@@ -94,7 +94,8 @@ class SLAM_IMPEXP CMonteCarloLocalization2D
 	void prediction_and_update_pfStandardProposal(
 		const mrpt::obs::CActionCollection* action,
 		const mrpt::obs::CSensoryFrame* observation,
-		const bayes::CParticleFilter::TParticleFilterOptions& PF_options);
+		const bayes::CParticleFilter::TParticleFilterOptions& PF_options)
+		override;
 
 	/** Update the m_particles, predicting the posterior of robot pose and map
 	 * after a movement command.
@@ -111,7 +112,8 @@ class SLAM_IMPEXP CMonteCarloLocalization2D
 	void prediction_and_update_pfAuxiliaryPFStandard(
 		const mrpt::obs::CActionCollection* action,
 		const mrpt::obs::CSensoryFrame* observation,
-		const bayes::CParticleFilter::TParticleFilterOptions& PF_options);
+		const bayes::CParticleFilter::TParticleFilterOptions& PF_options)
+		override;
 
 	/** Update the m_particles, predicting the posterior of robot pose and map
 	 * after a movement command.
@@ -128,7 +130,8 @@ class SLAM_IMPEXP CMonteCarloLocalization2D
 	void prediction_and_update_pfAuxiliaryPFOptimal(
 		const mrpt::obs::CActionCollection* action,
 		const mrpt::obs::CSensoryFrame* observation,
-		const bayes::CParticleFilter::TParticleFilterOptions& PF_options);
+		const bayes::CParticleFilter::TParticleFilterOptions& PF_options)
+		override;
 
 	// protected:
 	/** \name Virtual methods that the PF_implementations assume exist.
@@ -140,14 +143,14 @@ class SLAM_IMPEXP CMonteCarloLocalization2D
 
 	void PF_SLAM_implementation_custom_update_particle_with_new_pose(
 		CParticleDataContent* particleData,
-		const mrpt::math::TPose3D& newPose) const;
+		const mrpt::math::TPose3D& newPose) const override;
 
 	// We'll redefine this one:
 	void PF_SLAM_implementation_replaceByNewParticleSet(
 		CParticleList& old_particles,
 		const std::vector<mrpt::math::TPose3D>& newParticles,
 		const std::vector<double>& newParticlesWeight,
-		const std::vector<size_t>& newParticlesDerivedFromIdx) const;
+		const std::vector<size_t>& newParticlesDerivedFromIdx) const override;
 
 	/** Evaluate the observation likelihood for one particle at a given location
 	 */
@@ -155,7 +158,7 @@ class SLAM_IMPEXP CMonteCarloLocalization2D
 		const mrpt::bayes::CParticleFilter::TParticleFilterOptions& PF_options,
 		const size_t particleIndexForMap,
 		const mrpt::obs::CSensoryFrame& observation,
-		const mrpt::poses::CPose3D& x) const;
+		const mrpt::poses::CPose3D& x) const override;
 	/** @} */
 
 };  // End of class def.
