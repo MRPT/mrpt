22d36843976573894f9a4c9a1c4b7b997c04b81c
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2017-08-16 07:47:31 +0200

    cmake summary: show separate GUI libs section

--- cmakemodules/script_show_final_summary.cmake
+++ cmakemodules/script_show_final_summary.cmake
@@ -123,9 +123,12 @@ SHOW_CONFIG_LINE_SYSTEM("PCAP (Wireshark logs for Velodyne)  " CMAKE_MRPT_HAS_LI
 SHOW_CONFIG_LINE_SYSTEM("PCL (Pointscloud library)           " CMAKE_MRPT_HAS_PCL  "[Version: ${PCL_VERSION}]")
 SHOW_CONFIG_LINE       ("SuiteSparse                         " CMAKE_MRPT_HAS_SUITESPARSE)
 SHOW_CONFIG_LINE       ("VTK                                 " CMAKE_MRPT_HAS_VTK)
-SHOW_CONFIG_LINE_SYSTEM("wxWidgets                           " CMAKE_MRPT_HAS_WXWIDGETS)
-SHOW_CONFIG_LINE       ("Qt5                                 " CMAKE_MRPT_HAS_Qt5)
 SHOW_CONFIG_LINE_SYSTEM("yamlcpp (YAML file format)          " CMAKE_MRPT_HAS_YAMLCPP "[Version: ${LIBYAMLCPP_VERSION}]")
+MESSAGE(STATUS  "")
+
+MESSAGE(STATUS " ______________________ GUI LIBRARIES ______________________")
+SHOW_CONFIG_LINE       ("wxWidgets                           " CMAKE_MRPT_HAS_WXWIDGETS)
+SHOW_CONFIG_LINE       ("Qt5                                 " CMAKE_MRPT_HAS_Qt5)
 MESSAGE(STATUS  "")
 
 MESSAGE(STATUS " _______________________ WRAPPERS/BINDINGS ______________________")
