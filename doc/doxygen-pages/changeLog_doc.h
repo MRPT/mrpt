165b0eced562799c62c5c25c1c422b7fa3fb797d
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2017-10-27 11:37:32 +0200

    Fix out-of-range in convertTo2DScan()

--- doc/doxygen-pages/changeLog_doc.h
+++ doc/doxygen-pages/changeLog_doc.h
@@ -36,6 +36,7 @@
 		- Fix corrupted pointers in CNetworkOfPoses after copy or move operations.
 		- Fix invalid TP-targets generated during reactive navigation.
 		- Fix memory leak in reactivenav engine.
+		- Fix potential out-of-range access in CObservation3DRangeScan::convertTo2DScan()
 
 <hr>
 <a name="1.5.3">
