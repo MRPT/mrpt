2ad130c27722dbe54b9479819b2e4b7ae9fe629a
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2017-11-01 08:50:06 +0100

    start of 1.5.5

--- doc/doxygen-pages/changeLog_doc.h
+++ doc/doxygen-pages/changeLog_doc.h
@@ -12,6 +12,12 @@
 
 <p> <b>Note:</b> <i>If you are displaying a local version of this page and you have not built the whole HTML documentation, the links above will be broken. Either build the documentation invoking <code>make documentation_html</code> or [browse it on-line](http://www.mrpt.org/).</i></p>
 
+<a name="1.5.5">
+<h2>Version 1.5.5: (Under development) </h2></a>
+- <b>Detailed list of changes:</b>
+	- (None yet!)
+
+<hr>
 <a name="1.5.4">
 <h2>Version 1.5.4: Released 31/OCT/2017 </h2></a>
 - <b>Detailed list of changes:</b>
