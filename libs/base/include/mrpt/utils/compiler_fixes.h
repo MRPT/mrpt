4ef5018d7531c73ddb0073eef7c2a683dd0a2836
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2017-11-09 18:41:55 +0100

    Disable MSVC inherit from STL warnings
    cc: #557

--- libs/base/include/mrpt/utils/compiler_fixes.h
+++ libs/base/include/mrpt/utils/compiler_fixes.h
@@ -18,19 +18,17 @@
 	#pragma warning(disable:4127) // Disable conditional expression is constant (it shows up in templates where it's correct)
 	#pragma warning(disable:4244) // argument conversion "possible loss of data"
 	#pragma warning(disable:4503) // (Compiler: Visual C++ 2010) Disable warning for too long decorated name
-	//#pragma warning(disable:4305) // truncation 
-	//#pragma warning(disable:4308) // Disable warning for Eigen3 libs: negative integral converted to unsigned
 	#pragma warning(disable:4714) // force inlined -> not inlined (in Eigen3)
 	#pragma warning(disable:4267) // truncation size_t -> type
 	#pragma warning(disable:4290) // Visual C++ does not implement decl. specifiers: throw(A,B,...)
 	#pragma warning(disable:4251) // Visual C++ 2003+ warnings on STL classes when exporting to DLL...
 	#pragma warning(disable:4275) // DLL export class derived from STL
+	#pragma warning(disable:4251) // Warnings are emited even if a DLL export class contains a private STL field (?)
 	#if (_MSC_VER >= 1400 )
 		// MS believes they have the right to deprecate functions in the C++ Standard STL... disable their warnings:
 		#ifndef _SCL_SECURE_NO_WARNINGS
 			#define _SCL_SECURE_NO_WARNINGS
 		#endif
-		//#pragma warning(disable:4996)  // Deprecated functions
 		// For the new secure library in VC++8
 		#if !defined(_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES)
 			#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
