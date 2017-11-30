fbd39f04d8eff18c60a57c3a3946f878934edb9b
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2017-12-01 00:53:29 +0100

    fix error in debian inst files

--- scripts/prepare_debian.sh
+++ scripts/prepare_debian.sh
@@ -37,6 +37,11 @@ done
 if [ -f version_prefix.txt ];
 then
 	MRPT_VERSION_STR=`head -n 1 version_prefix.txt`
+  MRPT_VERSION_MAJOR=${MRPT_VERSION_STR:0:1}
+	MRPT_VERSION_MINOR=${MRPT_VERSION_STR:2:1}
+	MRPT_VERSION_PATCH=${MRPT_VERSION_STR:4:1}
+	MRPT_VER_MM="${MRPT_VERSION_MAJOR}.${MRPT_VERSION_MINOR}"
+	MRPT_VER_MMP="${MRPT_VERSION_MAJOR}.${MRPT_VERSION_MINOR}.${MRPT_VERSION_PATCH}"
 else
 	echo "Error: cannot find version_prefix.txt!!"
 	exit 1
