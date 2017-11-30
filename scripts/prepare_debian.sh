64116871a32d70d6208f3a0640be6d047b6c7df5
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2017-12-01 00:57:29 +0100

    fix ubuntu PPA dir names

--- scripts/prepare_debian.sh
+++ scripts/prepare_debian.sh
@@ -112,7 +112,10 @@ cp $HOME/mrpt_release/mrpt*.tar.gz $MRPT_DEB_DIR/mrpt_${MRPT_VERSION_STR}.orig.t
 cd ${MRPT_DEB_DIR}
 tar -xf mrpt_${MRPT_VERSION_STR}.orig.tar.gz
 
-mv mrpt-* ${MRPT_DEBSRC_DIR}  # fix different dir names for Ubuntu PPA packages
+if [ ! -d "${MRPT_DEBSRC_DIR}"];
+then
+  mv mrpt-* ${MRPT_DEBSRC_DIR}  # fix different dir names for Ubuntu PPA packages
+fi
 
 if [ ! -f "${MRPT_DEBSRC_DIR}/CMakeLists.txt" ];
 then
