5059be34ace6719b3172f304d685caf3dd2c3f62
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2017-12-02 04:16:30 +0100

    mrpt-io: Update backwards compatibility headers

--- libs/io/include/mrpt/utils/CFileGZOutputStream.h
+++ libs/io/include/mrpt/utils/CFileGZOutputStream.h
@@ -1,4 +1,6 @@
 #include <mrpt/io/CFileGZOutputStream.h>
+#include <mrpt/core/common.h>
+MRPT_WARNING("backwards-compat: update to <mrpt/io/CFileGZOutputStream.h>")
 namespace mrpt
 {
 namespace utils
---
+++ libs/io/include/mrpt/utils/CFileInputStream.h
@@ -0,0 +1,10 @@
+#include <mrpt/io/CFileInputStream.h>
+#include <mrpt/core/common.h>
+MRPT_WARNING("backwards-compat: update to <mrpt/io/CFileInputStream.h>")
+namespace mrpt
+{
+namespace utils
+{
+using CFileInputStream = mrpt::io::CFileInputStream;
+}
+}
