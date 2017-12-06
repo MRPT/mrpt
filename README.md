6774a45caf90b4cc19941f2eb7eaa31df1fe3cdb
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2017-12-06 09:12:25 +0100

    Use docker for travis
    so we can run on more modern distros of Ubuntu, including modern CMake

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ Minimum compiler requisites:
   * mrpt >=2.0 (`master` branch):
     * Ubuntu 16.04LTS Xenial: default gcc/clang versions
     * Windows: Visual Studio 2015/2017.
-    * cmake >= 3.1 required (>=3.4 for Windows).
+    * cmake >= 3.3 required (>=3.4 for Windows).
   * mrpt 1.5.* and maintenance `mrpt-1.5` branch:
     * Ubuntu 14.04LTS Trusty: default gcc/clang versions
     * Windows: Visual Studio 2012
--- travis/travis_main.sh
+++ travis/travis_main.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e   # Make sure any error makes the script to return an error code
 
 MRPT_DIR=`pwd`
@@ -8,6 +7,36 @@ BUILD_DIR=build
 CMAKE_C_FLAGS="-Wall -Wextra -Wabi -O2"
 CMAKE_CXX_FLAGS="-Wall -Wextra -Wabi -O2"
 EXTRA_CMAKE_ARGS="-DDISABLE_PCL=ON"  # PCL causes link errors (?!)
+
+function prepare_install()
+{
+  apt-get install build-essential software-properties-common gcc g++ clang pkg-config cmake python-pip -y
+  apt-get install git-core -y
+  apt-get install ccache -y
+
+  if [ "$TASK" == "lint" ]; then
+    pip install -r travis/python_reqs.txt
+  fi
+
+  if [ "$DEPS" != "minimal" ]; then
+    apt-get install libftdi-dev zlib1g-dev libusb-1.0-0-dev libdc1394-22-dev -y
+    apt-get install libjpeg-dev libopencv-dev libgtest-dev libeigen3-dev -y
+    apt-get install libsuitesparse-dev libopenni2-dev libudev-dev -y
+    apt-get install libboost-python-dev libpython-dev python-numpy -y
+
+    # We must use a custom PPA to solve errors in PCL official pkgs
+    add-apt-repository ppa:jolting/backport-mrpt
+    apt-get update -qq
+    apt-get install libpcl-dev -y
+    if [ "$DEPS" != "headless" ]; then
+      apt-get install libwxgtk3.0-dev -y
+      apt-get install freeglut3-dev -y
+      apt-get install libavformat-dev libswscale-dev -y
+      apt-get install libassimp-dev -y
+      apt-get install qtbase5-dev libqt5opengl5-dev -y
+    fi
+  fi
+}
 
 function prepare_build_dir()
 {
@@ -57,7 +86,7 @@ function test ()
 {
   # gcc is too slow and we have a time limit in Travis CI:
   if [ "$CC" == "gcc" ] && [ "$TRAVIS_OS_NAME" == "osx" ]; then
-	return
+    return
   fi
 
   prepare_build_dir
@@ -76,6 +105,8 @@ function test ()
 
   cd $MRPT_DIR
 }
+
+prepare_install
 
 case $TASK in
   build ) build;;
