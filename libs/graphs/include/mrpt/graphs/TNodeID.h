6467643d1856cda2a82ad373c33b115b8f96936a
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2018-01-04 05:22:33 +0100

    Changes: - mrpt-graphs builds - TNodeID moved to <mrpt/graphs/TNodeID.h>

--- libs/graphs/include/mrpt/graphs/TNodeID.h
+++ libs/graphs/include/mrpt/graphs/TNodeID.h
@@ -7,37 +7,16 @@
    | Released under BSD License. See details in http://www.mrpt.org/License |
    +------------------------------------------------------------------------+ */
 #pragma once
-
 #include <cstdint>
-#include <mrpt/core/exceptions.h>
-#include <mrpt/graphs/TNodeID.h>
-#include <stdexcept>
-#include <string>
-
+#include <utility>  // pair
 namespace mrpt
 {
 namespace graphs
 {
-// TODO - include docstring, group etc.
-class HypothesisNotFoundException : public std::runtime_error
-{
-   public:
-	HypothesisNotFoundException(
-		mrpt::graphs::TNodeID from, mrpt::graphs::TNodeID to);
-	HypothesisNotFoundException(size_t id);
-	~HypothesisNotFoundException() noexcept;
-	void clear();
-	std::string getErrorMsg() const noexcept;
-	const char* what() const noexcept;
-
-   private:
-	mrpt::graphs::TNodeID m_from, m_to;
-
-	/**\brief Hypothesis ID */
-	size_t m_id;
-
-	/**\brief Error message */
-	std::string m_msg;
-};
+/** A generic numeric type for unique IDs of nodes or entities */
+using TNodeID = uint64_t;
+/** A pair of node IDs */
+typedef std::pair<TNodeID, TNodeID> TPairNodeIDs;
+#define INVALID_NODEID static_cast<TNodeID>(-1)
 }  // namespace graphs
 }  // namespace mrpt
