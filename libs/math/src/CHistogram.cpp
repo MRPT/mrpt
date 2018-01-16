65f28ee8843b2b49309eac943bf8e7609b863675
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2018-01-16 09:58:53 +0100

    fix potential mem errors (gcc sanitizer)

--- libs/math/src/CHistogram.cpp
+++ libs/math/src/CHistogram.cpp
@@ -41,9 +41,11 @@ void CHistogram::clear()
  ---------------------------------------------------------------*/
 void CHistogram::add(const double x)
 {
+	ASSERT_(!m_bins.empty());
 	if (x < m_min || x > m_max) return;
 
 	size_t ind = static_cast<size_t>(m_binSizeInv * (x - m_min));
+	if (ind>=m_bins.size()) ind=m_bins.size()-1;
 
 	m_bins[ind]++;
 	m_count++;
