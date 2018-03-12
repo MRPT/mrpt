acc96da663e7c60520f4388ada583b305f7fb643
Hunter Laux <hunterlaux@gmail.com>
2018-03-11 21:10:20 -0700

    Pass CC to the container

--- travis/travis_docker.sh
+++ travis/travis_docker.sh
@@ -9,7 +9,7 @@ if [ "$STAGE" == "2" ]; then
   export DOCKER_IMAGE=$STAGE1_OUTPUT_IMAGE
 fi
 
-docker run -v $HOME:$HOME $CODECOV_VARS -e BUILD_TARGET -e STAGE -e BUILD_TYPE -e MAKEFLAGS -e CCACHE_SLOPPINESS -e TASK -e DEPS -e CI_SOURCE_PATH -e REPOSITORY_NAME -e HOME -e DISTRO -e CI_ROS_DISTRO -e CODECOV_TOKEN --name mrptbuild $DOCKER_IMAGE bash -c 'cd $CI_SOURCE_PATH; apt-get update -qq ; source travis/check_style.sh; source travis/travis_main.sh'
+docker run -v $HOME:$HOME $CODECOV_VARS -e CC -e BUILD_TARGET -e STAGE -e BUILD_TYPE -e MAKEFLAGS -e CCACHE_SLOPPINESS -e TASK -e DEPS -e CI_SOURCE_PATH -e REPOSITORY_NAME -e HOME -e DISTRO -e CI_ROS_DISTRO -e CODECOV_TOKEN --name mrptbuild $DOCKER_IMAGE bash -c 'cd $CI_SOURCE_PATH; apt-get update -qq ; source travis/check_style.sh; source travis/travis_main.sh'
 
 if [ "$STAGE" == "1" ]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
