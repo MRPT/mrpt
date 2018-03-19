f9bf6d19e223534755112d7dac1bcf3e6b1839d0
Hunter Laux <hunterlaux@gmail.com>
2018-03-18 23:06:41 -0700

    Complete reverse bytes tests

--- libs/core/src/reverse_bytes_unittest.cpp
+++ libs/core/src/reverse_bytes_unittest.cpp
@@ -32,8 +32,26 @@ TEST(bits, reverseBytes)
 		EXPECT_EQ(val, val_r_ok);
 	}
 	{
+		int16_t val = 0x1122, val_r;
+		const int16_t val_r_ok = 0x2211;
+
+		mrpt::reverseBytes(val, val_r);
+		EXPECT_EQ(val_r, val_r_ok);
+		mrpt::reverseBytesInPlace(val);
+		EXPECT_EQ(val, val_r_ok);
+	}
+	{
 		uint32_t val = UINT32_C(0x11223344), val_r;
 		const uint32_t val_r_ok = UINT32_C(0x44332211);
+
+		mrpt::reverseBytes(val, val_r);
+		EXPECT_EQ(val_r, val_r_ok);
+		mrpt::reverseBytesInPlace(val);
+		EXPECT_EQ(val, val_r_ok);
+	}
+	{
+		int32_t val = INT32_C(0x11223344), val_r;
+		const int32_t val_r_ok = INT32_C(0x44332211);
 
 		mrpt::reverseBytes(val, val_r);
 		EXPECT_EQ(val_r, val_r_ok);
@@ -48,5 +66,46 @@ TEST(bits, reverseBytes)
 		EXPECT_EQ(val_r, val_r_ok);
 		mrpt::reverseBytesInPlace(val);
 		EXPECT_EQ(val, val_r_ok);
+	}
+	{
+		int64_t val = INT64_C(0x1122334455667766), val_r;
+		const int64_t val_r_ok = INT64_C(0x6677665544332211);
+
+		mrpt::reverseBytes(val, val_r);
+		EXPECT_EQ(val_r, val_r_ok);
+		mrpt::reverseBytesInPlace(val);
+		EXPECT_EQ(val, val_r_ok);
+	}
+	{
+		bool valTrue  = true;
+		bool valFalse = false;
+		mrpt::reverseBytesInPlace(valTrue);
+		mrpt::reverseBytesInPlace(valFalse);
+		EXPECT_TRUE(valTrue);
+		EXPECT_FALSE(valFalse);
+	}
+	{
+		uint8_t val = UINT8_C(0xc1);
+
+		mrpt::reverseBytesInPlace(val);
+		EXPECT_EQ(val, 0xc1);
+	}
+	{
+		//1.0 == 0x3F800000
+		float val = 1;
+		const uint32_t val_r_ok = UINT32_C(0x803f);
+
+		mrpt::reverseBytesInPlace(val);
+		uint32_t val_check = *reinterpret_cast<uint32_t*>(&val);
+		EXPECT_EQ(val_check, val_r_ok);
+	}
+	{
+		//1.0 == 0x3ff0000000000000
+		double val = 1;
+		const uint64_t val_r_ok = UINT32_C(0xf03f);
+
+		mrpt::reverseBytesInPlace(val);
+		uint32_t val_check = *reinterpret_cast<uint32_t*>(&val);
+		EXPECT_EQ(val_check, val_r_ok);
 	}
 }
