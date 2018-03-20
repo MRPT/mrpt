51caf1a270c96e0d2b981ad734510b4a2e4f16d6
Hunter Laux <hunterlaux@gmail.com>
2018-03-19 22:09:56 -0700

    Add round unittest

---
+++ libs/core/src/round_unittest.cpp
@@ -0,0 +1,16 @@
+/* +------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)            |
+   |                          http://www.mrpt.org/                          |
+   |                                                                        |
+   | Copyright (c) 2005-2018, Individual contributors, see AUTHORS file     |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                |
+   | Released under BSD License. See details in http://www.mrpt.org/License |
+   +------------------------------------------------------------------------+ */
+
+#include <mrpt/core/round.h>
+#include <gtest/gtest.h>
+
+TEST(round, longRound)
+{
+	EXPECT_EQ(mrpt::round_long(4.8), 5);
+}
