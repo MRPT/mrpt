e8b894bef750f9c1f21dd474e8fc4e318ac743a6
David <david.serret.mayer@gmail.com>
2018-04-18 16:17:53 +0200

    Adding setFramePubli

--- libs/hwdrivers/include/mrpt/hwdrivers/CVelodyneScanner.h
+++ libs/hwdrivers/include/mrpt/hwdrivers/CVelodyneScanner.h
@@ -216,7 +216,9 @@ class CVelodyneScanner : public mrpt::hwdrivers::CGenericSensor
 	double m_pos_packets_timing_timeout;
 	/** Default: "" (no IP-based filtering) */
 	std::string m_device_ip;
-	/** Default: true Output PCAP Info msgs */
+	/** Default: true Output whole frames and not data packets  */
+	bool m_return_frames;
+	/** Default: "" (do not operate from an offline file) */
 	bool m_pcap_verbose;
 	/** Default: "" (do not operate from an offline file) */
 	std::string m_pcap_input_file;
@@ -339,6 +341,12 @@ class CVelodyneScanner : public mrpt::hwdrivers::CGenericSensor
 	 * Requires setting a device IP address.
 	 * \return false on error*/
 	bool setLidarOnOff(bool on);
+	
+	/** Switches whole frame(points in a single revolution) on/off publication  
+	 * to datapacket publication. When on, getNextObservation() will return 
+	 * true whenever a frame is avaliable, when off, getNextObservation() will 
+	 * return true whenever a datapacket is avaliable.*/
+	void setFramePublishing(bool on);
 
 	/** @} */
 
