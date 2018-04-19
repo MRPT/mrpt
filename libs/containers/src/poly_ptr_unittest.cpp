abd013ca3ba57a73944bf1bf2853b70843cfb6a4
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2018-04-19 08:48:37 +0200

    restore commented-out tests after mrpt2 modules refactor

--- libs/containers/src/poly_ptr_unittest.cpp
+++ libs/containers/src/poly_ptr_unittest.cpp
@@ -9,9 +9,6 @@
 
 #include <mrpt/containers/deepcopy_ptr.h>
 #include <mrpt/core/common.h>  // TODO
-MRPT_TODO("Re-enable when mrpt-poses is ported!");
-#if 0
-
 #include <mrpt/poses/CPose2D.h>
 #include <gtest/gtest.h>
 
@@ -20,7 +17,7 @@ using namespace std;
 
 TEST(copy_ptr, SimpleOps)
 {
-	mrpt::utils::copy_ptr<int> ptr1;
+	mrpt::containers::copy_ptr<int> ptr1;
 	EXPECT_FALSE(ptr1);
 
 	ptr1.reset(new int());
@@ -30,14 +27,14 @@ TEST(copy_ptr, SimpleOps)
 	EXPECT_TRUE(*ptr1 == 123);
 
 	{
-		mrpt::utils::copy_ptr<int> ptr2 = ptr1;
+		mrpt::containers::copy_ptr<int> ptr2 = ptr1;
 		EXPECT_TRUE(*ptr1 == *ptr2);
 
 		(*ptr2)++;
 		EXPECT_FALSE(*ptr1 == *ptr2);
 	}
 	{
-		mrpt::utils::copy_ptr<int> ptr2;
+		mrpt::containers::copy_ptr<int> ptr2;
 		ptr2 = ptr1;
 		EXPECT_TRUE(*ptr1 == *ptr2);
 
@@ -48,7 +45,7 @@ TEST(copy_ptr, SimpleOps)
 
 TEST(copy_ptr, StlContainer)
 {
-	using str2d_ptr = mrpt::utils::copy_ptr<std::pair<std::string, int>>;
+	using str2d_ptr = mrpt::containers::copy_ptr<std::pair<std::string, int>>;
 
 	str2d_ptr ptr;
 	EXPECT_FALSE(ptr);
@@ -71,7 +68,7 @@ TEST(copy_ptr, StlContainer)
 
 TEST(poly_ptr, SimpleOps)
 {
-	mrpt::utils::poly_ptr<mrpt::poses::CPose2D> ptr1;
+	mrpt::containers::poly_ptr<mrpt::poses::CPose2D> ptr1;
 	EXPECT_FALSE(ptr1);
 
 	ptr1.reset(new mrpt::poses::CPose2D());
@@ -81,14 +78,14 @@ TEST(poly_ptr, SimpleOps)
 	EXPECT_NEAR(ptr1->x(), 123.0, 1e-9);
 
 	{
-		mrpt::utils::poly_ptr<mrpt::poses::CPose2D> ptr2 = ptr1;
+		mrpt::containers::poly_ptr<mrpt::poses::CPose2D> ptr2 = ptr1;
 		EXPECT_TRUE(*ptr1 == *ptr2);
 
 		ptr2->x_incr(1.0);
 		EXPECT_FALSE(*ptr1 == *ptr2);
 	}
 	{
-		mrpt::utils::poly_ptr<mrpt::poses::CPose2D> ptr2;
+		mrpt::containers::poly_ptr<mrpt::poses::CPose2D> ptr2;
 		ptr2 = ptr1;
 		EXPECT_TRUE(*ptr1 == *ptr2);
 
@@ -99,7 +96,7 @@ TEST(poly_ptr, SimpleOps)
 
 TEST(poly_ptr, StlContainer)
 {
-	using str2d_ptr = mrpt::utils::poly_ptr<mrpt::poses::CPose2D>;
+	using str2d_ptr = mrpt::containers::poly_ptr<mrpt::poses::CPose2D>;
 
 	str2d_ptr ptr;
 	EXPECT_FALSE(ptr);
@@ -118,4 +115,3 @@ TEST(poly_ptr, StlContainer)
 	EXPECT_NEAR(v3->x(), 4.0, 1e-9);
 	EXPECT_NEAR(v[3]->x(), 3.0, 1e-9);
 }
-#endif
