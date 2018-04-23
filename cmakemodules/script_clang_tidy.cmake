6df69d2e309b49466971920312c78c58c3eb4b78
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2018-04-23 09:48:50 +0200

    add cmake version check

--- cmakemodules/script_clang_tidy.cmake
+++ cmakemodules/script_clang_tidy.cmake
@@ -1,3 +1,4 @@
+if (CMAKE_VERSION VERSION_GREATER "3.6.0")
 find_program(
   CLANG_TIDY_EXE
   NAMES "clang-tidy"
@@ -9,5 +10,6 @@ else()
   message(STATUS "clang-tidy found: ${CLANG_TIDY_EXE}")
   set(DO_CLANG_TIDY "${CLANG_TIDY_EXE}" "-checks=-*,cppcoreguidelines-*")
 endif()
-
-
+else()
+message(STATUS "clang-tidy: not using. CMake version must be >=3.6")
+endif()
