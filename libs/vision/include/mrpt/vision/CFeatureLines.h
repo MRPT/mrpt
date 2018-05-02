cc891389771cb5c532ac00fad9d09e56808a0cd3
EduFdez <efernandezmoral@gmail.com>
2018-05-02 14:24:13 +0200

    New class CFeatureLines to extract line segments from images (ideally rectified).

---
+++ libs/vision/include/mrpt/vision/CFeatureLines.h
@@ -0,0 +1,43 @@
+/* +---------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)               |
+   |                          http://www.mrpt.org/                             |
+   |                                                                           |
+   | Copyright (c) 2005-2017, Individual contributors, see AUTHORS file        |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                   |
+   | Released under BSD License. See details in http://www.mrpt.org/License    |
+   +---------------------------------------------------------------------------+ */
+#ifndef CFeatureLines_H
+#define CFeatureLines_H
+
+#include <mrpt/vision/utils.h>
+//#include <mrpt/utils/CImage.h>
+#include <opencv2/core/core.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/core/eigen.hpp>
+//#include <pcl/point_types.h>
+
+namespace mrpt
+{
+    namespace vision
+    {
+        /**  This class wraps different line detectors and descriptors from OpenCV.
+          *
+          *  \ingroup mrpt_vision_grp
+          */
+        class VISION_IMPEXP CFeatureLines
+        {
+          public:
+            void extractLines (const cv::Mat & image,
+                                std::vector<cv::Vec4i> & segments,
+                                size_t threshold , const bool display = false);
+
+            void extractLines_CannyHough(const cv::Mat & canny_image,
+                                         const std::vector<cv::Vec2f> lines,
+                                         std::vector<cv::Vec4i> & segments,
+                                         size_t threshold );
+        }; // end of class
+
+    } // end of namespace
+} // end of namespace
+
+#endif
