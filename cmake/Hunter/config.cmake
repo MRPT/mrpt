08b807735f1eadb10c7c583e67b65f87cd823de7
Lukas Solanka <lsolanka@gmail.com>
2018-06-17 20:18:26 +0100

    mrpt compiles - but tests do not pass

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,31 @@
+# Copyright (c) 2013-2017, Lukas Solanka
+# All rights reserved.
+
+# Do not place header guards here
+
+# Unset:
+#   * ${PACKAGE_NAME}_ROOT (CMake variable)
+#   * ${PACKAGE_NAME}_ROOT (CMake cache variable)
+#   * ${PACKAGE_NAME}_ROOT (environment variable)
+
+# Set CMake variables:
+#   * HUNTER_${PACKAGE_NAME}_VERSION
+#   * HUNTER_${PACKAGE_NAME}_CMAKE_ARGS (optionally)
+
+# Usage:
+#   hunter_config(Foo VERSION 1.0.0)
+#   hunter_config(Boo VERSION 1.2.3z CMAKE_ARGS BOO_WITH_A=ON)
+
+# Wiki:
+#   * https://github.com/ruslo/hunter/wiki/dev.modules#hunter_config
+
+include(hunter_config)
+include(hunter_user_error)
+
+# NOTE: no names with spaces!
+
+hunter_config(OpenCV VERSION ${HUNTER_OpenCV_VERSION}
+    CMAKE_ARGS
+        WITH_OPENEXR=OFF
+        WITH_IPP=OFF
+) 
