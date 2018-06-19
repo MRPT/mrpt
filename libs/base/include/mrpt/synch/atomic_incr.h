ee63410f565197ca644601bae191aaa073373da7
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2018-06-19 19:05:00 +0200

    Undo 863c4ba
    It caused a serious regression (memory leaks)

--- libs/base/include/mrpt/synch/atomic_incr.h
+++ libs/base/include/mrpt/synch/atomic_incr.h
@@ -5,40 +5,62 @@
    | Copyright (c) 2005-2017, Individual contributors, see AUTHORS file        |
    | See: http://www.mrpt.org/Authors - All rights reserved.                   |
    | Released under BSD License. See details in http://www.mrpt.org/License    |
-   +---------------------------------------------------------------------------+
- */
-#ifndef mrpt_synch_atomicincr_H
-#define mrpt_synch_atomicincr_H
+   +---------------------------------------------------------------------------+ */
+#ifndef  mrpt_synch_atomicincr_H
+#define  mrpt_synch_atomicincr_H
 
-#include <mrpt/base/link_pragmas.h> // DLL import/export definitions
 #include <mrpt/config.h>
 #include <mrpt/utils/compiler_fixes.h>
+#include <mrpt/base/link_pragmas.h>  // DLL import/export definitions
 
-namespace mrpt {
-namespace synch {
+#if defined( __clang__ ) && defined( MRPT_OS_APPLE )
+   //no include
+#elif defined( __GNUC__ )
+#if ( __GNUC__ * 100 + __GNUC_MINOR__ >= 402 )
+#  include <ext/atomicity.h>
+#else
+#  include <bits/atomicity.h>
+#endif
+#endif
 
-/** This class acts exactly as an int (or long) variable, but with atomic
- * increment and decrement operators. This is a useful component of thread-safe
- * smart pointers. \ingroup synch_grp
- */
-class BASE_IMPEXP CAtomicCounter {
+
+namespace mrpt
+{
+namespace synch
+{
+
+/** This class acts exactly as an int (or long) variable, but with atomic increment and decrement operators.
+  *   This is a useful component of thread-safe smart pointers.
+  * \note Based on code from the Boost library.
+  * \ingroup synch_grp
+  */
+class BASE_IMPEXP CAtomicCounter
+{
 public:
-  CAtomicCounter();
-  ~CAtomicCounter();
-  explicit CAtomicCounter(long v);
+#if defined( __GNUC__ ) && !defined( MRPT_OS_APPLE )
+	typedef _Atomic_word atomic_num_t;
+#elif defined(_WIN32) // mostly for MSVC in Windows
+	typedef long atomic_num_t;
+#else
+	typedef int atomic_num_t;
+#endif
 
-  void operator++();     //!< Atomic increment of value.
-  long operator--();     //!< Atomic decrement of value and return new value.
-  operator long() const; //!< Get current value
+	explicit CAtomicCounter( long v ): m_value( static_cast<atomic_num_t>(v) )
+	{ }
+
+	void operator++();  //!< Atomic increment of value.
+	atomic_num_t operator--(); 	//!< Atomic decrement of value and return new value.
+	operator atomic_num_t() const; //!< Get current value
 
 private:
-  long m_atomic[3];
+	mutable atomic_num_t m_value;
 
-  CAtomicCounter(CAtomicCounter const &);            //!< Forbidden method
-  CAtomicCounter &operator=(CAtomicCounter const &); //!< Forbidden method
-};                                                   // end of CAtomicCounter
+	CAtomicCounter( CAtomicCounter const & );	//!< Forbidden method
+	CAtomicCounter & operator=( CAtomicCounter const & ); 	//!< Forbidden method
+}; // end of CAtomicCounter
 
-} // namespace synch
-} // namespace mrpt
+
+} // End of namespace
+} // End of namespace
 
 #endif
--- libs/base/src/synch/atomic_incr.cpp
+++ libs/base/src/synch/atomic_incr.cpp
@@ -5,27 +5,106 @@
    | Copyright (c) 2005-2017, Individual contributors, see AUTHORS file        |
    | See: http://www.mrpt.org/Authors - All rights reserved.                   |
    | Released under BSD License. See details in http://www.mrpt.org/License    |
-   +---------------------------------------------------------------------------+
- */
+   +---------------------------------------------------------------------------+ */
 
-#include "base-precomp.h" // Precompiled headers
+#include "base-precomp.h"  // Precompiled headers
 
 #include <mrpt/synch/atomic_incr.h>
 
-#include <atomic>
-
 using namespace mrpt::synch;
 
-#define MY_ATOMIC reinterpret_cast<std::atomic_long *>(m_atomic)
-#define MY_ATOMIC_CONST reinterpret_cast<const std::atomic_long *>(m_atomic)
+#ifdef MRPT_OS_WINDOWS
+	#include <windows.h>
+#elif defined( __clang__ ) && defined( MRPT_OS_APPLE )
+	//no include
+#elif defined( __GNUC__ )
+	#if ( __GNUC__ * 100 + __GNUC_MINOR__ >= 402 )
+	#  include <ext/atomicity.h>
+	#else
+	#  include <bits/atomicity.h>
+	#endif
+#else
+	#error This is not Windows and compiler is not GCC: Non implemented atomic_incr for this case
+#endif
 
-CAtomicCounter::CAtomicCounter() {
-  static_assert(sizeof(m_atomic) >= sizeof(std::atomic_long));
-  new (MY_ATOMIC) std::atomic_long();
-}
-CAtomicCounter::~CAtomicCounter() { (MY_ATOMIC)->~atomic<long>(); }
-CAtomicCounter::CAtomicCounter(long v) { new (MY_ATOMIC) std::atomic_long(v); }
 
-void CAtomicCounter::operator++() { (*MY_ATOMIC)++; }
-long CAtomicCounter::operator--() { return (*MY_ATOMIC)--; }
-CAtomicCounter::operator long() const { return (*MY_ATOMIC_CONST); }
+#ifdef MRPT_OS_WINDOWS
+	void CAtomicCounter::operator++()
+	{
+		InterlockedIncrement(&m_value);
+	}
+
+	CAtomicCounter::atomic_num_t CAtomicCounter::operator--()
+	{
+		return InterlockedDecrement(&m_value);
+	}
+
+	CAtomicCounter::operator CAtomicCounter::atomic_num_t() const
+	{
+		return static_cast<long const volatile &>( m_value );
+	}
+
+#else
+	// This should be GCC:
+	#if ( defined( __i386__ ) || defined( __x86_64__ ) )
+		void CAtomicCounter::operator++()
+		{
+			__asm__
+			(
+				"lock\n\t"
+				"incl %0":
+				"+m"( m_value ): // output (%0)
+				: // inputs
+				"cc" // clobbers
+			);
+		}
+
+		int my_atomic_exchange_and_add( int * pw, int dv )
+		{
+			// int r = *pw;
+			// *pw += dv;
+			// return r;
+			int r;
+			__asm__ __volatile__
+			(
+				"lock\n\t"
+				"xadd %1, %0":
+				"+m"( *pw ), "=r"( r ): // outputs (%0, %1)
+				"1"( dv ): // inputs (%2 == %1)
+				"memory", "cc" // clobbers
+			);
+			return r;
+		}
+
+		CAtomicCounter::atomic_num_t CAtomicCounter::operator--()
+		{
+			return my_atomic_exchange_and_add( &m_value, -1 ) - 1;
+		}
+
+		CAtomicCounter::operator CAtomicCounter::atomic_num_t() const
+		{
+			return my_atomic_exchange_and_add( &m_value, 0 );
+		}
+
+	#else
+		#if defined(__GLIBCXX__) // g++ 3.4+
+		using __gnu_cxx::__atomic_add;
+		using __gnu_cxx::__exchange_and_add;
+		#endif
+
+		void CAtomicCounter::operator++()
+		{
+			__atomic_add(&m_value, 1);
+		}
+
+		CAtomicCounter::atomic_num_t CAtomicCounter::operator--()
+		{
+			return __exchange_and_add(&m_value, -1) - 1;
+		}
+
+		CAtomicCounter::operator CAtomicCounter::atomic_num_t() const
+		{
+			return __exchange_and_add(&m_value, 0);
+		}
+	#endif
+#endif
