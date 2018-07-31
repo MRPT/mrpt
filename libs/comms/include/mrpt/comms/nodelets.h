db9baddc55ecb05cdb83be489d6a835d65e84f0f
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2018-08-01 01:35:08 +0200

    Add nodelets-like pub/sub mechanism for intra-process comms
    Progress on #585

---
+++ libs/comms/include/mrpt/comms/nodelets.h
@@ -0,0 +1,146 @@
+/* +------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)            |
+   |                          http://www.mrpt.org/                          |
+   |                                                                        |
+   | Copyright (c) 2005-2018, Individual contributors, see AUTHORS file     |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                |
+   | Released under BSD License. See details in http://www.mrpt.org/License |
+   +------------------------------------------------------------------------+ */
+#pragma once
+
+#include <memory>  // shared_ptr
+#include <unordered_map>
+#include <mutex>
+#include <list>
+#include <any>
+#include <iostream>
+#include <functional>
+#include <mrpt/typemeta/TTypeName.h>
+#include <mrpt/typemeta/TTypeName_stl.h>
+
+namespace mrpt
+{
+namespace comms
+{
+/** Nodelet-like Pub/Sub communications (in #include <mrpt/comms/nodelets.h>)
+ * \ingroup mrpt_comms_grp
+ * @{ */
+
+class Subscriber
+{
+   private:
+	Subscriber(
+		std::function<void(const std::any&)>&& func,
+		std::function<void()>&& cleanup);
+
+   public:
+	using Ptr = std::shared_ptr<Subscriber>;
+	~Subscriber();
+
+	static Ptr create(
+		std::function<void(const std::any&)>&& func,
+		std::function<void()>&& cleanup);
+
+	void pub(const std::any& a);
+
+   private:
+	std::function<void(const std::any&)> m_func;
+	std::function<void()> m_cleanup;
+};
+
+class Topic : public std::enable_shared_from_this<Topic>
+{
+   private:
+	Topic(std::function<void()>&& cleanup);
+
+   public:
+	using Ptr = std::shared_ptr<Topic>;
+
+	~Topic();
+
+	template <typename ARG>
+	Subscriber::Ptr createSubscriber(
+		typename std::function<void(const ARG&)>&& func)
+	{
+		std::lock_guard<std::mutex> lock(m_mutex);
+		m_subs.emplace_back();
+		auto it = m_subs.end();
+		it--;
+		auto capturedShared = shared_from_this();
+		auto newNode = Subscriber::create(
+			[=](const std::any& anyArg) {
+				try
+				{
+					func(std::any_cast<const ARG&>(anyArg));
+				}
+				catch (std::bad_any_cast&)
+				{
+					std::cerr << "Subscriber has wrong type: "
+							  << mrpt::typemeta::TTypeName<ARG>::get()
+							  << std::endl;
+				}
+			},
+			// cleanup function
+			[=] {
+				// list operations don't invalidate iterators
+				// This iterator capture will be valid
+				capturedShared->cleanupSubscriber(it);
+			});
+		*it = newNode;
+		return newNode;
+	}
+
+	void publish(const std::any& any);
+	void cleanupSubscriber(std::list<std::weak_ptr<Subscriber>>::iterator it);
+
+	template <typename CLEANUP>
+	static Ptr create(CLEANUP&& cleanup)
+	{
+		return Ptr(new Topic(std::forward<CLEANUP>(cleanup)));
+	}
+
+   private:
+	std::mutex m_mutex;
+	std::list<std::weak_ptr<Subscriber>> m_subs;
+	std::function<void()> m_cleanup;
+};
+
+/** The central directory of existing topics for pub/sub */
+class TopicDirectory : public std::enable_shared_from_this<TopicDirectory>
+{
+   private:
+	TopicDirectory();
+
+   public:
+	using Ptr = std::shared_ptr<TopicDirectory>;
+
+	template <typename PATH>
+	Topic::Ptr getTopic(PATH&& path)
+	{
+		std::lock_guard<std::mutex> lock(m_mutex);
+		auto it = m_mapService.find(path);
+		if (it != m_mapService.end())
+		{
+			auto ptr = it->second.lock();
+			if (ptr) return ptr;
+		}
+
+		auto capturedShared = shared_from_this();
+		auto newNode =
+			Topic::create([=]() { capturedShared->cleanupTopic(path); });
+		m_mapService.insert({path, newNode});
+		return newNode;
+	}
+
+	void cleanupTopic(const std::string& key);
+	static Ptr create();
+
+   private:
+	std::mutex m_mutex;
+	std::unordered_map<std::string, std::weak_ptr<Topic>> m_mapService;
+};
+
+/** @} */  // end grouping
+
+}  // namespace comms
+}  // namespace mrpt
---
+++ libs/comms/src/nodelets.cpp
@@ -0,0 +1,63 @@
+/* +------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)            |
+   |                          http://www.mrpt.org/                          |
+   |                                                                        |
+   | Copyright (c) 2005-2018, Individual contributors, see AUTHORS file     |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                |
+   | Released under BSD License. See details in http://www.mrpt.org/License |
+   +------------------------------------------------------------------------+ */
+
+#include "comms-precomp.h"  // Precompiled headers
+
+#include <mrpt/comms/nodelets.h>
+
+using namespace mrpt::comms;
+
+// ------- Subscriber --------------
+Subscriber::Subscriber(
+	std::function<void(const std::any&)>&& func,
+	std::function<void()>&& cleanup)
+	: m_func(std::move(func)), m_cleanup(std::move(cleanup))
+{
+}
+
+Subscriber::~Subscriber() { m_cleanup(); }
+
+Subscriber::Ptr Subscriber::create(
+	std::function<void(const std::any&)>&& func,
+	std::function<void()>&& cleanup)
+{
+	return Ptr(new Subscriber(std::move(func), std::move(cleanup)));
+}
+
+void Subscriber::pub(const std::any& a) { m_func(a); }
+
+// ------- Topic --------------
+Topic::Topic(std::function<void()>&& cleanup) : m_cleanup(std::move(cleanup)) {}
+Topic::~Topic() { m_cleanup(); }
+
+void Topic::publish(const std::any& any)
+{
+	std::lock_guard<std::mutex> lock(m_mutex);
+	for (auto& sub : m_subs)
+	{
+		sub.lock()->pub(any);
+	}
+}
+
+void Topic::cleanupSubscriber(std::list<std::weak_ptr<Subscriber>>::iterator it)
+{
+	std::lock_guard<std::mutex> lock(m_mutex);
+	m_subs.erase(it);
+}
+
+// -------------- TopicDirectory ---------------------
+TopicDirectory::TopicDirectory() {}
+
+void TopicDirectory::cleanupTopic(const std::string& key)
+{
+	std::lock_guard<std::mutex> lock(m_mutex);
+	m_mapService.erase(m_mapService.find(key));
+}
+
+TopicDirectory::Ptr TopicDirectory::create() { return Ptr(new TopicDirectory); }
