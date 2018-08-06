fd663c92d89673c7ac501a6d72094ed6d79e11ab
Lukas Solanka <lsolanka@gmail.com>
2018-08-06 23:38:19 +0100

    add cusom FindwxWidgets.cmake

--- cmakemodules/script_wxwidgets.cmake
+++ cmakemodules/script_wxwidgets.cmake
@@ -27,6 +27,7 @@ endif(msvc)
 
 
 # We need the Find package for wxWidgets to work
+message(STATUS "CMAKE_FIND_ROOT_PATH: ${CMAKE_FIND_ROOT_PATH}")
 FIND_PACKAGE(wxWidgets COMPONENTS ${wxWidgets_MRPT_COMPONENTS_TO_SEARCH})
 # Did we find wxWidgets ? This condition will fail for as long as the internal vars do not point to the proper wxWidgets configuration
 IF(wxWidgets_FOUND)
