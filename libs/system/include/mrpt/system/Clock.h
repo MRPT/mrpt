e0d013fe7139a87537b58b26a7be2a63561fe5bd
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2018-08-12 10:41:15 +0200

    doxygen and formatting

--- libs/system/include/mrpt/system/Clock.h
+++ libs/system/include/mrpt/system/Clock.h
@@ -12,11 +12,12 @@
 
 namespace mrpt::system
 {
-
-// Clock that is compatible with TTimeStamp representation
+/** Clock that is compatible with TTimeStamp representation
+ * \ingroup mrpt_system_grp
+ */
 class Clock
 {
-public:
+   public:
 	using rep = int64_t;
 	// 100-nanoseconds
 	using period = std::ratio<1, 10000000>;
@@ -25,23 +26,26 @@ public:
 
 	static constexpr bool is_steady = std::chrono::system_clock::is_steady;
 
-	static time_point now() noexcept{
-		duration d = std::chrono::duration_cast<duration>(std::chrono::system_clock::now().time_since_epoch());
-		d += std::chrono::seconds(11644473600); //TTimeStamp offset constant
+	static time_point now() noexcept
+	{
+		duration d = std::chrono::duration_cast<duration>(
+			std::chrono::system_clock::now().time_since_epoch());
+		d += std::chrono::seconds(11644473600);  // TTimeStamp offset constant
 		return time_point(d);
 	}
 };
 
 /** Returns the time difference from t1 to t2 (positive if t2 is posterior to
- * t1), in seconds \sa secondsToTimestamp */
+ * t1), in seconds \sa secondsToTimestamp
+ * \ingroup mrpt_system_grp
+ */
 double timeDifference(
-	const mrpt::system::Clock::time_point &t_first,
-	const mrpt::system::Clock::time_point &t_later);
-
+	const mrpt::system::Clock::time_point& t_first,
+	const mrpt::system::Clock::time_point& t_later);
 
 inline Clock::time_point toTimePoint(int64_t time)
 {
 	return Clock::time_point(Clock::duration(time));
 }
 
-}
+}  // namespace mrpt::system
