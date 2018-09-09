4f7e7f138555f352791135c238d7de93b444f010
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2018-09-09 23:21:27 +0200

    clang-tidy: change default set of checks

--- cmakemodules/script_clang_tidy.cmake
+++ cmakemodules/script_clang_tidy.cmake
@@ -8,7 +8,15 @@ if(NOT CLANG_TIDY_EXE)
   message(STATUS "clang-tidy not found.")
 else()
   message(STATUS "clang-tidy found: ${CLANG_TIDY_EXE}")
-  set(DO_CLANG_TIDY "${CLANG_TIDY_EXE}" "-checks=-*,cppcoreguidelines-*")
+  # cppcoreguidelines checks: not all for now...
+#  cppcoreguidelines-no-malloc
+#  cppcoreguidelines-owning-memory
+#  cppcoreguidelines-pro-bounds-array-to-pointer-decay
+#  cppcoreguidelines-pro-bounds-constant-array-index
+#  cppcoreguidelines-pro-type-const-cast
+#  cppcoreguidelines-pro-type-vararg  (used A LOT in mrpt::format()...)
+  set(CLANG_TIDY_CHECKS "-checks=-*,bugprone-*,cppcoreguidelines-pro-bounds-pointer-arithmetic,cppcoreguidelines-c-copy-assignment-signature,cppcoreguidelines-interfaces-global-init,cppcoreguidelines-pro-type-cstyle-cast,cppcoreguidelines-pro-type-member-init,cppcoreguidelines-pro-type-reinterpret-cast,cppcoreguidelines-pro-type-static-cast-downcast,cppcoreguidelines-pro-type-union-access,cppcoreguidelines-slicing,cppcoreguidelines-special-member-functions" CACHE STRING "clang tidy cheks to run")
+  set(DO_CLANG_TIDY "${CLANG_TIDY_EXE}" "${CLANG_TIDY_CHECKS}")
 endif()
 else()
 message(STATUS "clang-tidy: not using. CMake version must be >=3.6")
