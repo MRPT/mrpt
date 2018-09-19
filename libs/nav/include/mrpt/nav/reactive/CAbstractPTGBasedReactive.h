2af419a98ee648d82858b719079b1638ff6045c8
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2018-09-20 00:24:07 +0200

    Add virtual method getHoloMethod()

--- libs/nav/include/mrpt/nav/reactive/CAbstractPTGBasedReactive.h
+++ libs/nav/include/mrpt/nav/reactive/CAbstractPTGBasedReactive.h
@@ -211,6 +211,7 @@ namespace mrpt
 		/** The main method for the navigator */
 		virtual void performNavigationStep() MRPT_OVERRIDE;
 
+		virtual CAbstractHolonomicReactiveMethod* getHoloMethod(int idx);
 		std::vector<CAbstractHolonomicReactiveMethod*>  m_holonomicMethod;   //!< The holonomic navigation algorithm (one object per PTG, so internal states are maintained)
 		mrpt::utils::CStream  *m_logFile, *m_prev_logfile;         //!< The current log file stream, or NULL if not being used
 		bool                   m_enableKeepLogRecords; //!< See enableKeepLogRecords
