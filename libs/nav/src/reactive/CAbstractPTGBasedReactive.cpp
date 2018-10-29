7e899efa16ddb61cbf9e2aeb8583058b97d45086
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2018-10-29 10:08:35 +0100

    Avoid potential call to pure virtual function
    Addresses #850

--- libs/nav/src/reactive/CAbstractPTGBasedReactive.cpp
+++ libs/nav/src/reactive/CAbstractPTGBasedReactive.cpp
@@ -90,7 +90,10 @@ void CAbstractPTGBasedReactive::preDestructor()
 
 	// Just in case.
 	try {
-		this->stop(false /*not emergency*/);
+		// Call base class method, NOT the generic, virtual one, 
+		// to avoid problems if we are in the dtor, while the vtable 
+		// is being destroyed.
+		CAbstractNavigator::stop(false /*not emergency*/);
 	} catch (...) { }
 
 	mrpt::utils::delete_safe(m_logFile);
