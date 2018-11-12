ecd36b2dcd0e477c7774c8a7d53a5278947145b2
Jose Luis Blanco-Claraco <jlblanco@ual.es>
2018-11-12 16:14:41 +0100

    add missing PCH for vision-lgpl

---
+++ libs/vision-lgpl/src/vision-lgpl-precomp.cpp
@@ -0,0 +1,10 @@
+/* +------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)            |
+   |                          http://www.mrpt.org/                          |
+   |                                                                        |
+   | Copyright (c) 2005-2018, Individual contributors, see AUTHORS file     |
+   | See: http://www.mrpt.org/Authors - All rights reserved.                |
+   | Released under BSD License. See details in http://www.mrpt.org/License |
+   +------------------------------------------------------------------------+ */
+
+#include "vision-lgpl-precomp.h"  // Precompiled headers
