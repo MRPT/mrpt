83ecf32db7439246dd55d799e5ab76b8afd57599
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2018-11-13 11:35:50 +0100

    Fix missing Eigen3::Eigen target in u16.04
    Create our own version of the imported target in u16.04, where it does not exist.

--- cmakemodules/script_eigen.cmake
+++ cmakemodules/script_eigen.cmake
@@ -20,14 +20,13 @@ if (EIGEN_USE_EMBEDDED_VERSION)
 	  SOURCE_DIR        "${MRPT_BINARY_DIR}/otherlibs/eigen3/"
 	  CONFIGURE_COMMAND ""
 	  BUILD_COMMAND     ""
-	  INSTALL_COMMAND   ""
 	  TEST_COMMAND      ""
 	)
 
 	set(EIGEN_EMBEDDED_INCLUDE_DIR "${MRPT_BINARY_DIR}/otherlibs/eigen3/" CACHE PATH "Eigen path for embedded use" FORCE)
 	mark_as_advanced(EIGEN_EMBEDDED_INCLUDE_DIR)
 
-	set(MRPT_EIGEN_INCLUDE_DIR "${EIGEN_EMBEDDED_INCLUDE_DIR}")
+	set(MRPT_EIGEN_INCLUDE_DIR "${EIGEN_EMBEDDED_INCLUDE_DIR}")  # only to find out version
 	# define interface lib for Eigen3:
 	add_library(Eigen INTERFACE)
 	add_dependencies(Eigen EP_eigen3)
@@ -44,14 +43,25 @@ if (EIGEN_USE_EMBEDDED_VERSION)
 		SYSTEM  # omit warnings for these hdrs
 		INTERFACE
 	  $<BUILD_INTERFACE:${EIGEN_EMBEDDED_INCLUDE_DIR}>
-		$<BUILD_INTERFACE:${EIGEN_EMBEDDED_INCLUDE_DIR}/unsupported>
+	  $<BUILD_INTERFACE:${EIGEN_EMBEDDED_INCLUDE_DIR}/unsupported>
 #	  $<INSTALL_INTERFACE:include/Eigen>
 	)
 	add_library(Eigen3::Eigen ALIAS Eigen)
 
 elseif(Eigen3_FOUND)
 	# Use system version
-	set(MRPT_EIGEN_INCLUDE_DIR "${EIGEN3_INCLUDE_DIR}")
+	set(MRPT_EIGEN_INCLUDE_DIR "${EIGEN3_INCLUDE_DIR}") # only to find out version
+
+	# For Ubuntu 16.04, the Eigen version does not support imported target,
+	# so create it ourselves:
+	if (NOT TARGET Eigen3::Eigen)
+		add_library(Eigen3::Eigen INTERFACE IMPORTED GLOBAL)
+		set_target_properties(Eigen3::Eigen PROPERTIES
+			INTERFACE_INCLUDE_DIRECTORIES
+				$<BUILD_INTERFACE:${EIGEN3_INCLUDE_DIR}>
+#				$<INSTALL_INTERFACE:include/Eigen>
+ 		)
+	endif()
 else()
 	message(FATAL_ERROR "eigen3 is required to build MRPT! Either install it and set EIGEN3_DIR or enable the variable EIGEN_USE_EMBEDDED_VERSION to automatically download it now.")
 endif()
