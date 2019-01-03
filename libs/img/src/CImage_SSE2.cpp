56db00b5eb1f15e47e596174f7cdef262198b045
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-01-03 14:27:30 +0100

    CImage SSEx: support w!=0x0f*N

--- libs/img/src/CImage_SSE2.cpp
+++ libs/img/src/CImage_SSE2.cpp
@@ -34,17 +34,18 @@
  * ignoring the other 3
  *  - <b>Input format:</b> uint8_t, 1 channel
  *  - <b>Output format:</b> uint8_t, 1 channel
- *  - <b>Preconditions:</b> in & out aligned to 16bytes, w = k*16 (w=width in
- * pixels), widthStep=w*1
+ *  - <b>Preconditions:</b> in & out aligned to 16bytes, step = k*16
  *  - <b>Notes:</b>
  *  - <b>Requires:</b> SSE2
  *  - <b>Invoked from:</b> mrpt::img::CImage::scaleHalf()
  */
-void image_SSE2_scale_half_1c8u(const uint8_t* in, uint8_t* out, int w, int h)
+void image_SSE2_scale_half_1c8u(
+    const uint8_t* in, uint8_t* out, int w, int h, size_t step_in,
+    size_t step_out)
 {
 	alignas(32) const unsigned long long mask[2] = {0x00FF00FF00FF00FFull,
 													0x00FF00FF00FF00FFull};
-	const __m128i m = _mm_load_si128((const __m128i*)mask);
+	const __m128i m = _mm_load_si128(reinterpret_cast<const __m128i*>(mask));
 
 	int sw = w >> 4;
 	int sh = h >> 1;
@@ -53,78 +54,66 @@ void image_SSE2_scale_half_1c8u(const uint8_t* in, uint8_t* out, int w, int h)
 	{
 		for (int j = 0; j < sw; j++)
 		{
-			const __m128i here_sampled =
-				_mm_and_si128(_mm_load_si128((const __m128i*)in), m);
+			const uint8_t* inp = in;
+			uint8_t* outp = out;
+
+			const __m128i here_sampled = _mm_and_si128(
+			    _mm_load_si128(reinterpret_cast<const __m128i*>(inp)), m);
 			_mm_storel_epi64(
-				(__m128i*)out, _mm_packus_epi16(here_sampled, here_sampled));
-			in += 16;
-			out += 8;
+			    reinterpret_cast<__m128i*>(outp),
+			    _mm_packus_epi16(here_sampled, here_sampled));
+			inp += 16;
+			outp += 8;
 		}
-		in += w;
+		in += 2 * step_in;  // Skip one row
+		out += step_out;
 	}
 }
 
 /** Average each 2x2 pixels into 1x1 pixel (arithmetic average)
  *  - <b>Input format:</b> uint8_t, 1 channel
  *  - <b>Output format:</b> uint8_t, 1 channel
- *  - <b>Preconditions:</b> in & out aligned to 16bytes, w = k*16 (w=width in
- * pixels), widthStep=w*1
+ *  - <b>Preconditions:</b> in & out aligned to 16bytes, step = k*16
  *  - <b>Notes:</b>
  *  - <b>Requires:</b> SSE2
  *  - <b>Invoked from:</b> mrpt::img::CImage::scaleHalfSmooth()
  */
 void image_SSE2_scale_half_smooth_1c8u(
-	const uint8_t* in, uint8_t* out, int w, int h)
+    const uint8_t* in, uint8_t* out, int w, int h, size_t step_in,
+    size_t step_out)
 {
 	alignas(MRPT_MAX_ALIGN_BYTES) const unsigned long long mask[2] = {
 		0x00FF00FF00FF00FFull, 0x00FF00FF00FF00FFull};
-	const uint8_t* nextRow = in + w;
-	__m128i m = _mm_load_si128((const __m128i*)mask);
+	__m128i m = _mm_load_si128(reinterpret_cast<const __m128i*>(mask));
 	int sw = w >> 4;
 	int sh = h >> 1;
 
 	for (int i = 0; i < sh; i++)
 	{
+		const uint8_t* inp = in;
+		uint8_t* outp = out;
+
+		const uint8_t* nextRow = in + w;
 		for (int j = 0; j < sw; j++)
 		{
-			__m128i here = _mm_load_si128((const __m128i*)in);
-			__m128i next = _mm_load_si128((const __m128i*)nextRow);
+			__m128i here =
+			    _mm_load_si128(reinterpret_cast<const __m128i*>(inp));
+			__m128i next =
+			    _mm_load_si128(reinterpret_cast<const __m128i*>(nextRow));
 			here = _mm_avg_epu8(here, next);
 			next = _mm_and_si128(_mm_srli_si128(here, 1), m);
 			here = _mm_and_si128(here, m);
 			here = _mm_avg_epu16(here, next);
-			_mm_storel_epi64((__m128i*)out, _mm_packus_epi16(here, here));
-			in += 16;
+			_mm_storel_epi64(
+			    reinterpret_cast<__m128i*>(outp), _mm_packus_epi16(here, here));
+			inp += 16;
 			nextRow += 16;
-			out += 8;
+			outp += 8;
 		}
-
-		in += w;
-		nextRow += w;
+		in += 2 * step_in;  // Skip one row
+		out += step_out;
 	}
 }
-
-/** KLT score at a given point of a grayscale image.
- *  - <b>Requires:</b> SSE2
- *  - <b>Invoked from:</b> mrpt::img::CImage::KLT_response()
- *
- *  This function is not manually optimized for SSE2 but templatized for
- * different
- *   window sizes such as the compiler can optimize automatically for that
- * size.
- *
- *  Only for the most common window sizes this templates are instantiated
- * (W=[2-16] and W=32 ),
- *   falling back to
- *   a generic implementation otherwise. The next figure shows the performance
- * (time for
- *   KLT_response() to compute the score for one single pixel) for different
- * window sizes.
- *
- *  <img src="KLT_response_performance_SSE2.png" >
- *
- */
-float KLT_response_optimized();
 
 // TODO:
 // Sum of absolute differences: Use  _mm_sad_epu8
