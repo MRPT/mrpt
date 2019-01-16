bf039b8d57bcfc52769eb96d679d9812efbbbe20
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-01-16 11:56:54 +0100

    Use cmake namespace names in mrpt libraries
    Now, all imported libraries are named `mrpt::${module}` instead of the
    old `mrpt-module`.
    This is consistent with "modern cmake".
    
    OTOH, I added a MRPT-1.5 backwards-compatible `mrpt-config.cmake` which
    allows old cmake scripts to transparently continue working, by setting
    the new `mrpt::xxx` imported targets in the returned variable
    `MRPT_LIBRARIES`.

--- cmakemodules/DeclareAppDependencies.cmake
+++ cmakemodules/DeclareAppDependencies.cmake
@@ -13,16 +13,16 @@ macro(DeclareAppDependencies name)
 	set(ALL_DEPS "")
 
 	foreach(DEP ${ARGN})
-		list(APPEND ALL_DEPS ${DEP})
-	endforeach(DEP)
-
-	foreach(DEP ${ARGN})
 		# Only for "mrpt-XXX" libs:
-		if (${DEP} MATCHES "mrpt-")
+		if (DEP MATCHES "^mrpt-?:*(.*)")
+			# First, add the lib itself:
+			string(REGEX REPLACE "mrpt-?:*(.*)" "\\1" DEP_MRPT_NAME ${DEP})
+			list(APPEND ALL_DEPS mrpt::${DEP_MRPT_NAME})
+			# now, their dependencies:
 			get_property(LIB_DEP GLOBAL PROPERTY "${DEP}_LIB_DEPS")
 			list(APPEND ALL_DEPS ${LIB_DEP})
-		endif (${DEP} MATCHES "mrpt-")
-	endforeach(DEP)
+		endif ()
+	endforeach()
 
 
 	# Add the detected dependencies:
@@ -42,19 +42,19 @@ macro(DeclareAppDependencies name)
 				list(APPEND AUX_EXTRA_LINK_LIBS
 					optimized ${MRPT_LIB_PREFIX}${DEP}${MRPT_DLL_VERSION_POSTFIX}
 					debug     ${MRPT_LIB_PREFIX}${DEP}${MRPT_DLL_VERSION_POSTFIX}${CMAKE_DEBUG_POSTFIX})
-			endif (NOT _LIB_HDRONLY)
+			endif ()
 
 			# Include:
-			string(REGEX REPLACE "mrpt-(.*)" "\\1" DEP_MRPT_NAME ${_DEP})
+			string(REGEX REPLACE "mrpt::(.*)" "\\1" DEP_MRPT_NAME ${_DEP})
 			if(NOT "${DEP_MRPT_NAME}" STREQUAL "")
 				include_directories("${MRPT_LIBS_ROOT}/${DEP_MRPT_NAME}/include/")
-			endif(NOT "${DEP_MRPT_NAME}" STREQUAL "")
+			endif()
 
 			# Check if all dependencies are to be build:
 			if (${BUILD_mrpt-${DEP_MRPT_NAME}} STREQUAL "OFF")
 				set(AUX_ALL_DEPS_BUILD 0)
 				message(STATUS "*Warning*: App ${name} cannot be built because dependency mrpt-${DEP_MRPT_NAME} has been disabled!")
-			endif (${BUILD_mrpt-${DEP_MRPT_NAME}} STREQUAL "OFF")
+			endif ()
 
 		endforeach (_DEP)
 	endif ()
@@ -64,18 +64,18 @@ macro(DeclareAppDependencies name)
 		message(STATUS "*Warning* ==> Forcing BUILD_APP_${name}=OFF for missing dependencies listed above (re-enable manually if needed).")
 		set(BUILD_APP_${name} OFF CACHE BOOL "Build ${name}" FORCE) # this var is checked in [MRPT]/app/CMakeLists.txt
 		mark_as_advanced(CLEAR BUILD_APP_${name})
-	endif (NOT AUX_ALL_DEPS_BUILD)
+	endif ()
 
 	# We need pthread's on unices
 	target_link_libraries(${name} Threads::Threads)
 
-endmacro(DeclareAppDependencies)
+endmacro()
 
 # Macro for adding links to the Start menu folder (for binary packages in Windows)
 macro(AppStartMenuLink name title)
 	get_property(_str GLOBAL PROPERTY "MRPT_CPACK_PACKAGE_EXECUTABLES")
 	set_property(GLOBAL PROPERTY "MRPT_CPACK_PACKAGE_EXECUTABLES" "${_str}${name};${title};")
-endmacro(AppStartMenuLink)
+endmacro()
 
 
 macro(DeclareAppForInstall name)
@@ -83,4 +83,4 @@ install(TARGETS ${name}
 	RUNTIME DESTINATION ${mrpt_apps_INSTALL_PREFIX}bin COMPONENT Apps
 	LIBRARY DESTINATION ${mrpt_apps_INSTALL_PREFIX}lib${LIB_SUFFIX} COMPONENT Apps
 	ARCHIVE DESTINATION ${mrpt_apps_INSTALL_PREFIX}lib${LIB_SUFFIX} COMPONENT Apps)
-endmacro(DeclareAppForInstall)
+endmacro()
--- parse-files/MRPTConfig.cmake
+++
@@ -1,6 +0,0 @@
-# ** DEPRECATED **
-MESSAGE(FATAL_ERROR
-  "find_package(MRPT ... module_name) is **deprecated**."
-  "Replace by: find_package(mrpt-module_name)"
-  "Please, read: https://docs.mrpt.org/reference/devel/mrpt_from_cmake.html"
-)
