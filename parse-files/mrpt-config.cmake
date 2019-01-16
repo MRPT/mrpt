f357f00c759b992a0bafccdd32fccb172cf00815
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-01-16 11:56:54 +0100

    Use cmake namespace names in mrpt libraries
    Now, all imported libraries are named `mrpt::${module}` instead of the
    old `mrpt-module`.
    This is consistent with "modern cmake".
    
    OTOH, I added a MRPT-1.5 backwards-compatible `mrpt-config.cmake` which
    allows old cmake scripts to transparently continue working, by setting
    the new `mrpt::xxx` imported targets in the returned variable
    `MRPT_LIBRARIES`.

---
+++ parse-files/mrpt-config.cmake
@@ -0,0 +1,36 @@
+# =========================================================================
+#  The Mobile Robot Programming Toolkit (MRPT) CMake configuration file
+#
+#             ** File generated automatically, do not modify **
+#
+#  Please, read:
+#  https://docs.mrpt.org/reference/devel/mrpt_from_cmake.html
+
+#   In your CMakeLists.txt, add these lines:
+#
+#    find_package(MRPT REQUIRED
+#       COMPONENTS slam nav
+#       OPTIONAL_COMPONENTS  vision)
+#    target_link_libraries(MY_TARGET_NAME ${MRPT_LIBRARIES})
+#
+#   or do it individually:
+#    find_package(mrpt-slam)
+#    find_package(mrpt-nav)
+#    target_link_libraries(MY_TARGET_NAME mrpt::slam mrpt::nav)
+# =========================================================================
+include(CMakeFindDependencyMacro)
+
+set(MRPT_LIBRARIES "")
+foreach(_comp ${MRPT_FIND_COMPONENTS})
+  if (MRPT_FIND_REQUIRED_${_comp})
+    find_dependency(mrpt-${_comp})
+  else()
+    find_package(mrpt-${_comp} CONFIG QUIET)
+  endif()
+  if (mrpt-${_comp}_FOUND)
+    list(APPEND MRPT_LIBRARIES mrpt::${_comp})
+  endif()
+endforeach()
+
+# backwards-compatibility var name:
+set(MRPT_LIBS ${MRPT_LIBRARIES})
