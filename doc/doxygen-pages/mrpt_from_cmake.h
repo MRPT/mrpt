ebd5285f7a1338d7424f966743a1b7fc67016d04
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-01-18 23:35:28 +0100

    fix cmake exported target namespace in all cases

--- doc/doxygen-pages/mrpt_from_cmake.h
+++ doc/doxygen-pages/mrpt_from_cmake.h
@@ -15,12 +15,24 @@
 MRPT defines exported projects that can be imported as usual in modern CMake:
 
 ```
+# Find all MRPT libraries:
+find_package(MRPT 1.9.9 COMPONENTS poses gui OPTIONAL_COMPONENTS vision)
+message(STATUS "MRPT_VERSION: ${MRPT_VERSION}")
+message(STATUS "MRPT_LIBRARIES: ${MRPT_LIBRARIES}")
+
+# Define your own targets:
+add_executable(myapp  main.cpp)
+
+# Link against MRPT: this will also add all required flags,
+# include directories, etc.
+target_link_libraries(myapp ${MRPT_LIBRARIES})
+```
+or individually like:
+
+```
 # Find MRPT libraries, one by one:
 find_package(mrpt-poses)
 find_package(mrpt-gui)
-# Or the alternative:
-#find_package(MRPT 1.9.9 COMPONENTS poses gui OPTIONAL_COMPONENTS vision)
-
 
 # Define your own targets:
 add_executable(myapp  main.cpp)
@@ -33,16 +45,18 @@ target_link_libraries(myapp
 )
 ```
 
+
+
+
 ## For MRPT 1.x
 
-Prior to MRPT 2.0.0, the correct way to find for MRPT was different:
-
+Prior to MRPT 2.0.0, the correct way to find for MRPT was:
 ```
 # Find MRPT libraries:
 find_package(MRPT REQUIRED poses gui)
 
 add_executable(myapp  main.cpp)
-target_link_libraries(myapp ${MRPT_LIBS})
+target_link_libraries(myapp ${MRPT_LIBRARIES})
 ```
 
 
