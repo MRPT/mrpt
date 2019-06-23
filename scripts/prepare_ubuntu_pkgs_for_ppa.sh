ad40a26355d74a765cc6d0733211640bc6728b26
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-06-24 01:09:29 +0200

    less verbose ppa package names

--- scripts/prepare_ubuntu_pkgs_for_ppa.sh
+++ scripts/prepare_ubuntu_pkgs_for_ppa.sh
@@ -76,7 +76,7 @@ do
 	cd ${MRPT_DEB_DIR}/mrpt-${MRPT_VER_MMP}~snapshot${MRPT_SNAPSHOT_VERSION}${DEBIAN_DIST}/debian
 	#cp ${MRPT_EXTERN_DEBIAN_DIR}/changelog changelog
 	cp /tmp/my_changelog changelog
-	DEBCHANGE_CMD="--newversion 1:${MRPT_VERSION_STR}~snapshot${MRPT_SNAPSHOT_VERSION}${DEBIAN_DIST}-1~ppa1~${DEBIAN_DIST}"
+	DEBCHANGE_CMD="--newversion 1:${MRPT_VERSION_STR}~snapshot${MRPT_SNAPSHOT_VERSION}${DEBIAN_DIST}-1"
 	echo "Changing to a new Debian version: ${DEBCHANGE_CMD}"
 	echo "Adding a new entry to debian/changelog for distribution ${DEBIAN_DIST}"
 	DEBEMAIL=${EMAIL4DEB} debchange $DEBCHANGE_CMD -b --distribution ${DEBIAN_DIST} --force-distribution New version of upstream sources.
