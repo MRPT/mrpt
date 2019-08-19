c32d5b3936d30eaf8e9637243ebb95c120edd8ad
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2019-08-19 17:44:42 +0200

    FIX BUG: max/min filter=0 should mean ignore filter

--- libs/obs/include/mrpt/obs/CObservation3DRangeScan_project3D_impl.h
+++ libs/obs/include/mrpt/obs/CObservation3DRangeScan_project3D_impl.h
@@ -15,8 +15,8 @@ namespace mrpt {
 namespace obs {
 namespace detail {
     // Auxiliary functions which implement SSE-optimized proyection of 3D point cloud:
-	template <class POINTMAP> void do_project_3d_pointcloud(const int H,const int W,const float *kys,const float *kzs,mrpt::math::CMatrix &rangeImage, mrpt::utils::PointCloudAdapter<POINTMAP> &pca, std::vector<uint16_t> &idxs_x, std::vector<uint16_t> &idxs_y,const mrpt::obs::TRangeImageFilterParams &filterParams, bool MAKE_DENSE, const int DECIM);
-	template <class POINTMAP> void do_project_3d_pointcloud_SSE2(const int H,const int W,const float *kys,const float *kzs,mrpt::math::CMatrix &rangeImage, mrpt::utils::PointCloudAdapter<POINTMAP> &pca, std::vector<uint16_t> &idxs_x, std::vector<uint16_t> &idxs_y,const mrpt::obs::TRangeImageFilterParams &filterParams, bool MAKE_DENSE);
+	template <class POINTMAP> void do_project_3d_pointcloud(const int H,const int W,const float *kys,const float *kzs,mrpt::math::CMatrix &rangeImage, mrpt::utils::PointCloudAdapter<POINTMAP> &pca, std::vector<uint16_t> &idxs_x, std::vector<uint16_t> &idxs_y,const mrpt::obs::TRangeImageFilterParams &fp, bool MAKE_DENSE, const int DECIM);
+	template <class POINTMAP> void do_project_3d_pointcloud_SSE2(const int H,const int W,const float *kys,const float *kzs,mrpt::math::CMatrix &rangeImage, mrpt::utils::PointCloudAdapter<POINTMAP> &pca, std::vector<uint16_t> &idxs_x, std::vector<uint16_t> &idxs_y,const mrpt::obs::TRangeImageFilterParams &fp, bool MAKE_DENSE);
 
     template <typename POINTMAP, bool isDepth>
     inline void range2XYZ(
@@ -384,7 +384,7 @@ namespace detail {
 
 	// Auxiliary functions which implement (un)projection of 3D point clouds:
 	template <class POINTMAP>
-	inline void do_project_3d_pointcloud_SSE2(const int H,const int W,const float *kys,const float *kzs,mrpt::math::CMatrix &rangeImage, mrpt::utils::PointCloudAdapter<POINTMAP> &pca, std::vector<uint16_t> &idxs_x, std::vector<uint16_t> &idxs_y,const mrpt::obs::TRangeImageFilterParams &filterParams, bool MAKE_DENSE)
+	inline void do_project_3d_pointcloud_SSE2(const int H,const int W,const float *kys,const float *kzs,mrpt::math::CMatrix &rangeImage, mrpt::utils::PointCloudAdapter<POINTMAP> &pca, std::vector<uint16_t> &idxs_x, std::vector<uint16_t> &idxs_y,const mrpt::obs::TRangeImageFilterParams &fp, bool MAKE_DENSE)
 	{
     #if MRPT_HAS_SSE2
 		    // Preconditions: minRangeMask() has the right size
@@ -393,54 +393,65 @@ namespace detail {
 			size_t idx=0;
 			MRPT_ALIGN16 float xs[4],ys[4],zs[4];
 			const __m128 D_zeros = _mm_set_ps(.0f,.0f,.0f,.0f);
-			const __m128 xormask = (filterParams.rangeCheckBetween) ?
+			const __m128 xormask = (fp.rangeCheckBetween) ?
 			    _mm_cmpneq_ps(D_zeros,D_zeros) :	// want points BETWEEN min and max to be valid
 			    _mm_cmpeq_ps(D_zeros,D_zeros);		// want points OUTSIDE of min and max to be valid
 			for (int r=0;r<H;r++)
 			{
 				const float *D_ptr = &rangeImage.coeffRef(r,0);  // Matrices are 16-aligned
-				const float *Dgt_ptr = !filterParams.rangeMask_min ? NULL : &filterParams.rangeMask_min->coeffRef(r,0);
-				const float *Dlt_ptr = !filterParams.rangeMask_max ? NULL : &filterParams.rangeMask_max->coeffRef(r,0);
+				const float *Dgt_ptr = !fp.rangeMask_min ? NULL : &fp.rangeMask_min->coeffRef(r,0);
+				const float *Dlt_ptr = !fp.rangeMask_max ? NULL : &fp.rangeMask_max->coeffRef(r,0);
 
 				for (int c=0;c<W_4;c++)
 				{
 					const __m128 D = _mm_load_ps(D_ptr);
 					const __m128 nz_mask = _mm_cmpgt_ps(D, D_zeros);
 					__m128 valid_range_mask;
-					if (!filterParams.rangeMask_min && !filterParams.rangeMask_max)
+					if (!fp.rangeMask_min && !fp.rangeMask_max)
 					{	// No filter: just skip D=0 points
 						valid_range_mask = nz_mask;
 					}
 					else
 					{
-						if (!filterParams.rangeMask_min || !filterParams.rangeMask_max)
-						{	// Only one filter
-							if (filterParams.rangeMask_min)
+						if (!fp.rangeMask_min || !fp.rangeMask_max)
+						{  // Only one filter
+							if (fp.rangeMask_min)
 							{
 								const __m128 Dmin = _mm_load_ps(Dgt_ptr);
-								 valid_range_mask = _mm_and_ps(_mm_cmpgt_ps(D, Dmin ), _mm_cmpgt_ps(Dmin, D_zeros));
+								valid_range_mask = _mm_or_ps(
+									_mm_cmpgt_ps(D, Dmin), _mm_cmpeq_ps(Dmin, D_zeros));
 							}
 							else
 							{
 								const __m128 Dmax = _mm_load_ps(Dlt_ptr);
-								valid_range_mask = _mm_and_ps(_mm_cmplt_ps(D, Dmax ), _mm_cmpgt_ps(Dmax, D_zeros));
+								valid_range_mask = _mm_or_ps(
+									_mm_cmplt_ps(D, Dmax), _mm_cmpeq_ps(Dmax, D_zeros));
 							}
-							valid_range_mask = _mm_and_ps(valid_range_mask, nz_mask ); // Filter out D=0 points
+							valid_range_mask = _mm_and_ps(
+								valid_range_mask, nz_mask);  // Filter out D=0 points
 						}
 						else
 						{
-							// We have both: D>Dmin and D<Dmax conditions, with XOR to optionally invert the selection:
+							// We have both: D>Dmin and D<Dmax conditions, with XOR to
+							// optionally invert the selection:
 							const __m128 Dmin = _mm_load_ps(Dgt_ptr);
 							const __m128 Dmax = _mm_load_ps(Dlt_ptr);
 
-							const __m128 gt_mask = _mm_cmpgt_ps(D, Dmin );
-							const __m128 lt_mask = _mm_and_ps( _mm_cmplt_ps(D, Dmax), nz_mask ); // skip points at zero
-							valid_range_mask = _mm_and_ps(gt_mask, lt_mask ); // (D>Dmin && D<Dmax)
-							valid_range_mask = _mm_xor_ps(valid_range_mask, xormask );
+							const __m128 gt_mask = _mm_or_ps(
+								_mm_cmpgt_ps(D, Dmin), _mm_cmpeq_ps(Dmin, D_zeros));
+							const __m128 lt_mask = _mm_or_ps(
+								_mm_cmplt_ps(D, Dmax), _mm_cmpeq_ps(Dmax, D_zeros));
+							// (D>Dmin && D<Dmax) & skip points at zero
+							valid_range_mask =
+								_mm_and_ps(nz_mask, _mm_and_ps(gt_mask, lt_mask));
+							valid_range_mask = _mm_xor_ps(valid_range_mask, xormask);
 							// Add the case of D_min & D_max = 0 (no filtering)
-							valid_range_mask = _mm_or_ps(valid_range_mask, _mm_and_ps(_mm_cmpeq_ps(Dmin,D_zeros),_mm_cmpeq_ps(Dmax,D_zeros)) );
+							valid_range_mask = _mm_or_ps(
+								valid_range_mask, _mm_and_ps(
+													  _mm_cmpeq_ps(Dmin, D_zeros),
+													  _mm_cmpeq_ps(Dmax, D_zeros)));
 							// Finally, ensure no invalid ranges get thru:
-							valid_range_mask = _mm_and_ps(valid_range_mask, nz_mask );
+							valid_range_mask = _mm_and_ps(valid_range_mask, nz_mask);
 						}
 					}
 					const int valid_range_maski = _mm_movemask_epi8(_mm_castps_si128(valid_range_mask)); // 0x{f|0}{f|0}{f|0}{f|0}
@@ -469,7 +480,7 @@ namespace detail {
 									pca.setInvalidPoint(idx);
 									++idx;
 								}
-								if (filterParams.mark_invalid_ranges)
+								if (fp.mark_invalid_ranges)
 									rangeImage.coeffRef(r, actual_c) = 0;
 							}
 						}
@@ -481,7 +492,7 @@ namespace detail {
 							pca.setInvalidPoint(idx);
 							++idx;
 							const int actual_c = (c << 2) + q;
-							if (filterParams.mark_invalid_ranges)
+							if (fp.mark_invalid_ranges)
 								rangeImage.coeffRef(r, actual_c) = 0;
 						}
 					}
