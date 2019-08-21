d489cccda09af60e307928192f3cf23a7e8e48e1
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-08-21 11:29:19 +0200

    refactor SIMD auto detection

--- cmakemodules/script_SIMD.cmake
+++ cmakemodules/script_SIMD.cmake
@@ -1,44 +1,47 @@
-# SSE{2,3,4} extensions?
+# SIMD extensions
 # ===================================================
-set(MRPT_AUTODETECT_SSE ON CACHE BOOL "Check /proc/cpuinfo to determine if SSE{2,3,4} optimizations are available")
-mark_as_advanced(MRPT_AUTODETECT_SSE)
-
-# Read info about CPUs:
-set(DO_SSE_AUTODETECT 0)
-if(MRPT_AUTODETECT_SSE AND EXISTS "/proc/cpuinfo" AND
+set(AUTODETECT_SIMD_DEFAULT OFF)
+if(EXISTS "/proc/cpuinfo" AND
 	("${CMAKE_MRPT_ARCH}" STREQUAL "x86_64" OR
 	"${CMAKE_MRPT_ARCH}" STREQUAL "i686") )
-	set(DO_SSE_AUTODETECT 1)
+	set(AUTODETECT_SIMD_DEFAULT ON)
 endif()
 
-if (DO_SSE_AUTODETECT)
+set(MRPT_AUTODETECT_SIMD ${AUTODETECT_SIMD_DEFAULT} CACHE BOOL "Check /proc/cpuinfo to determine if SSE{2,3,4} optimizations are available")
+mark_as_advanced(MRPT_AUTODETECT_SIMD)
+
+# Read info about CPUs:
+if (MRPT_AUTODETECT_SIMD)
 	file(READ "/proc/cpuinfo" MRPT_CPU_INFO)
-endif (DO_SSE_AUTODETECT)
+endif()
 
 # Macro for each SSE* var: Invoke with name in uppercase:
-macro(DEFINE_SSE_VAR  _setname)
+macro(DEFINE_SIMD_VAR  _setname)
 	string(TOLOWER ${_setname} _set)
 
-	if (DO_SSE_AUTODETECT)
+	if (MRPT_AUTODETECT_SIMD)
 		# Automatic detection:
 		set(CMAKE_MRPT_HAS_${_setname} 0)
 		if (${MRPT_CPU_INFO} MATCHES ".*${_set}.*")
 			set(CMAKE_MRPT_HAS_${_setname} 1)
 		endif()
-	else (DO_SSE_AUTODETECT)
+	else()
 		# Manual:
-		set("DISABLE_${_setname}" OFF CACHE BOOL "Forces compilation WITHOUT ${_setname} extensions")
-		mark_as_advanced("DISABLE_${_setname}")
+		set("ENABLE_${_setname}" OFF CACHE BOOL "Forces compilation WITH ${_setname} extensions")
+		mark_as_advanced("ENABLE_${_setname}")
 		set(CMAKE_MRPT_HAS_${_setname} 0)
-		if (NOT DISABLE_${_setname})
+		if (ENABLE_${_setname})
 			set(CMAKE_MRPT_HAS_${_setname} 1)
-		endif (NOT DISABLE_${_setname})
-	endif (DO_SSE_AUTODETECT)
-endmacro(DEFINE_SSE_VAR)
+		endif()
+	endif()
+endmacro()
 
 # SSE optimizations:
-DEFINE_SSE_VAR(SSE2)
-DEFINE_SSE_VAR(SSE3)
-DEFINE_SSE_VAR(SSE4_1)
-DEFINE_SSE_VAR(SSE4_2)
-DEFINE_SSE_VAR(SSE4_A)
+DEFINE_SIMD_VAR(SSE2)
+DEFINE_SIMD_VAR(SSE3)
+DEFINE_SIMD_VAR(SSE4_1)
+DEFINE_SIMD_VAR(SSE4_2)
+DEFINE_SIMD_VAR(SSE4_A)
+DEFINE_SIMD_VAR(AVX)
+DEFINE_SIMD_VAR(AVX2)
+DEFINE_SIMD_VAR(NEON)
