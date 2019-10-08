12d93a69c5c128760f7e8f38145cf308824fa4fa
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2019-10-08 13:42:12 +0200

    fix msvc build errors in pbmap

--- cmakemodules/script_pcl.cmake
+++ cmakemodules/script_pcl.cmake
@@ -7,9 +7,11 @@ option(DISABLE_PCL "Forces NOT using PCL, even if it could be found by CMake" "O
 mark_as_advanced(DISABLE_PCL)
 
 if(NOT DISABLE_PCL)
-	# It seems the PCL find_package changes some wxWidgets variables (wtf!), so make a backup:
-	#set(BCK_wxWidgets_LIB_DIR ${wxWidgets_LIB_DIR})
-	#set(BCK_wxWidgets_LIBRARIES ${wxWidgets_LIBRARIES})
+	# Avoid warning about env var PCL_ROOT:
+	if(POLICY CMP0074)
+	  cmake_policy(SET CMP0074 NEW)
+	endif()
+
 
 	# PCL library:
 	# --------------------------------------------
@@ -39,9 +41,5 @@ if(NOT DISABLE_PCL)
 		ADD_DIRECTORIES_AS_ISYSTEM(PCL_INCLUDE_DIRS)
 
 	endif()
-
-	# Undo backups: (read above)
-	#set(wxWidgets_LIB_DIR ${BCK_wxWidgets_LIB_DIR})
-	#set(wxWidgets_LIBRARIES ${BCK_wxWidgets_LIBRARIES})
 
 endif()
--- libs/pbmap/src/registerAllClasses.cpp
+++ libs/pbmap/src/registerAllClasses.cpp
@@ -7,9 +7,10 @@
    | Released under BSD License. See: https://www.mrpt.org/License          |
    +------------------------------------------------------------------------+ */
 
+#include "pbmap-precomp.h"  // precomp. hdr
+//
 #include <mrpt/core/initializer.h>
 #include <mrpt/pbmap.h>
-#include "pbmap-precomp.h"  // precomp. hdr
 
 using namespace mrpt::pbmap;
 
