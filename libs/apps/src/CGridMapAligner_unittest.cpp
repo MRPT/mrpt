3bc7f62e80f5e67a1fd7157ce2d78257093c46bd
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-10-14 00:50:49 +0200

    fix build with opencv<3.2.2

--- libs/apps/src/CGridMapAligner_unittest.cpp
+++ libs/apps/src/CGridMapAligner_unittest.cpp
@@ -33,20 +33,28 @@ TEST(
 		std::string("/share/mrpt/datasets/malaga-cs-fac-building.simplemap.gz");
 	EXPECT_TRUE(mrpt::system::fileExists(gridmap1_fil));
 
-	mrpt::apps::CGridMapAlignerApp app;
-	app.setMinLoggingLevel(mrpt::system::LVL_ERROR);
+	try
+	{
+		mrpt::apps::CGridMapAlignerApp app;
+		app.setMinLoggingLevel(mrpt::system::LVL_ERROR);
 
-	const char* argv[] = {
-		"grid-matching", "--config",		   ini_fil.c_str(),
-		"--map1",		 gridmap1_fil.c_str(), "--detect-test",
-		"--nologo",		 "--nosave",		   "--noicp"};
-	const int argc = sizeof(argv) / sizeof(argv[0]);
+		const char* argv[] = {
+			"grid-matching", "--config",		   ini_fil.c_str(),
+			"--map1",		 gridmap1_fil.c_str(), "--detect-test",
+			"--nologo",		 "--nosave",		   "--noicp"};
+		const int argc = sizeof(argv) / sizeof(argv[0]);
 
-	app.initialize(argc, argv);
-	app.run();
+		app.initialize(argc, argv);
+		app.run();
 
-	// Check result:
-	EXPECT_NEAR(app.estimateMean.x(), app.GT_Ax, 0.1);
-	EXPECT_NEAR(app.estimateMean.y(), app.GT_Ay, 0.1);
-	EXPECT_NEAR(app.estimateMean.phi(), app.GT_Aphi_rad, 0.05);
+		// Check result:
+		EXPECT_NEAR(app.estimateMean.x(), app.GT_Ax, 0.1);
+		EXPECT_NEAR(app.estimateMean.y(), app.GT_Ay, 0.1);
+		EXPECT_NEAR(app.estimateMean.phi(), app.GT_Aphi_rad, 0.05);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << mrpt::exception_to_str(e);
+		GTEST_FAIL();
+	}
 }
