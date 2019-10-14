95bc75a184292de2d6eee85aefa45c3145c1ab7b
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-10-15 01:19:12 +0200

    add mrpt-apps doc page

---
+++ doc/doxygen-pages/mrpt_apps_grp.h
@@ -0,0 +1,35 @@
+/* +---------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)               |
+   |                          https://www.mrpt.org/                            |
+   |                                                                           |
+   | Copyright (c) 2005-2019, Individual contributors, see AUTHORS file        |
+   | See: https://www.mrpt.org/Authors - All rights reserved.                  |
+   | Released under BSD License. See details in https://www.mrpt.org/License   |
+   +---------------------------------------------------------------------------+
+ */
+
+// clang-format off
+
+/** \defgroup mrpt_apps_grp [mrpt-apps]
+
+Wrapper C++ classes for standalone MRPT applications
+
+<small> <a href="index.html#libs">Back to list of all libraries</a> | <a
+href="modules.html" >See all modules</a> </small> <br>
+
+[TOC]
+
+# Library `mrpt-apps`
+
+This C++ library is part of MRPT and can be installed in Debian-based systems with:
+
+		sudo apt install libmrpt-apps-dev
+
+See: \ref mrpt_from_cmake
+
+Refer to classes in the namespace mrpt::apps.
+
+This library includes classes that make easy to automate the programmatic use of
+formerly (mrpt<2.0.0) standalone applications.
+
+*/
