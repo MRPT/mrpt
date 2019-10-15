9b245559cef14c6732405d93769fa8868e80232c
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-10-15 19:51:54 +0200

    clang format fix

--- libs/apps/src/ICP_SLAM_App_unittest.cpp
+++ libs/apps/src/ICP_SLAM_App_unittest.cpp
@@ -18,17 +18,17 @@ using post_tester_t = std::function<void(mrpt::apps::ICP_SLAM_App_Base&)>;
 
 template <class SLAM_CLASS>
 void generic_icp_slam_test(
-    const std::string& ini_filename, const std::string& rawlog_filename,
-    config_changer_t cfg_changer, post_tester_t post_tester)
+	const std::string& ini_filename, const std::string& rawlog_filename,
+	config_changer_t cfg_changer, post_tester_t post_tester)
 {
 	using namespace std::string_literals;
 
 	const auto ini_fil = mrpt::UNITTEST_BASEDIR +
-	                     "/share/mrpt/config_files/icp-slam/"s + ini_filename;
+						 "/share/mrpt/config_files/icp-slam/"s + ini_filename;
 	EXPECT_TRUE(mrpt::system::fileExists(ini_fil));
 
 	const auto rawlog_fil =
-	    mrpt::UNITTEST_BASEDIR + "/share/mrpt/datasets/"s + rawlog_filename;
+		mrpt::UNITTEST_BASEDIR + "/share/mrpt/datasets/"s + rawlog_filename;
 	EXPECT_TRUE(mrpt::system::fileExists(rawlog_fil));
 
 	try
@@ -42,10 +42,10 @@ void generic_icp_slam_test(
 		app.initialize(argc, argv);
 
 		app.params.write(
-		    "MappingApplication", "logOutput_dir",
-		    mrpt::system::getTempFileName() + "_dir"s);
+			"MappingApplication", "logOutput_dir",
+			mrpt::system::getTempFileName() + "_dir"s);
 		app.params.write(
-		    "MappingApplication", "SHOW_PROGRESS_3D_REAL_TIME", false);
+			"MappingApplication", "SHOW_PROGRESS_3D_REAL_TIME", false);
 
 #if !MRPT_HAS_OPENCV
 		app.params.write("MappingApplication", "SAVE_3D_SCENE", false);
@@ -69,7 +69,7 @@ static auto tester_for_2006_01_21 = [](mrpt::apps::ICP_SLAM_App_Base& o) {
 	EXPECT_EQ(o.out_estimated_path.size(), 224U);
 	const mrpt::math::TPose3D p = *o.out_estimated_path.rbegin();
 	const auto p_gt = mrpt::math::TPose3D::FromString(
-	    "[4.308892 -18.489119 0.000000 -87.687641 0.000000 0.000000]");
+		"[4.308892 -18.489119 0.000000 -87.687641 0.000000 0.000000]");
 
 	for (int i = 0; i < 6; i++) EXPECT_NEAR(p[i], p_gt[i], 0.5);
 };
@@ -79,9 +79,9 @@ TEST(ICP_SLAM_App, MapFromRawlog_PointMap)
 	using namespace std::string_literals;
 
 	generic_icp_slam_test<mrpt::apps::ICP_SLAM_App_Rawlog>(
-	    "icp-slam_demo_classic.ini",
-	    "2006-01ENE-21-SENA_Telecom Faculty_one_loop_only.rawlog",
-	    [](mrpt::config::CConfigFileBase&) {}, tester_for_2006_01_21);
+		"icp-slam_demo_classic.ini",
+		"2006-01ENE-21-SENA_Telecom Faculty_one_loop_only.rawlog",
+		[](mrpt::config::CConfigFileBase&) {}, tester_for_2006_01_21);
 }
 
 TEST(ICP_SLAM_App, MapFromRawlog_Grid)
@@ -89,9 +89,9 @@ TEST(ICP_SLAM_App, MapFromRawlog_Grid)
 	using namespace std::string_literals;
 
 	generic_icp_slam_test<mrpt::apps::ICP_SLAM_App_Rawlog>(
-	    "icp-slam_demo_classic_gridmatch.ini",
-	    "2006-01ENE-21-SENA_Telecom Faculty_one_loop_only.rawlog",
-	    [](mrpt::config::CConfigFileBase&) {}, tester_for_2006_01_21);
+		"icp-slam_demo_classic_gridmatch.ini",
+		"2006-01ENE-21-SENA_Telecom Faculty_one_loop_only.rawlog",
+		[](mrpt::config::CConfigFileBase&) {}, tester_for_2006_01_21);
 }
 
 TEST(ICP_SLAM_App, MapFromRawlog_LM)
@@ -99,7 +99,7 @@ TEST(ICP_SLAM_App, MapFromRawlog_LM)
 	using namespace std::string_literals;
 
 	generic_icp_slam_test<mrpt::apps::ICP_SLAM_App_Rawlog>(
-	    "icp-slam_demo_LM.ini",
-	    "2006-01ENE-21-SENA_Telecom Faculty_one_loop_only.rawlog",
-	    [](mrpt::config::CConfigFileBase&) {}, tester_for_2006_01_21);
+		"icp-slam_demo_LM.ini",
+		"2006-01ENE-21-SENA_Telecom Faculty_one_loop_only.rawlog",
+		[](mrpt::config::CConfigFileBase&) {}, tester_for_2006_01_21);
 }
