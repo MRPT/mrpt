aa1e90237bddf5d1553e5f17a7a6f4b7c8ff4a4e
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2019-10-23 16:57:17 +0200

    Upgrade to python3

--- scripts/clang_git_format/clang_git_format/__init__.py
+++ scripts/clang_git_format/clang_git_format/__init__.py
@@ -1,6 +1,6 @@
-from clang_format import ClangFormat
-from repo import Repo
-from custom_exceptions import CommitIDTooShort
+from .clang_format import ClangFormat
+from .repo import Repo
+from .custom_exceptions import CommitIDTooShort
 
 
 __all__ = [
