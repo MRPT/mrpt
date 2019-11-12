cb0c07652f7da3aecc240b6843a713515e7661b1
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2019-11-12 07:52:15 +0100

    refactor of apps base classes

---
+++ libs/apps/src/DataSourceRawlog.cpp
@@ -0,0 +1,55 @@
+/* +------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)            |
+   |                          https://www.mrpt.org/                         |
+   |                                                                        |
+   | Copyright (c) 2005-2019, Individual contributors, see AUTHORS file     |
+   | See: https://www.mrpt.org/Authors - All rights reserved.               |
+   | Released under BSD License. See: https://www.mrpt.org/License          |
+   +------------------------------------------------------------------------+ */
+
+#include <mrpt/apps/DataSourceRawlog.h>
+#include <mrpt/obs/CRawlog.h>
+
+using namespace mrpt::apps;
+
+bool DataSourceRawlog::impl_get_next_observations(
+	mrpt::obs::CActionCollection::Ptr& action,
+	mrpt::obs::CSensoryFrame::Ptr& observations,
+	mrpt::obs::CObservation::Ptr& observation)
+{
+	MRPT_START
+
+	// 1st time? Open rawlog:
+	if (!m_rawlog_arch)
+	{
+		std::string err_msg;
+		if (!m_rawlog_io.open(m_rawlogFileName, err_msg))
+		{
+			THROW_EXCEPTION_FMT(
+				"Error opening rawlog file: `%s`", err_msg.c_str());
+		}
+		m_rawlog_arch = mrpt::serialization::archiveUniquePtrFrom(m_rawlog_io);
+
+		MRPT_LOG_INFO_FMT("RAWLOG file: `%s`", m_rawlogFileName.c_str());
+	}
+
+	// Read:
+
+	for (;;)
+	{
+		if (!mrpt::obs::CRawlog::getActionObservationPairOrObservation(
+				*m_rawlog_arch, action, observations, observation,
+				m_rawlogEntry))
+			return false;
+
+		// Optional skip of first N entries
+		if (m_rawlogEntry < m_rawlog_offset) continue;
+
+		// Ok, accept this new observations:
+		return true;
+	};
+
+	return false;
+
+	MRPT_END
+}
