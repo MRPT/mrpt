d7399f4897a046c6879dc662e7e6094ee4357e75
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2020-02-25 19:11:06 +0100

    Fix clocks epoch correction

--- libs/core/include/mrpt/core/Clock.h
+++ libs/core/include/mrpt/core/Clock.h
@@ -58,5 +58,27 @@ class Clock
 
 	/** Returns the currently selected clock. */
 	static Source getActiveClock();
+
+	/** Monotonic clock *might* drift over time with respect to Realtime.
+	 * The first time a time is requested to now() with Monotonic clock enabled,
+	 * MRPT internally saves the current values of both, Realtime and Monotonic
+	 * clocks, then use the difference in all subsequent calls to set Monotonic
+	 * timepoints in the same epoch than Realtime.
+	 *
+	 * By explicitly calling this static method, a user is able to force a
+	 * resynchrnization between the two clocks. Potentially useful for systems
+	 * that run for very long periods of time without interruption.
+	 *
+	 * \return The mismatch between the former and the new estimations of the
+	 * epochs differences between the two clocks, in units of nanoseconds.
+	 *
+	 * \note This method is ignored in non-Linux systems.
+	 */
+	static int64_t resetMonotonicToRealTimeEpoch();
+
+	/** Returns the number of nanoseconds that are added to the output of the
+	 * POSIX `CLOCK_MONOTONIC` to make timestamps match the epoch of POSIX
+	 * `CLOCK_REALTIME`. */
+	static uint64_t getMonotonicToRealtimeOffset();
 };
 }  // namespace mrpt
