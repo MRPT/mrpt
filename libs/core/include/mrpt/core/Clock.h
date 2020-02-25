bc4a1b0ede01ec7ce61785bb70ae032a7fa54747
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2020-02-25 14:41:38 +0100

    Allow selecting between Realtime or Monotonic clock sources

--- libs/core/include/mrpt/core/Clock.h
+++ libs/core/include/mrpt/core/Clock.h
@@ -26,13 +26,37 @@ class Clock
 
 	static constexpr bool is_steady = std::chrono::system_clock::is_steady;
 
-	/** Returns the current time, with the highest resolution available.
-	 *  Typically this is better than 1 microsecond. */
+	/** Returns the current time using the currently selected Clock source.
+	 * \sa setActiveClock(), mrpt::Clock::Source  */
 	static time_point now() noexcept;
 	/** Create a timestamp from its double representation. \sa toDouble */
 	static time_point fromDouble(const double t) noexcept;
 	/** Converts a timestamp to a UNIX time_t-like number, with fractional part
 	 * \sa fromDouble */
 	static double toDouble(const time_point t) noexcept;
+
+	/** Options for setting the source of all timestamps across MRPT:
+	 * setActiveClock(), now()
+	 */
+	enum Source
+	{
+		/** Realtime: POSIX `CLOCK_REALTIME`. The highest resolution available
+		 *  Typically this is better than 1 microsecond. Selected by default.
+		 */
+		Realtime = 0,
+
+		/** Monotonic: POSIX `CLOCK_MONOTONIC`. Only available in Linux systems.
+		 */
+		Monotonic
+	};
+
+	/** Changes the selected clock to get time from when calling now().
+	 * Default: Realtime.
+	 * Ignored in non-Linux systems.
+	 */
+	static void setActiveClock(const Source s);
+
+	/** Returns the currently selected clock. */
+	static Source getActiveClock();
 };
 }  // namespace mrpt
