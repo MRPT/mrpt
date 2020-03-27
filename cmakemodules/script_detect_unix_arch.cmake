e0b6917d9912cd4473f7a3a57e093629d108c500
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2020-03-27 02:51:46 +0100

    better detection of architecture

--- cmakemodules/script_detect_unix_arch.cmake
+++ cmakemodules/script_detect_unix_arch.cmake
@@ -1,7 +1,8 @@
+
+# Rely on CMake to detect target system architecture:
+SET(CMAKE_MRPT_ARCH ${CMAKE_SYSTEM_PROCESSOR})
+
 if(UNIX)
-	execute_process(COMMAND uname -m
-		OUTPUT_VARIABLE CMAKE_MRPT_ARCH
-		OUTPUT_STRIP_TRAILING_WHITESPACE)
 	execute_process(COMMAND uname -s
 		OUTPUT_VARIABLE CMAKE_MRPT_KERNEL
 		OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -11,7 +12,8 @@ if(UNIX)
 endif()
 
 # Detect if we are in i386 / amd64:
-if ("${CMAKE_MRPT_ARCH}" STREQUAL "x86_64" OR "${CMAKE_MRPT_ARCH}" STREQUAL "i686")
+# Intel arch names in Linux & Windows.
+if ("${CMAKE_MRPT_ARCH}" MATCHES "^(x86_64|i686|AMD64|IA64|x86)$")
 	set(MRPT_ARCH_INTEL_COMPATIBLE 1)
 else()
 	set(MRPT_ARCH_INTEL_COMPATIBLE 0)
