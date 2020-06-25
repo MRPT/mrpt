0169b5b8099d325a4baae07272684ef7a202e4f5
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2020-06-25 08:32:07 +0200

    Add constexpr for_()

--- doc/doxygen-pages/changeLog_doc.h
+++ doc/doxygen-pages/changeLog_doc.h
@@ -13,6 +13,8 @@
  *
 # Version 2.0.5: (Under development)
 - Changes in libraries:
+  - \ref mrpt_core_grp
+    - New mrpt::for_<> constexpr for loop helper function.
   - \ref mrpt_containers_grp
     - New class mrpt::containers::Parameters for variant-based, nested, YAML-like configuration blocks.
 
