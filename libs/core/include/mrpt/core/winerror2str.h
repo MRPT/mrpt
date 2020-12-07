975522f89eba98bf73a3c243cf889aabd25604eb
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2020-12-07 08:21:41 +0100

    Fix new call stack functions in Windows

---
+++ libs/core/include/mrpt/core/winerror2str.h
@@ -0,0 +1,28 @@
+/* +------------------------------------------------------------------------+
+   |                     Mobile Robot Programming Toolkit (MRPT)            |
+   |                          https://www.mrpt.org/                         |
+   |                                                                        |
+   | Copyright (c) 2005-2020, Individual contributors, see AUTHORS file     |
+   | See: https://www.mrpt.org/Authors - All rights reserved.               |
+   | Released under BSD License. See: https://www.mrpt.org/License          |
+   +------------------------------------------------------------------------+ */
+#pragma once
+
+#include <string>
+
+namespace mrpt
+{
+/** [Windows only] Returns an error string "[<caller>] Error: <error msg>" with
+ * "<caller>" the given user-provided string, and "<error msg>" the combination
+ * of calling GetLastError() + FormatMessageA(). If no "<caller>" string is
+ * provided (nullptr), it just returns the "<error msg>" part.
+ *
+ * Returns an empty string in non Windows OSes, or if GetLastError() gives no
+ * error.
+ * \note (New in MRPT 2.1.5)
+ * \ingroup mrpt_core_grp
+ */
+std::string winerror2str(
+	const char* caller = nullptr, const char* errorPrefix = " Error: ");
+
+}  // namespace mrpt
--- libs/core/src/exception_unittest.cpp
+++ libs/core/src/exception_unittest.cpp
@@ -52,11 +52,15 @@ TEST(exception, stackedExceptionComplex)
 	{
 		const auto sExc = mrpt::exception_to_str(e);
 		const auto num_lines = std::count(sExc.begin(), sExc.end(), '\n');
-		EXPECT_GT(num_lines, 10);
+		EXPECT_GT(num_lines, 10) << sExc;
 
 		EXPECT_TRUE(sExc.find("Message:  Aw!") != std::string::npos) << sExc;
+
+// This test doesn't pass in Windows if building w/o debug symbols:
+#if !defined(_WIN32) || defined(_DEBUG)
 		EXPECT_TRUE(sExc.find("test_except_toplevel") != std::string::npos)
 			<< sExc;
+#endif
 	}
 }
 
