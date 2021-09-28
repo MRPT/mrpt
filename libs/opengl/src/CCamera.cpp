63a2c7d245921e37edb1704e1dae77418deefcf2
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2021-09-28 09:10:31 +0200

    add CCamera::setProjectiveFromPinhole()

--- libs/opengl/src/CCamera.cpp
+++ libs/opengl/src/CCamera.cpp
@@ -12,6 +12,7 @@
 #include <mrpt/opengl/CCamera.h>
 #include <mrpt/opengl/opengl_api.h>
 #include <mrpt/serialization/CArchive.h>
+#include <mrpt/serialization/optional_serialization.h>
 
 using namespace mrpt;
 using namespace mrpt::opengl;
@@ -20,13 +21,14 @@ using namespace std;
 
 IMPLEMENTS_SERIALIZABLE(CCamera, CRenderizable, mrpt::opengl)
 
-uint8_t CCamera::serializeGetVersion() const { return 1; }
+uint8_t CCamera::serializeGetVersion() const { return 2; }
 void CCamera::serializeTo(mrpt::serialization::CArchive& out) const
 {
 	// Save data:
 	out << m_pointingX << m_pointingY << m_pointingZ << m_eyeDistance
 		<< m_azimuthDeg << m_elevationDeg << m_projectiveModel
 		<< m_projectiveFOVdeg;
+	out << m_pinholeModel;	// v2
 }
 
 void CCamera::serializeFrom(mrpt::serialization::CArchive& in, uint8_t version)
@@ -34,11 +36,15 @@ void CCamera::serializeFrom(mrpt::serialization::CArchive& in, uint8_t version)
 	switch (version)
 	{
 		case 1:
+		case 2:
 		{
 			// Load data:
 			in >> m_pointingX >> m_pointingY >> m_pointingZ >> m_eyeDistance >>
 				m_azimuthDeg >> m_elevationDeg >> m_projectiveModel >>
 				m_projectiveFOVdeg;
+			if (version >= 2) in >> m_pinholeModel;
+			else
+				m_pinholeModel.reset();
 		}
 		break;
 		case 0:
