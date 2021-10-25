26d16ceb6df1edbd913588c8f5ae0cbd0a0f9e50
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2021-10-25 12:13:09 +0200

    Pass shellcheck

--- packaging/ppa_build_and_upload_all.sh
+++ packaging/ppa_build_and_upload_all.sh
@@ -9,18 +9,18 @@ set -e
 # Check:
 if [ -f version_prefix.txt ];
 then
-	MRPT_VERSION_STR=`head -n 1 version_prefix.txt`
+	MRPT_VERSION_STR=$(head -n 1 version_prefix.txt)
 	echo "MRPT version: ${MRPT_VERSION_STR}"
 else
 	echo "ERROR: Run this script from the MRPT root directory."
 	exit 1
 fi
 
-MRPTDIR=`pwd`
+MRPTDIR=$(pwd)
 
 # Build PPA packages:
 bash packaging/prepare_ubuntu_pkgs_for_ppa.sh
-cd $HOME/mrpt_ubuntu
+cd "$HOME/mrpt_ubuntu"
 
 # upload:
 find . -name '*.changes' | xargs -I FIL dput ppa:joseluisblancoc/mrpt FIL
