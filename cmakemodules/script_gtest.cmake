e873a1dd513bb51f92170b1dcd6e30cd1d50bafe
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2021-12-14 12:38:16 +0100

    Actually use gtest system package if found

--- cmakemodules/script_gtest.cmake
+++ cmakemodules/script_gtest.cmake
@@ -1,4 +1,13 @@
-# By default, use embedded version of gtest:
-set(CMAKE_MRPT_HAS_GTEST         1)
-set(CMAKE_MRPT_HAS_GTEST_SYSTEM  0)
-set(CMAKE_MRPT_GTEST_SRC_DIR     "${MRPT_SOURCE_DIR}/3rdparty/googletest/googletest/")
+# By default, use embedded version of gtest, unless it is found in the system:
+
+find_package(GTest QUIET)
+
+if (GTest_FOUND)
+	set(CMAKE_MRPT_HAS_GTEST         1)
+	set(CMAKE_MRPT_HAS_GTEST_SYSTEM  1)
+	# We can use the imported targets: GTest::GTest and GTest::Main
+ else()
+	set(CMAKE_MRPT_HAS_GTEST         1)
+	set(CMAKE_MRPT_HAS_GTEST_SYSTEM  0)
+	set(CMAKE_MRPT_GTEST_SRC_DIR     "${MRPT_SOURCE_DIR}/3rdparty/googletest/googletest/")
+endif()
