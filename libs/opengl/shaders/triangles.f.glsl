94c35ce7078c1dc7f45174f072bdd7602b222072
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-01-07 15:51:16 +0100

    Convert shaders to GLSL 3.0.0 ES

--- libs/opengl/shaders/triangles.f.glsl
+++ libs/opengl/shaders/triangles.f.glsl
@@ -1,29 +1,29 @@
 R"XXX(
-#version 330 core
+#version 300 es
 
 // FRAGMENT SHADER: Default shader for MRPT CRenderizable objects
 // Jose Luis Blanco Claraco (C) 2019-2020
 // Part of the MRPT project
 
-uniform mat4 mv_matrix;
-uniform vec4 light_diffuse, light_ambient;
-uniform vec3 light_direction;
+uniform highp mat4 mv_matrix;
+uniform highp vec4 light_diffuse, light_ambient;
+uniform highp vec3 light_direction;
 uniform int enableLight;  // 0 or 1
 
-in vec3 frag_position, frag_normal;
-in vec4 frag_materialColor;
+in highp vec3 frag_position, frag_normal;
+in highp vec4 frag_materialColor;
 
-out vec4 color;
+out highp vec4 color;
 
 void main()
 {
     if (enableLight!=0)
     {
-        vec3 mv_light_direction = light_direction;
-        vec3 eye = normalize(frag_position);
-        vec3 fn = normalize(frag_normal);
+        highp vec3 mv_light_direction = light_direction;
+        highp vec3 eye = normalize(frag_position);
+        highp vec3 fn = normalize(frag_normal);
 
-        vec4 diffuse_factor = max(-dot(frag_normal, mv_light_direction), 0.0) * light_diffuse;
+        highp vec4 diffuse_factor = max(-dot(frag_normal, mv_light_direction), 0.0) * light_diffuse;
 
         color = frag_materialColor * (diffuse_factor + light_ambient);
     }
