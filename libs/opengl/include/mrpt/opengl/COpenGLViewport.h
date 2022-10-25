3663473ddd8ec8e495959b5d06ebc62501e0048d
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-10-25 14:04:32 +0200

    make multithread-opengl compatible

--- libs/opengl/include/mrpt/opengl/COpenGLViewport.h
+++ libs/opengl/include/mrpt/opengl/COpenGLViewport.h
@@ -8,6 +8,7 @@
    +------------------------------------------------------------------------+ */
 #pragma once
 
+#include <mrpt/containers/PerThreadDataHolder.h>
 #include <mrpt/core/safe_pointers.h>
 #include <mrpt/img/CImage.h>
 #include <mrpt/opengl/CCamera.h>
@@ -341,7 +342,10 @@ class COpenGLViewport : public mrpt::serialization::CSerializable,
 	mrpt::math::TBoundingBox getBoundingBox() const;
 
 	/** Returns a copy of the latest render matrices structure. */
-	TRenderMatrices getRenderMatrices() const { return m_state; }
+	TRenderMatrices getRenderMatrices() const
+	{
+		return m_threadedData.get().state;
+	}
 
 	/** @} */  // end of Contained objects set/get/search
 
@@ -364,7 +368,7 @@ class COpenGLViewport : public mrpt::serialization::CSerializable,
 	/** Provides read access to the opengl shaders */
 	const std::map<shader_id_t, mrpt::opengl::Program::Ptr>& shaders() const
 	{
-		return m_shaders;
+		return m_threadedData.get().shaders;
 	}
 
 	/** Load all MPRT predefined shader programs into m_shaders */
@@ -373,7 +377,7 @@ class COpenGLViewport : public mrpt::serialization::CSerializable,
 	/** Provides write access to the opengl shaders */
 	std::map<shader_id_t, mrpt::opengl::Program::Ptr>& shaders()
 	{
-		return m_shaders;
+		return m_threadedData.get().shaders;
 	}
 
    protected:
@@ -442,11 +446,19 @@ class COpenGLViewport : public mrpt::serialization::CSerializable,
 
 	mutable mrpt::opengl::CSetOfLines::Ptr m_borderLines;
 
-	/** Info updated with each "render()" and used in "get3DRayForPixelCoord" */
-	mutable TRenderMatrices m_state;
+	struct PerThreadData
+	{
+		PerThreadData() = default;
 
-	/** Default shader program */
-	mutable std::map<shader_id_t, mrpt::opengl::Program::Ptr> m_shaders;
+		/** Info updated with each "render()" and used in
+		 * "get3DRayForPixelCoord" */
+		TRenderMatrices state;
+
+		/** Default shader program */
+		std::map<shader_id_t, mrpt::opengl::Program::Ptr> shaders;
+	};
+
+	mutable mrpt::containers::PerThreadDataHolder<PerThreadData> m_threadedData;
 
 	/** Unload shader programs in m_shaders */
 	void unloadShaders();
