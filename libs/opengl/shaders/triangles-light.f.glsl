eb6a86cf109f9cbfc053aa83c351dddfa90f484d
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2022-11-20 08:22:36 +0100

    update copyright year

--- libs/opengl/shaders/triangles-light.f.glsl
+++ libs/opengl/shaders/triangles-light.f.glsl
@@ -1,7 +1,7 @@
 R"XXX(#version 300 es
 
 // FRAGMENT SHADER: Default shader for MRPT CRenderizable objects
-// Jose Luis Blanco Claraco (C) 2019-2020
+// Jose Luis Blanco Claraco (C) 2019-2022
 // Part of the MRPT project
 
 uniform mediump vec4 light_diffuse, light_ambient;
