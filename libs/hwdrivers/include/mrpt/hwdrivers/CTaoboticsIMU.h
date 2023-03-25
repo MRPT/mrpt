d22467254d1e4c7f0221631ebaa64fa9301969b1
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2023-03-25 02:42:47 +0100

    Add support for TAObotics IMUs

--- libs/hwdrivers/include/mrpt/hwdrivers/CTaoboticsIMU.h
+++ libs/hwdrivers/include/mrpt/hwdrivers/CTaoboticsIMU.h
@@ -10,9 +10,12 @@
 #pragma once
 
 #include <mrpt/comms/CSerialPort.h>
+#include <mrpt/containers/circular_buffer.h>
 #include <mrpt/hwdrivers/CGenericSensor.h>
 #include <mrpt/obs/CObservationIMU.h>
 #include <mrpt/poses/CPose3D.h>
+
+#include <functional>
 
 namespace mrpt::hwdrivers
 {
@@ -20,11 +23,39 @@ namespace mrpt::hwdrivers
  *
  * Supported models: HFI-A9, HFI-B9, HFI-B6.
  *
+ * ## Frame format
+ * From analysis the provided [python scripts]()
+ * we found that the sensor uses these data frames, documented here for
+ * reference:
+ *
+ * ### Model: "hfi-a9"
+ *
+ *  Two frames:
+ *  Frame field :  0xAA 0x55 0x2c | (9 * 4[float]) D0-D8  | ?? ??
+ *  Byte index  :   0     1    2  | 3 4 5 6 |  ...        | 48
+ *   Data:
+ *    - D{0,1,2}: wx,wy,wz
+ *    - D{3,4,5}: accx,accy,accz
+ *    - D{6,7,8}: magx,magy,magz
+ *
+ *  Frame field :  0xAA 0x55 0x14 | (5 * 4[float]) D0-D4   | ??
+ *  Byte index  :   0     1    2  |  3 ... |      ...      | 24
+ *
+ * ### Model: "hfi-b6"
+ *
+ *  Frame field :  0x55 | TYPE |  D0  |  D1  |  D2  |  D3  | CHKSUM
+ *  Byte index  :   0   |  1   | 2  3 | 4  5 | 6  7 | 8  9 |   10
+ *  Type:
+ *  0x51: acceleration, 0x52: angular velocity, 0x53: euler angles
+ *
+ * ### Configuration file block
  *  \code
  *  PARAMETERS IN THE ".INI"-LIKE CONFIGURATION STRINGS:
  * -------------------------------------------------------
  *   [supplied_section_name]
  *    process_rate = 500   # (Hz) must be larger than the sensor rate
+ *    serialPort  = /dev/ttyUSB0
+ *    sensorModel = hfi-a9
  *    pose_x=0	    # Sensor 3D position relative to the robot (meters)
  *    pose_y=0
  *    pose_z=0
@@ -32,7 +63,6 @@ namespace mrpt::hwdrivers
  *    pose_pitch=0
  *    pose_roll=0
  *    sensorLabel = name   # Label of the sensor
- *    serialPort  = /dev/ttyUSB0
  *  \endcode
  *
  * If used programmatically, this class will be used as:
@@ -71,25 +101,49 @@ class CTaoboticsIMU : public hwdrivers::CGenericSensor
 	 */
 	void doProcess() override;
 
-	/** Turns on the KVH DSP 3000 device and configure it for getting
-	 * orientation data. you must have called loadConfig_sensorSpecific before
-	 * calling this function.
+	/** Opens the serial port and start streaming data.
+	 * You must have called loadConfig_sensorSpecific before
+	 * calling this function, or set the configuration via the provided methods,
+	 * e.g. setSerialPort(), etc.
 	 */
 	void initialize() override;
+
+	/// Must be called before initialize(). If not set, the default is
+	/// "/dev/ttyUSB0". Use "COM1", etc. for Windows.
+	void setSerialPort(const std::string& serialPort);
+
+	/// Must be called before initialize(). If not called, the default 921600 is
+	/// used.
+	void setSerialBaudRate(int rate);
 
    protected:
 	/** This serial port will be attempted to be opened automatically when this
 	 * class is first used to request data from the device.
 	 * \sa comms::CSerialPort
 	 */
-	int m_baudRate = 38400;
-	std::string m_com_port;
+	int m_baudRate = 921600;
+	std::string m_com_port = "/dev/ttyUSB0";
+	std::string m_sensorModel = "hfi-a9";
 	mrpt::poses::CPose3D m_sensorPose;
+
+	/** Auxiliary buffer for readings */
+	mrpt::containers::circular_buffer<uint8_t> m_rx_buffer{4096};
 
 	/** The serial port connection */
 	std::unique_ptr<mrpt::comms::CSerialPort> m_serialPort;
 
 	mrpt::obs::CObservationIMU::Ptr m_observationGyro;
+
+	using parser_t = std::function<std::vector<mrpt::obs::CObservation::Ptr>(
+		CTaoboticsIMU*, mrpt::containers::circular_buffer<uint8_t>& /*buf*/)>;
+
+	parser_t m_activeParser;
+
+	std::vector<mrpt::obs::CObservation::Ptr> parser_hfi_b6(
+		mrpt::containers::circular_buffer<uint8_t>& buf) const;
+
+	std::vector<mrpt::obs::CObservation::Ptr> parser_hfi_a9(
+		mrpt::containers::circular_buffer<uint8_t>& buf) const;
 
 };	// end of class
 
--- libs/hwdrivers/src/CTaoboticsIMU.cpp
+++ libs/hwdrivers/src/CTaoboticsIMU.cpp
@@ -9,6 +9,8 @@
 
 #include "hwdrivers-precomp.h"	// Precompiled headers
 //
+#include <mrpt/core/bit_cast.h>
+#include <mrpt/core/reverse_bytes.h>
 #include <mrpt/hwdrivers/CTaoboticsIMU.h>
 #include <mrpt/obs/CObservationIMU.h>
 
@@ -30,9 +32,24 @@ CTaoboticsIMU::CTaoboticsIMU()
 
 CTaoboticsIMU::~CTaoboticsIMU() { m_serialPort->close(); }
 
+void CTaoboticsIMU::setSerialPort(const std::string& serialPort)
+{
+	ASSERT_(!m_serialPort);
+
+	m_com_port = serialPort;
+}
+
+void CTaoboticsIMU::setSerialBaudRate(int rate)
+{
+	ASSERT_(!m_serialPort);
+	m_baudRate = rate;
+}
+
 void CTaoboticsIMU::doProcess()
 {
 	using namespace std::chrono_literals;
+
+	ASSERTMSG_(m_serialPort, "initialize() must be called first");
 
 	if (m_state == ssError)
 	{
@@ -43,20 +60,32 @@ void CTaoboticsIMU::doProcess()
 	if (m_state == ssError) return;
 
 	// try to read and parse a frame from the serial port:
-	MRPT_TODO("continue");
+	std::vector<uint8_t> buf(m_rx_buffer.available());
+	const auto nRead =
+		m_serialPort->ReadBufferImmediate(buf.data(), buf.size());
+	m_rx_buffer.push_many(buf.data(), nRead);
 
-	// yes: we have a new frame:
-	auto obs = std::make_shared<CObservationIMU>();
-	obs->timestamp = mrpt::system::now();
+	// Parse:
+	ASSERT_(m_activeParser);
+	const auto allObs = m_activeParser(this, m_rx_buffer);
 
-	obs->sensorPose = m_sensorPose;
-	obs->sensorLabel = m_sensorLabel;
-
-	appendObservation(obs);
+	// and send observations out, if any:
+	for (const auto& obs : allObs)
+		this->appendObservation(obs);
 }
 
 void CTaoboticsIMU::initialize()
 {
+	if (m_sensorModel == "hfi-b6")
+		m_activeParser = &CTaoboticsIMU::parser_hfi_b6;
+	else if (m_sensorModel == "hfi-a9")
+		m_activeParser = &CTaoboticsIMU::parser_hfi_a9;
+	else
+	{
+		THROW_EXCEPTION_FMT(
+			"Unknown sensor model: '%s'", m_sensorModel.c_str());
+	}
+
 	if (m_verbose)
 		std::cout << "[CTaoboticsIMU] Opening port: " << m_com_port << " at "
 				  << m_baudRate << " bauds.\n";
@@ -66,22 +95,271 @@ void CTaoboticsIMU::initialize()
 		THROW_EXCEPTION_FMT("can't open serial port %s", m_com_port.c_str());
 
 	m_serialPort->setConfig(m_baudRate);
+	m_serialPort->setTimeouts(1, 1, 1, 1, 1);
+
+	m_serialPort->purgeBuffers();
+	m_rx_buffer.clear();
 
 	m_state = ssWorking;
 }
 
 void CTaoboticsIMU::loadConfig_sensorSpecific(
-	const mrpt::config::CConfigFileBase& configSource,
-	const std::string& iniSection)
+	const mrpt::config::CConfigFileBase& c, const std::string& s)
 {
 	m_sensorPose.setFromValues(
-		configSource.read_float(iniSection, "pose_x", 0, false),
-		configSource.read_float(iniSection, "pose_y", 0, false),
-		configSource.read_float(iniSection, "pose_z", 0, false),
-		DEG2RAD(configSource.read_float(iniSection, "pose_yaw", 0, false)),
-		DEG2RAD(configSource.read_float(iniSection, "pose_pitch", 0, false)),
-		DEG2RAD(configSource.read_float(iniSection, "pose_roll", 0, false)));
+		c.read_float(s, "pose_x", 0, false),
+		c.read_float(s, "pose_y", 0, false),
+		c.read_float(s, "pose_z", 0, false),
+		DEG2RAD(c.read_float(s, "pose_yaw", 0, false)),
+		DEG2RAD(c.read_float(s, "pose_pitch", 0, false)),
+		DEG2RAD(c.read_float(s, "pose_roll", 0, false)));
 
-	m_com_port =
-		configSource.read_string(iniSection, "serialPort", m_com_port, false);
+	m_com_port = c.read_string(s, "serialPort", m_com_port);
+	m_sensorModel = c.read_string(s, "sensorModel", m_sensorModel);
+}
+
+std::vector<mrpt::obs::CObservation::Ptr> CTaoboticsIMU::parser_hfi_b6(
+	mrpt::containers::circular_buffer<uint8_t>& buf) const
+{
+	std::vector<mrpt::obs::CObservation::Ptr> out;
+
+	// Parse:
+	// Header must be: "0x55 0x5*".
+	// But we must wait for the first "0x55 0x51" to ensure synchronization
+	// of all IMU data fields in one observation:
+	while (buf.size() >= 11)
+	{
+		if (buf.peek(0) == 0x55 && buf.peek(1) == 0x51) { break; }
+		else
+		{
+			// discard byte:
+			buf.pop();
+		}
+	}
+
+	// Actual processing, of one or more observations:
+	mrpt::obs::CObservationIMU::Ptr obs;
+
+	while (buf.size() >= 11)
+	{
+		if (buf.peek(0) == 0x55 && (buf.peek(1) & 0x50) == 0x50)
+		{
+			std::array<uint8_t, 11> frame;
+			buf.pop_many(frame.data(), frame.size());
+
+			// Parse this frame. Each frame seems to be fixed size = 11
+			// bytes.
+			//
+			// Frame field :  0x55 | TYPE |  D0  |  D1  |  D2  |  D3  |CHKSUM
+			//  Byte index :   0   |  1   | 2  3 | 4  5 | 6  7 | 8  9 |   10
+			//
+			// Type:
+			// 0x51: acceleration, 0x52: angular velocity, 0x53: euler
+			// angles
+			//
+			// TODO: Checksum
+
+			uint16_t data[4];
+			for (int i = 0; i < 4; i++)
+			{
+				data[i] = frame[2 + 2 * i] | (frame[2 + 2 * i + 1] << 8);
+				mrpt::toNativeEndianness(data[i]);
+			}
+
+			switch (frame[1])
+			{
+				case 0x51:
+				{
+					if (!obs) obs = std::make_shared<CObservationIMU>();
+					const double accx = data[0] / 32768.0 * 16 * -9.8;
+					const double accy = data[1] / 32768.0 * 16 * -9.8;
+					const double accz = data[2] / 32768.0 * 16 * -9.8;
+					printf("acc: %f %f %f\n", accx, accy, accz);
+				}
+				break;
+				case 0x52:
+				{
+					if (!obs) obs = std::make_shared<CObservationIMU>();
+					//
+				}
+				break;
+				case 0x53:
+				{
+					// and send out one obs:
+					if (!obs) obs = std::make_shared<CObservationIMU>();
+
+					// get euler angle:
+
+					// send out:
+					obs->timestamp = mrpt::system::now();
+
+					obs->sensorPose = m_sensorPose;
+					obs->sensorLabel = m_sensorLabel;
+
+					out.push_back(obs);
+					obs.reset();
+				}
+				break;
+				default:
+					fprintf(
+						stderr,
+						"[CTaoboticsIMU] Received frame with unknown data type "
+						"id=0x%02X\n",
+						frame[1]);
+					break;
+			}
+		}
+		else
+		{
+			// discard byte:
+			buf.pop();
+		}
+	}
+
+	return out;
+}
+
+std::vector<mrpt::obs::CObservation::Ptr> CTaoboticsIMU::parser_hfi_a9(
+	mrpt::containers::circular_buffer<uint8_t>& buf) const
+{
+	using namespace mrpt::obs;
+
+	std::vector<mrpt::obs::CObservation::Ptr> out;
+
+	// Parse. See .h for data frame format
+	while (buf.size() >= 49 + 25)
+	{
+		if (buf.peek(0) == 0xAA && buf.peek(1) == 0x55 && buf.peek(2) == 0x2c)
+		{
+			break;
+		}
+		else
+		{
+			// discard byte:
+			buf.pop();
+		}
+	}
+
+	// Actual processing, of one or more observations:
+	mrpt::obs::CObservationIMU::Ptr obs;
+
+	while (buf.size() >= 49 + 25)
+	{
+		if (buf.peek(0) == 0xAA && buf.peek(1) == 0x55 && buf.peek(2) == 0x2c)
+		{
+			std::array<uint8_t, 49> frame;
+			buf.pop_many(frame.data(), frame.size());
+
+#if 0
+			const uint32_t hwTimestamp = (frame[7 + 3] << 24) |	 //
+				(frame[7 + 2] << 16) |	//
+				(frame[7 + 1] << 8) |  //
+				(frame[7 + 0] << 0);
+#endif
+
+			float data[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+			for (int i = 0; i < 9; i++)
+			{
+				// Fixed format: little endian for this sensor.
+				const uint32_t d =	//
+					(frame[11 + 4 * i + 3] << 24) |	 //
+					(frame[11 + 4 * i + 2] << 16) |	 //
+					(frame[11 + 4 * i + 1] << 8) |	//
+					(frame[11 + 4 * i + 0] << 0);
+				data[i] = bit_cast<float>(d);
+			}
+
+			if (!obs)
+			{
+				obs = std::make_shared<CObservationIMU>();
+				obs->sensorPose = m_sensorPose;
+				obs->sensorLabel = m_sensorLabel;
+			}
+
+			const float wx = data[0], wy = data[1], wz = data[2];
+			const float ax = data[3], ay = data[4], az = data[5];
+			const float mx = data[6], my = data[7], mz = data[8];
+
+			// send out:
+			obs->timestamp = mrpt::system::now();
+
+			obs->set(IMU_X_ACC, -9.8f * ax);
+			obs->set(IMU_Y_ACC, -9.8f * ay);
+			obs->set(IMU_Z_ACC, -9.8f * az);
+
+			obs->set(IMU_WZ, wz);
+			obs->set(IMU_WY, wy);
+			obs->set(IMU_WX, wx);
+
+			obs->set(IMU_MAG_X, mx);
+			obs->set(IMU_MAG_Y, my);
+			obs->set(IMU_MAG_Z, mz);
+
+			// do not send out the obs yet, we want to also add the ypr angles
+			// in the next frame:
+		}
+		else
+		{
+			// discard byte:
+			buf.pop();
+		}
+
+		if (buf.peek(0) == 0xAA && buf.peek(1) == 0x55 && buf.peek(2) == 0x14)
+		{
+			std::array<uint8_t, 25> frame;
+			buf.pop_many(frame.data(), frame.size());
+
+#if 0
+			const uint32_t hwTimestamp = (frame[7 + 3] << 24) |	 //
+				(frame[7 + 2] << 16) |	//
+				(frame[7 + 1] << 8) |  //
+				(frame[7 + 0] << 0);
+#endif
+
+			float data[3] = {0, 0, 0};
+			for (int i = 0; i < 3; i++)
+			{
+				// Fixed format: little endian for this sensor.
+				const uint32_t d =	//
+					(frame[11 + 4 * i + 3] << 24) |	 //
+					(frame[11 + 4 * i + 2] << 16) |	 //
+					(frame[11 + 4 * i + 1] << 8) |	//
+					(frame[11 + 4 * i + 0] << 0);
+				data[i] = bit_cast<float>(d);
+			}
+
+			if (!obs)
+			{
+				obs = std::make_shared<CObservationIMU>();
+				obs->sensorPose = m_sensorPose;
+				obs->sensorLabel = m_sensorLabel;
+			}
+
+			const float roll = mrpt::DEG2RAD(data[0]),
+						pitch = mrpt::DEG2RAD(-data[1]),
+						yaw = mrpt::DEG2RAD(-data[2]);
+
+			// send out:
+			obs->timestamp = mrpt::system::now();
+
+			mrpt::math::CQuaternionDouble q;
+			mrpt::poses::CPose3D::FromYawPitchRoll(yaw, pitch, roll)
+				.getAsQuaternion(q);
+
+			obs->set(IMU_ORI_QUAT_W, q.r());
+			obs->set(IMU_ORI_QUAT_X, q.x());
+			obs->set(IMU_ORI_QUAT_Y, q.y());
+			obs->set(IMU_ORI_QUAT_Z, q.z());
+
+			out.push_back(obs);
+			obs.reset();
+		}
+		else
+		{
+			// discard byte:
+			buf.pop();
+		}
+	}
+
+	return out;
 }
