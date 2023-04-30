bd4227caf3c4f72f1b873e927b9d9b86b2932c2a
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-04-30 23:58:42 +0200

    progress generating pydoc

--- python-examples/lines-3d-geometry-example.py
+++ python-examples/lines-3d-geometry-example.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
 # ---------------------------------------------------------------------
-# Install python3-mrpt-*, or test with a local build with:
+# Install python3-mrpt, or test with a local build with:
 # export PYTHONPATH=$HOME/code/mrpt/build-Release/:$PYTHONPATH
 # ---------------------------------------------------------------------
 
-from mrpt_math.pymrpt_math import mrpt as mrpt_math
+from mrpt import pymrpt as m
 
 # Aliases:
-TPoint3D = mrpt_math.math.TPoint3D_double_t
-TLine3D = mrpt_math.math.TLine3D
+TPoint3D = m.mrpt.math.TPoint3D_double_t
+TLine3D = m.mrpt.math.TLine3D
 
-if __name__ == "__main__":
-    l1 = TLine3D.FromTwoPoints(TPoint3D(0, 0, 0), TPoint3D(1, 0, 0))
-    l2 = TLine3D.FromTwoPoints(TPoint3D(1, 1, 1), TPoint3D(2, 2, 0))
+l1 = TLine3D.FromTwoPoints(TPoint3D(0, 0, 0), TPoint3D(1, 0, 0))
+l2 = TLine3D.FromTwoPoints(TPoint3D(1, 1, 1), TPoint3D(2, 2, 0))
 
-    print('l1             : ' + str(l1))
-    print('l2             : ' + str(l2))
-    print('dist(l1,l2)    : ' + str(mrpt_math.math.distance(l1, l2)))
+print('l1             : ' + str(l1))
+print('l2             : ' + str(l2))
+print('dist(l1,l2)    : ' + str(m.mrpt.math.distance(l1, l2)))
--- python-examples/se2-poses-example.py
+++ python-examples/se2-poses-example.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 
 # ---------------------------------------------------------------------
-# Install python3-mrpt-*, or test with a local build with:
+# Install python3-mrpt, or test with a local build with:
 # export PYTHONPATH=$HOME/code/mrpt/build-Release/:$PYTHONPATH
 # ---------------------------------------------------------------------
 
-from mrpt import pymrpt as pm
+from mrpt import pymrpt as m
 from math import radians
 
-if __name__ == "__main__":
-    p1 = pm.mrpt.poses.CPose2D(1.0, 2.0, radians(90.0))
-    p2 = pm.mrpt.poses.CPose2D(3.0, 0.0, radians(0.0))
+p1 = m.mrpt.poses.CPose2D(1.0, 2.0, radians(90.0))
+p2 = m.mrpt.poses.CPose2D(3.0, 0.0, radians(0.0))
 
-    p3 = p1 + p2
-    p4 = p3 - p1
+p3 = p1 + p2
+p4 = p3 - p1
 
-    print('p1             : ' + str(p1))
-    print('p2             : ' + str(p2))
-    print('p1(+)p2        : ' + str(p3))
-    print('(p1(+)p2)(-)p1 : ' + str(p4))
+print('p1             : ' + str(p1))
+print('p2             : ' + str(p2))
+print('p1(+)p2        : ' + str(p3))
+print('(p1(+)p2)(-)p1 : ' + str(p4))
