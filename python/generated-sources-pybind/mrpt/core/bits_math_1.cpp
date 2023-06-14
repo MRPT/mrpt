95e034636d54ce71ba76b82a4a0c49bad8311cdf
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-06-14 21:46:03 +0200

    Remove std::thread from pymrpt (led to problems with gcc13)

--- python/generated-sources-pybind/mrpt/core/bits_math_1.cpp
+++ python/generated-sources-pybind/mrpt/core/bits_math_1.cpp
@@ -15,37 +15,37 @@
 
 void bind_mrpt_core_bits_math_1(std::function< pybind11::module &(std::string const &namespace_) > &M)
 {
-	// mrpt::sign(double) file:mrpt/core/bits_math.h line:96
+	// mrpt::sign(double) file:mrpt/core/bits_math.h line:97
 	M("mrpt").def("sign", (int (*)(double)) &mrpt::sign<double>, "C++: mrpt::sign(double) --> int", pybind11::arg("x"));
 
-	// mrpt::signWithZero(double) file:mrpt/core/bits_math.h line:103
+	// mrpt::signWithZero(double) file:mrpt/core/bits_math.h line:104
 	M("mrpt").def("signWithZero", (int (*)(double)) &mrpt::signWithZero<double>, "C++: mrpt::signWithZero(double) --> int", pybind11::arg("x"));
 
-	// mrpt::keep_min(float &, const float) file:mrpt/core/bits_math.h line:141
+	// mrpt::keep_min(float &, const float) file:mrpt/core/bits_math.h line:142
 	M("mrpt").def("keep_min", (void (*)(float &, const float)) &mrpt::keep_min<float,float>, "C++: mrpt::keep_min(float &, const float) --> void", pybind11::arg("var"), pybind11::arg("test_val"));
 
-	// mrpt::keep_min(double &, const double) file:mrpt/core/bits_math.h line:141
+	// mrpt::keep_min(double &, const double) file:mrpt/core/bits_math.h line:142
 	M("mrpt").def("keep_min", (void (*)(double &, const double)) &mrpt::keep_min<double,double>, "C++: mrpt::keep_min(double &, const double) --> void", pybind11::arg("var"), pybind11::arg("test_val"));
 
-	// mrpt::keep_min(double &, const float) file:mrpt/core/bits_math.h line:141
+	// mrpt::keep_min(double &, const float) file:mrpt/core/bits_math.h line:142
 	M("mrpt").def("keep_min", (void (*)(double &, const float)) &mrpt::keep_min<double,float>, "C++: mrpt::keep_min(double &, const float) --> void", pybind11::arg("var"), pybind11::arg("test_val"));
 
-	// mrpt::keep_max(float &, const float) file:mrpt/core/bits_math.h line:148
+	// mrpt::keep_max(float &, const float) file:mrpt/core/bits_math.h line:149
 	M("mrpt").def("keep_max", (void (*)(float &, const float)) &mrpt::keep_max<float,float>, "C++: mrpt::keep_max(float &, const float) --> void", pybind11::arg("var"), pybind11::arg("test_val"));
 
-	// mrpt::keep_max(double &, const double) file:mrpt/core/bits_math.h line:148
+	// mrpt::keep_max(double &, const double) file:mrpt/core/bits_math.h line:149
 	M("mrpt").def("keep_max", (void (*)(double &, const double)) &mrpt::keep_max<double,double>, "C++: mrpt::keep_max(double &, const double) --> void", pybind11::arg("var"), pybind11::arg("test_val"));
 
-	// mrpt::keep_max(double &, const float) file:mrpt/core/bits_math.h line:148
+	// mrpt::keep_max(double &, const float) file:mrpt/core/bits_math.h line:149
 	M("mrpt").def("keep_max", (void (*)(double &, const float)) &mrpt::keep_max<double,float>, "C++: mrpt::keep_max(double &, const float) --> void", pybind11::arg("var"), pybind11::arg("test_val"));
 
-	// mrpt::d2f(const double) file:mrpt/core/bits_math.h line:185
+	// mrpt::d2f(const double) file:mrpt/core/bits_math.h line:186
 	M("mrpt").def("d2f", (float (*)(const double)) &mrpt::d2f, "shortcut for static_cast<float>(double) \n\nC++: mrpt::d2f(const double) --> float", pybind11::arg("d"));
 
-	// mrpt::f2u8(const float) file:mrpt/core/bits_math.h line:189
+	// mrpt::f2u8(const float) file:mrpt/core/bits_math.h line:190
 	M("mrpt").def("f2u8", (uint8_t (*)(const float)) &mrpt::f2u8, "converts a float [0,1] into an uint8_t [0,255] (without checking for out of\n bounds) \n\n u8tof \n\nC++: mrpt::f2u8(const float) --> uint8_t", pybind11::arg("f"));
 
-	// mrpt::u8tof(const unsigned char) file:mrpt/core/bits_math.h line:192
+	// mrpt::u8tof(const unsigned char) file:mrpt/core/bits_math.h line:193
 	M("mrpt").def("u8tof", (float (*)(const unsigned char)) &mrpt::u8tof, "converts a uint8_t [0,255] into a float [0,1] \n f2u8 \n\nC++: mrpt::u8tof(const unsigned char) --> float", pybind11::arg("v"));
 
 }
--- python/generated-sources-pybind/mrpt/system/filesystem.cpp
+++ python/generated-sources-pybind/mrpt/system/filesystem.cpp
@@ -24,79 +24,79 @@
 
 void bind_mrpt_system_filesystem(std::function< pybind11::module &(std::string const &namespace_) > &M)
 {
-	// mrpt::system::getTempFileName() file:mrpt/system/filesystem.h line:35
+	// mrpt::system::getTempFileName() file:mrpt/system/filesystem.h line:36
 	M("mrpt::system").def("getTempFileName", (std::string (*)()) &mrpt::system::getTempFileName, "Returns the name of a proposed temporary file name \n\nC++: mrpt::system::getTempFileName() --> std::string");
 
-	// mrpt::system::getcwd() file:mrpt/system/filesystem.h line:38
+	// mrpt::system::getcwd() file:mrpt/system/filesystem.h line:39
 	M("mrpt::system").def("getcwd", (std::string (*)()) &mrpt::system::getcwd, "Returns the current working directory  \n\nC++: mrpt::system::getcwd() --> std::string");
 
-	// mrpt::system::getShareMRPTDir() file:mrpt/system/filesystem.h line:44
+	// mrpt::system::getShareMRPTDir() file:mrpt/system/filesystem.h line:45
 	M("mrpt::system").def("getShareMRPTDir", (std::string (*)()) &mrpt::system::getShareMRPTDir, "Attempts to find the directory `[PREFIX/]share/mrpt/` and returns its\n absolute path, or empty string if not found.\n Example return paths: Linux after installing = `/usr/share/mrpt/`;\n manually-built system = `[MRPT_SOURCE_DIR]/share/mrpt/`, etc.  \n\nC++: mrpt::system::getShareMRPTDir() --> std::string");
 
-	// mrpt::system::createDirectory(const std::string &) file:mrpt/system/filesystem.h line:50
+	// mrpt::system::createDirectory(const std::string &) file:mrpt/system/filesystem.h line:51
 	M("mrpt::system").def("createDirectory", (bool (*)(const std::string &)) &mrpt::system::createDirectory, "Creates a directory\n \n\n Returns false on any error, true on directory created or already\n existed.\n\nC++: mrpt::system::createDirectory(const std::string &) --> bool", pybind11::arg("dirName"));
 
-	// mrpt::system::deleteFile(const std::string &) file:mrpt/system/filesystem.h line:56
+	// mrpt::system::deleteFile(const std::string &) file:mrpt/system/filesystem.h line:57
 	M("mrpt::system").def("deleteFile", (bool (*)(const std::string &)) &mrpt::system::deleteFile, "Deletes a single file. For multiple files see deleteFiles\n \n\n Returns false on any error, true on everything OK.\n \n\n deleteFiles\n\nC++: mrpt::system::deleteFile(const std::string &) --> bool", pybind11::arg("fileName"));
 
-	// mrpt::system::deleteFiles(const std::string &) file:mrpt/system/filesystem.h line:64
+	// mrpt::system::deleteFiles(const std::string &) file:mrpt/system/filesystem.h line:65
 	M("mrpt::system").def("deleteFiles", (void (*)(const std::string &)) &mrpt::system::deleteFiles, "Delete one or more files, especified by the (optional) path and the file\n name (including '?' or '*') - Use forward slash ('/') for directories for\n compatibility between Windows and Linux, since they will be internally\n traslated into backward slashes ('\\') if MRPT is compiled under Windows.\n \n\n deleteFile\n\nC++: mrpt::system::deleteFiles(const std::string &) --> void", pybind11::arg("s"));
 
-	// mrpt::system::renameFile(const std::string &, const std::string &, std::string *) file:mrpt/system/filesystem.h line:71
+	// mrpt::system::renameFile(const std::string &, const std::string &, std::string *) file:mrpt/system/filesystem.h line:72
 	M("mrpt::system").def("renameFile", [](const std::string & a0, const std::string & a1) -> bool { return mrpt::system::renameFile(a0, a1); }, "", pybind11::arg("oldFileName"), pybind11::arg("newFileName"));
 	M("mrpt::system").def("renameFile", (bool (*)(const std::string &, const std::string &, std::string *)) &mrpt::system::renameFile, "Renames a file - If the target path is different and the filesystem allows\n it, it will be moved to the new location.\n \n\n false on any error. In that case, if a pointer to a receiver string\n is passed in error_msg, a description of the error is saved there.\n\nC++: mrpt::system::renameFile(const std::string &, const std::string &, std::string *) --> bool", pybind11::arg("oldFileName"), pybind11::arg("newFileName"), pybind11::arg("error_msg"));
 
-	// mrpt::system::deleteFilesInDirectory(const std::string &, bool) file:mrpt/system/filesystem.h line:80
+	// mrpt::system::deleteFilesInDirectory(const std::string &, bool) file:mrpt/system/filesystem.h line:81
 	M("mrpt::system").def("deleteFilesInDirectory", [](const std::string & a0) -> bool { return mrpt::system::deleteFilesInDirectory(a0); }, "", pybind11::arg("s"));
 	M("mrpt::system").def("deleteFilesInDirectory", (bool (*)(const std::string &, bool)) &mrpt::system::deleteFilesInDirectory, "Delete all the files in a given directory (nothing done if directory does\n not exists, or path is a file).\n \n\n deleteFile\n \n\n true on success\n\nC++: mrpt::system::deleteFilesInDirectory(const std::string &, bool) --> bool", pybind11::arg("s"), pybind11::arg("deleteDirectoryAsWell"));
 
-	// mrpt::system::extractFileName(const std::string &) file:mrpt/system/filesystem.h line:88
+	// mrpt::system::extractFileName(const std::string &) file:mrpt/system/filesystem.h line:89
 	M("mrpt::system").def("extractFileName", (std::string (*)(const std::string &)) &mrpt::system::extractFileName, "Extract just the name (without extension) of a filename from a complete path\n plus name plus extension.\n  This function works for either \"/\" or \"\" directory separators.\n \n\n extractFileExtension,extractFileDirectory\n\nC++: mrpt::system::extractFileName(const std::string &) --> std::string", pybind11::arg("filePath"));
 
-	// mrpt::system::extractFileExtension(const std::string &, bool) file:mrpt/system/filesystem.h line:97
+	// mrpt::system::extractFileExtension(const std::string &, bool) file:mrpt/system/filesystem.h line:98
 	M("mrpt::system").def("extractFileExtension", [](const std::string & a0) -> std::string { return mrpt::system::extractFileExtension(a0); }, "", pybind11::arg("filePath"));
 	M("mrpt::system").def("extractFileExtension", (std::string (*)(const std::string &, bool)) &mrpt::system::extractFileExtension, "Extract the extension of a filename.\n  For example, for \"dummy.cpp\", it will return \"cpp\".\n  If \"ignore_gz\" is true, the second extension will be returned if the file\n name\n   ends in \".gz\", for example, for \"foo.map.gz\", this will return \"map\".\n \n\n extractFileName,extractFileDirectory\n\nC++: mrpt::system::extractFileExtension(const std::string &, bool) --> std::string", pybind11::arg("filePath"), pybind11::arg("ignore_gz"));
 
-	// mrpt::system::extractFileDirectory(const std::string &) file:mrpt/system/filesystem.h line:105
+	// mrpt::system::extractFileDirectory(const std::string &) file:mrpt/system/filesystem.h line:106
 	M("mrpt::system").def("extractFileDirectory", (std::string (*)(const std::string &)) &mrpt::system::extractFileDirectory, "Extract the whole path (the directory) of a filename from a complete path\n plus name plus extension.\n  This function works for either \"/\" or \"\" directory separators.\n \n\n extractFileName,extractFileExtension\n\nC++: mrpt::system::extractFileDirectory(const std::string &) --> std::string", pybind11::arg("filePath"));
 
-	// mrpt::system::fileExists(const std::string &) file:mrpt/system/filesystem.h line:110
+	// mrpt::system::fileExists(const std::string &) file:mrpt/system/filesystem.h line:111
 	M("mrpt::system").def("fileExists", (bool (*)(const std::string &)) &mrpt::system::fileExists, "Test if a given file (or directory) exists.\n \n\n directoryExists\n\nC++: mrpt::system::fileExists(const std::string &) --> bool", pybind11::arg("fileName"));
 
-	// mrpt::system::directoryExists(const std::string &) file:mrpt/system/filesystem.h line:116
+	// mrpt::system::directoryExists(const std::string &) file:mrpt/system/filesystem.h line:117
 	M("mrpt::system").def("directoryExists", (bool (*)(const std::string &)) &mrpt::system::directoryExists, "Test if a given directory exists (it fails if the given path refers to an\n existing file).\n \n\n fileExists\n\nC++: mrpt::system::directoryExists(const std::string &) --> bool", pybind11::arg("fileName"));
 
-	// mrpt::system::fileNameStripInvalidChars(const std::string &, const char) file:mrpt/system/filesystem.h line:122
+	// mrpt::system::fileNameStripInvalidChars(const std::string &, const char) file:mrpt/system/filesystem.h line:123
 	M("mrpt::system").def("fileNameStripInvalidChars", [](const std::string & a0) -> std::string { return mrpt::system::fileNameStripInvalidChars(a0); }, "", pybind11::arg("filename"));
 	M("mrpt::system").def("fileNameStripInvalidChars", (std::string (*)(const std::string &, const char)) &mrpt::system::fileNameStripInvalidChars, "Replace invalid filename chars by underscores ('_') or any other user-given\n char.\n  Invalid chars are: '<','>',':','\"','/','\\','|','?','*'\n\nC++: mrpt::system::fileNameStripInvalidChars(const std::string &, const char) --> std::string", pybind11::arg("filename"), pybind11::arg("replacement_to_invalid_chars"));
 
-	// mrpt::system::fileNameChangeExtension(const std::string &, const std::string &) file:mrpt/system/filesystem.h line:131
+	// mrpt::system::fileNameChangeExtension(const std::string &, const std::string &) file:mrpt/system/filesystem.h line:132
 	M("mrpt::system").def("fileNameChangeExtension", (std::string (*)(const std::string &, const std::string &)) &mrpt::system::fileNameChangeExtension, "Replace the filename extension by another one.\n  Example:\n   \n\n\n\n \n\nC++: mrpt::system::fileNameChangeExtension(const std::string &, const std::string &) --> std::string", pybind11::arg("filename"), pybind11::arg("newExtension"));
 
-	// mrpt::system::getFileSize(const std::string &) file:mrpt/system/filesystem.h line:136
+	// mrpt::system::getFileSize(const std::string &) file:mrpt/system/filesystem.h line:137
 	M("mrpt::system").def("getFileSize", (uint64_t (*)(const std::string &)) &mrpt::system::getFileSize, "Return the size of the given file, or size_t(-1) if some error is found\n accessing that file.  \n\nC++: mrpt::system::getFileSize(const std::string &) --> uint64_t", pybind11::arg("fileName"));
 
-	// mrpt::system::getFileModificationTime(const std::string &) file:mrpt/system/filesystem.h line:140
+	// mrpt::system::getFileModificationTime(const std::string &) file:mrpt/system/filesystem.h line:141
 	M("mrpt::system").def("getFileModificationTime", (long (*)(const std::string &)) &mrpt::system::getFileModificationTime, "Return the time of the file last modification, or \"0\" if the file doesn't\n exist.  \n\nC++: mrpt::system::getFileModificationTime(const std::string &) --> long", pybind11::arg("filename"));
 
-	// mrpt::system::filePathSeparatorsToNative(const std::string &) file:mrpt/system/filesystem.h line:143
+	// mrpt::system::filePathSeparatorsToNative(const std::string &) file:mrpt/system/filesystem.h line:144
 	M("mrpt::system").def("filePathSeparatorsToNative", (std::string (*)(const std::string &)) &mrpt::system::filePathSeparatorsToNative, "Windows: replace all '/'->'\\' , in Linux/MacOS: replace all '\\'->'/' \n\nC++: mrpt::system::filePathSeparatorsToNative(const std::string &) --> std::string", pybind11::arg("filePath"));
 
-	// mrpt::system::copyFile(const std::string &, const std::string &, std::string *) file:mrpt/system/filesystem.h line:153
+	// mrpt::system::copyFile(const std::string &, const std::string &, std::string *) file:mrpt/system/filesystem.h line:154
 	M("mrpt::system").def("copyFile", [](const std::string & a0, const std::string & a1) -> bool { return mrpt::system::copyFile(a0, a1); }, "", pybind11::arg("sourceFile"), pybind11::arg("targetFile"));
 	M("mrpt::system").def("copyFile", (bool (*)(const std::string &, const std::string &, std::string *)) &mrpt::system::copyFile, "Copies file  to  If the target file exists, it\n will be overwritten.\n\n \n true on success, false on any error, whose description can be\n optionally get in outErrStr\n\n \n (In MRPT 2.5.0, the copyAttributes param was removed)\n\nC++: mrpt::system::copyFile(const std::string &, const std::string &, std::string *) --> bool", pybind11::arg("sourceFile"), pybind11::arg("targetFile"), pybind11::arg("outErrStr"));
 
-	// mrpt::system::toAbsolutePath(const std::string &, bool) file:mrpt/system/filesystem.h line:171
+	// mrpt::system::toAbsolutePath(const std::string &, bool) file:mrpt/system/filesystem.h line:172
 	M("mrpt::system").def("toAbsolutePath", [](const std::string & a0) -> std::string { return mrpt::system::toAbsolutePath(a0); }, "", pybind11::arg("path"));
 	M("mrpt::system").def("toAbsolutePath", (std::string (*)(const std::string &, bool)) &mrpt::system::toAbsolutePath, "Portable version of std::filesystem::absolute() and canonical()\n\n If `canonical==true` relative paths, symlinks, etc. will be resolved too,\n but an exception will be thrown if the referenced file/path does not exist.\n If `canonical==true`, an absolute path will be always returned, even if does\n not actually exist.\n\n  \n\n\n\n  \n (New in MRPT 2.5.0)\n\nC++: mrpt::system::toAbsolutePath(const std::string &, bool) --> std::string", pybind11::arg("path"), pybind11::arg("resolveToCanonical"));
 
-	// mrpt::system::pathJoin(const class std::vector<std::string, class std::allocator<std::string > > &) file:mrpt/system/filesystem.h line:184
+	// mrpt::system::pathJoin(const class std::vector<std::string, class std::allocator<std::string > > &) file:mrpt/system/filesystem.h line:185
 	M("mrpt::system").def("pathJoin", (std::string (*)(const class std::vector<std::string, class std::allocator<std::string > > &)) &mrpt::system::pathJoin, "Portable version of std::filesystem::path::append(), with Python-like name.\n\n  \n\n\n\n\n  \n (New in MRPT 2.5.0)\n\nC++: mrpt::system::pathJoin(const class std::vector<std::string, class std::allocator<std::string > > &) --> std::string", pybind11::arg("paths"));
 
-	// mrpt::system::md5(const std::string &) file:mrpt/system/md5.h line:23
+	// mrpt::system::md5(const std::string &) file:mrpt/system/md5.h line:24
 	M("mrpt::system").def("md5", (std::string (*)(const std::string &)) &mrpt::system::md5, "Header: `#include <mrpt/system/md5.h>`.\n Library: \n  \n\n\n @{ \n\n Computes the md5 of a block of data. \n\nC++: mrpt::system::md5(const std::string &) --> std::string", pybind11::arg("str"));
 
-	// mrpt::system::md5(const unsigned char *, size_t) file:mrpt/system/md5.h line:27
+	// mrpt::system::md5(const unsigned char *, size_t) file:mrpt/system/md5.h line:28
 	M("mrpt::system").def("md5", (std::string (*)(const unsigned char *, size_t)) &mrpt::system::md5, "Computes the md5 of a block of data. \n\nC++: mrpt::system::md5(const unsigned char *, size_t) --> std::string", pybind11::arg("data"), pybind11::arg("len"));
 
 	// mrpt::system::getMemoryUsage() file:mrpt/system/memory.h line:25
@@ -135,7 +135,7 @@ void bind_mrpt_system_filesystem(std::function< pybind11::module &(std::string c
 	// mrpt::system::changeCurrentProcessPriority(enum mrpt::system::TProcessPriority) file:mrpt/system/scheduler.h line:66
 	M("mrpt::system").def("changeCurrentProcessPriority", (void (*)(enum mrpt::system::TProcessPriority)) &mrpt::system::changeCurrentProcessPriority, "Change the priority of the given process (it applies to all the threads,\n  plus independent modifiers for each thread).\n  - Windows: See\n  [SetPriorityClass](https://msdn.microsoft.com/es-es/library/windows/desktop/ms686219(v=vs.85).aspx)\n  - Linux (pthreads): Requires `root` permissions to increase process\n  priority! Internally it calls [nice()](http://linux.die.net/man/3/nice), so it\n  has no effect if\n  () was called and a SCHED_RR is already active.\n \n\n createThread, changeThreadPriority\n\nC++: mrpt::system::changeCurrentProcessPriority(enum mrpt::system::TProcessPriority) --> void", pybind11::arg("priority"));
 
-	// mrpt::system::thread_name(const std::string &, class std::thread &) file:mrpt/system/thread_name.h line:20
-	M("mrpt::system").def("thread_name", (void (*)(const std::string &, class std::thread &)) &mrpt::system::thread_name, "Sets the name of the given thread; useful for debuggers.\n \n\n\n \n New in MRPT 2.0.4\n\nC++: mrpt::system::thread_name(const std::string &, class std::thread &) --> void", pybind11::arg("name"), pybind11::arg("theThread"));
+	// mrpt::system::thread_name(const std::string &) file:mrpt/system/thread_name.h line:26
+	M("mrpt::system").def("thread_name", (void (*)(const std::string &)) &mrpt::system::thread_name, "Sets the name of the current thread; useful for debuggers.\n \n\n\n \n New in MRPT 2.0.4\n\nC++: mrpt::system::thread_name(const std::string &) --> void", pybind11::arg("name"));
 
 }
