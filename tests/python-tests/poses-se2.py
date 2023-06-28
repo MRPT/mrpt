d45e0563e2566bac9764e0afc1a3a1d9c65faab4
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-06-29 01:49:04 +0200

    small test addition

--- tests/python-tests/poses-se2.py
+++ tests/python-tests/poses-se2.py
@@ -2,6 +2,7 @@
 
 from math import radians
 from mrpt.pymrpt.mrpt.poses import CPose2D
+from mrpt.pymrpt.mrpt.math import TPose2D
 import os
 
 print('[Running python test ' + os.path.basename(__file__) + ']')
@@ -14,5 +15,10 @@ p4 = p3 - p1
 
 assert ((p4-p2).norm() < 1e-4)
 assert (abs(p4.phi()-p2.phi()) < 1e-4)
+
+p4t: TPose2D = p4.asTPose()
+assert (abs(p4t.x - p4.x()) < 1e-8)
+assert (abs(p4t.y - p4.y()) < 1e-8)
+assert (abs(p4t.phi - p4.phi()) < 1e-8)
 
 print('[Successfully run ' + os.path.basename(__file__) + ']')
