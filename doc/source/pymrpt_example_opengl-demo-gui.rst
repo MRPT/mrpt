b095eafb460ab4ee36fd0e7d6e067150b1c20f37
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-07-03 12:37:14 +0200

    Update opengl python example

---
+++ doc/source/pymrpt_example_opengl-demo-gui.rst
@@ -0,0 +1,18 @@
+.. _pyexample_opengl-demo-gui:
+
+====================================================
+Python example: opengl-demo-gui.py
+====================================================
+
+This example illustrates how to create a 3D GUI in MRPT using the Python API
+and populate it with different objects, including animating them across the scene.
+
+.. raw:: html
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/qaaP9BmZYmo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+
+
+.. literalinclude:: ../../python-examples/opengl-demo-gui.py
+   :language: python
+   :linenos:
--- python-examples/opengl-demo-gui.py
+++ python-examples/opengl-demo-gui.py
@@ -7,6 +7,7 @@
 
 from mrpt.pymrpt import mrpt
 import time
+import math
 
 # Create GUI:
 win = mrpt.gui.CDisplayWindow3D('MRPT GUI demo', 800, 600)
@@ -16,10 +17,12 @@ win = mrpt.gui.CDisplayWindow3D('MRPT GUI demo', 800, 600)
 # data race between the main and the rendering threads.
 scene = win.get3DSceneAndLock()
 
+# A grid on the XY horizontal plane:
 # ctor args: xMin: float, xMax: float, yMin: float, yMax: float, z: float, frequency: float
-glGrid = mrpt.opengl.CGridPlaneXY.Create(-10, 10, -10, 10, 0, 1)
+glGrid = mrpt.opengl.CGridPlaneXY.Create(-3, 3, -3, 3, 0, 1)
 scene.insert(glGrid)
 
+# A couple of XYZ "corners":
 glCorner = mrpt.opengl.stock_objects.CornerXYZ(2.0)
 scene.insert(glCorner)
 
@@ -27,6 +30,7 @@ glCorner2: mrpt.opengl.CSetOfObjects = mrpt.opengl.stock_objects.CornerXYZ(1.0)
 glCorner2.setLocation(4.0, 0.0, 0.0)
 scene.insert(glCorner2)
 
+# A 3D inverse-depth ellipsoid:
 glEllip = mrpt.opengl.CEllipsoidInverseDepth3D()
 cov = mrpt.math.CMatrixFixed_double_3UL_3UL_t.Zero()
 cov[0, 0] = 0.01
@@ -39,14 +43,40 @@ mean[2, 0] = -0.6  # pitch
 glEllip.setCovMatrixAndMean(cov, mean)
 scene.insert(glEllip)
 
+# A floor "block":
+glFloor = mrpt.opengl.CBox()
+glFloor.setBoxCorners(mrpt.math.TPoint3D_double_t(-15, -15, 0),
+                      mrpt.math.TPoint3D_double_t(15, 15, 0.1))
+glFloor.setLocation(0, 0, -3.0)
+glFloor.setColor_u8(mrpt.img.TColor(0x70, 0x70, 0x70))
+scene.insert(glFloor)
+
+# A mobile box to illustrate animations:
+glBox = mrpt.opengl.CBox()
+glBox.setBoxCorners(mrpt.math.TPoint3D_double_t(0, 0, 0),
+                    mrpt.math.TPoint3D_double_t(1, 1, 1))
+glBox.setBoxBorderColor(mrpt.img.TColor(0, 0, 0))
+glBox.castShadows(True)
+scene.insert(glBox)
+
+# Shadows are disabled by default, enable them:
+scene.getViewport().enableShadowCasting(True)
+print('Shadow casting: ' + str(scene.getViewport().isShadowCastingEnabled()))
+
+# Move camera:
+win.setCameraAzimuthDeg(-40.0)
+win.setCameraElevationDeg(30.0)
 
 # end of scene lock:
 win.unlockAccess3DScene()
 
 
 print('Close the window to quit the program')
+timer = mrpt.system.CTicTac()
 
 while win.isOpen():
+    y = 5.0*math.sin(1.0*timer.Tac())
+    glBox.setLocation(4.0, y, 0.0)
 
     win.repaint()
     time.sleep(50e-3)
