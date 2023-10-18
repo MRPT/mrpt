58515afaaa6110c81ddefed9b890e3218b1858f2
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-10-18 10:22:48 +0200

    Update python wrapper

--- python/all_mrpt_rtti.cpp
+++ python/all_mrpt_rtti.cpp
@@ -2,4 +2,5 @@
 #include "src/mrpt/rtti/CObject_1.cpp"
 #include "src/mrpt/rtti/CObject_2.cpp"
 #include "src/mrpt/rtti/CObject_3.cpp"
+#include "src/mrpt/rtti/CObject_4.cpp"
 #include "src/mrpt/rtti/CObject.cpp"
