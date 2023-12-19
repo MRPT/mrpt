05292fedb05cf672b407b0e35a8fb8326bdd4064
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-12-19 23:36:25 +0100

    Add override methods

--- libs/maps/include/mrpt/maps/CColouredPointsMap.h
+++ libs/maps/include/mrpt/maps/CColouredPointsMap.h
@@ -37,7 +37,7 @@ class CColouredPointsMap : public CPointsMap
 	CColouredPointsMap(const CPointsMap& o) { CPointsMap::operator=(o); }
 	CColouredPointsMap(const CColouredPointsMap& o) : CPointsMap()
 	{
-		impl_copyFrom(o);
+		CColouredPointsMap::impl_copyFrom(o);
 	}
 	CColouredPointsMap& operator=(const CPointsMap& o)
 	{
@@ -226,6 +226,20 @@ class CColouredPointsMap : public CPointsMap
 	/** Reset the minimum-observed-distance buffer for all the points to a
 	 * predefined value */
 	void resetPointsMinDist(float defValue = 2000.0f);
+
+	// clang-format off
+	auto getPointsBufferRef_color_R() const    -> const mrpt::aligned_std_vector<float>* override { return &m_color_R;}
+	auto getPointsBufferRef_color_G() const    -> const mrpt::aligned_std_vector<float>* override { return &m_color_G; }
+	auto getPointsBufferRef_color_B() const    -> const mrpt::aligned_std_vector<float>* override { return &m_color_B; }
+
+	auto getPointsBufferRef_color_R()          -> mrpt::aligned_std_vector<float>* override { return &m_color_R; }
+	auto getPointsBufferRef_color_G()          -> mrpt::aligned_std_vector<float>* override { return &m_color_G; }
+	auto getPointsBufferRef_color_B()          -> mrpt::aligned_std_vector<float>* override { return &m_color_B; }
+
+	void insertPointField_color_R(float v) override { m_color_R.push_back(v); }
+	void insertPointField_color_G(float v) override { m_color_G.push_back(v); }
+	void insertPointField_color_B(float v) override { m_color_B.push_back(v); }
+	/// clang-format on
 
 	/** @name PCL library support
 		@{ */
