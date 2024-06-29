63243c3c7aa88a02033642c4d7c087a8fc430849
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-06-29 18:58:38 +0200

    also avoid problematic test in all PPC archs

--- libs/img/src/CImage_unittest.cpp
+++ libs/img/src/CImage_unittest.cpp
@@ -476,7 +476,8 @@ TEST(CImage, Serialize)
 }
 
 // This seems to fail now as of Jun 2024, don't have bandwith to debug it (!)
-#if !defined(__APPLE__) && !defined(__aarch64__) && !defined(__ppc64__) && !defined(__s390x__)
+#if !defined(__APPLE__) && !defined(__aarch64__) && !defined(__ppc64__) && !defined(__s390x__) && \
+    !defined(__powerpc) && !defined(__powerpc__) && !defined(__powerpc64__)
 TEST(CImage, KLT_response)
 {
   using namespace mrpt::img;
