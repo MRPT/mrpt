8f74a0b46fc6c26c8250ca3b22fbb0428c1877f3
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-05-02 18:54:46 +0200

    port mrpt_config module

--- modules/mrpt_config/src/CConfigFileBase.cpp
+++ modules/mrpt_config/src/CConfigFileBase.cpp
@@ -7,8 +7,6 @@
    | Released under BSD License. See: https://www.mrpt.org/License          |
    +------------------------------------------------------------------------+ */
 
-#include "config-precomp.h"  // Precompiled headers
-//
 #include <mrpt/config/CConfigFileBase.h>
 #include <mrpt/containers/yaml.h>
 #include <mrpt/core/exceptions.h>
