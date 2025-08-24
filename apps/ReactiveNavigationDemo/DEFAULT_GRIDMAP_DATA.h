4c7fe2f814e9189437c257e25f54874ffa591bb5
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-08-24 13:17:25 +0200

    Modernize copyright notice

--- apps/ReactiveNavigationDemo/DEFAULT_GRIDMAP_DATA.h
+++ apps/ReactiveNavigationDemo/DEFAULT_GRIDMAP_DATA.h
@@ -1,11 +1,16 @@
-/* +------------------------------------------------------------------------+
-   |                     Mobile Robot Programming Toolkit (MRPT)            |
-   |                          https://www.mrpt.org/                         |
-   |                                                                        |
-   | Copyright (c) 2005-2025, Individual contributors, see AUTHORS file     |
-   | See: https://www.mrpt.org/Authors - All rights reserved.               |
-   | Released under BSD License. See: https://www.mrpt.org/License          |
-   +------------------------------------------------------------------------+ */
+/*                    _
+                     | |    Mobile Robot Programming Toolkit (MRPT)
+ _ __ ___  _ __ _ __ | |_
+| '_ ` _ \| '__| '_ \| __|          https://www.mrpt.org/
+| | | | | | |  | |_) | |_
+|_| |_| |_|_|  | .__/ \__|     https://github.com/MRPT/mrpt/
+               | |
+               |_|
+
+ Copyright (c) 2005-2025, Individual contributors, see AUTHORS file
+ See: https://www.mrpt.org/Authors - All rights reserved.
+ SPDX-License-Identifier: BSD-3-Clause
+*/
 static const unsigned char DEFAULT_GRIDMAP_DATA[] = {
     0x13, 0x00, 0x00, 0x00, 0x43, 0x4F, 0x63, 0x63, 0x75, 0x70, 0x61, 0x6E, 0x63, 0x79, 0x47, 0x72,
     0x69, 0x64, 0x4D, 0x61, 0x70, 0x32, 0x44, 0x00, 0x00, 0x00, 0x00, 0x20, 0x03, 0x00, 0x00, 0xF4,
