2b20f1a53ceaabf68fd7d1dc5fca028c8b682db9
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-08-24 13:17:25 +0200

    Modernize copyright notice

--- modules/mrpt_serialization/include/mrpt/serialization/registerAllClasses.h
+++ modules/mrpt_serialization/include/mrpt/serialization/registerAllClasses.h
@@ -1,11 +1,16 @@
-/* +------------------------------------------------------------------------+
-   |                     Mobile Robot Programming Toolkit (MRPT)            |
-   |                          https://www.mrpt.org/                         |
-   |                                                                        |
-   | Copyright (c) 2005-2025, Individual contributors, see AUTHORS file     |
-   | See: https://www.mrpt.org/Authors - All rights reserved.               |
-   | Released under BSD License. See: https://www.mrpt.org/License          |
-   +------------------------------------------------------------------------+ */
+/*                    _
+                     | |    Mobile Robot Programming Toolkit (MRPT)
+ _ __ ___  _ __ _ __ | |_
+| '_ ` _ \| '__| '_ \| __|          https://www.mrpt.org/
+| | | | | | |  | |_) | |_
+|_| |_| |_|_|  | .__/ \__|     https://github.com/MRPT/mrpt/
+               | |
+               |_|
+
+ Copyright (c) 2005-2025, Individual contributors, see AUTHORS file
+ See: https://www.mrpt.org/Authors - All rights reserved.
+ SPDX-License-Identifier: BSD-3-Clause
+*/
 #pragma once
 
 namespace mrpt::serialization
