3c43ce55067fadae803b17561bc313e3c623467e
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-01-10 09:05:12 +0100

    update copyright year

--- doc/mrpt-zeromq-example/main_pub.cpp
+++ doc/mrpt-zeromq-example/main_pub.cpp
@@ -2,7 +2,7 @@
    |                     Mobile Robot Programming Toolkit (MRPT)               |
    |                          https://www.mrpt.org/                            |
    |                                                                           |
-   | Copyright (c) 2005-2024, Individual contributors, see AUTHORS file        |
+   | Copyright (c) 2005-2026, Individual contributors, see AUTHORS file        |
    | See: https://www.mrpt.org/Authors - All rights reserved.                  |
    | Released under BSD License. See details in https://www.mrpt.org/License   |
    +---------------------------------------------------------------------------+
@@ -19,37 +19,35 @@ using mrpt::DEG2RAD;
 
 int main()
 {
-	try
-	{
-		printf("Starting publisher...\n");
+  try
+  {
+    printf("Starting publisher...\n");
 
-		void* context = zmq_ctx_new();
-		void* pub_sock = zmq_socket(context, ZMQ_PUB);
-		int rc = zmq_bind(pub_sock, "tcp://*:5555");
-		assert(rc == 0);
+    void* context = zmq_ctx_new();
+    void* pub_sock = zmq_socket(context, ZMQ_PUB);
+    int rc = zmq_bind(pub_sock, "tcp://*:5555");
+    assert(rc == 0);
 
-		while (1)
-		{
-			mrpt::poses::CPose3D my_pose(
-				0.5f, 0.5f, 1.5f, DEG2RAD(-90.0f), DEG2RAD(0), DEG2RAD(-90.0f));
-			printf("Publishing pose...\n");
-			mrpt::comms::mrpt_send_to_zmq(pub_sock, my_pose);
-			std::this_thread::sleep_for(100ms);
+    while (1)
+    {
+      mrpt::poses::CPose3D my_pose(0.5f, 0.5f, 1.5f, DEG2RAD(-90.0f), DEG2RAD(0), DEG2RAD(-90.0f));
+      printf("Publishing pose...\n");
+      mrpt::comms::mrpt_send_to_zmq(pub_sock, my_pose);
+      std::this_thread::sleep_for(100ms);
 
-			mrpt::img::CImage my_img(800, 600, CH_RGB);
-			printf("Publishing img...\n");
-			mrpt::comms::mrpt_send_to_zmq(
-				pub_sock, my_img, 0 /* max_packet_len: 0=no max size */);
-			std::this_thread::sleep_for(100ms);
-		}
+      mrpt::img::CImage my_img(800, 600, CH_RGB);
+      printf("Publishing img...\n");
+      mrpt::comms::mrpt_send_to_zmq(pub_sock, my_img, 0 /* max_packet_len: 0=no max size */);
+      std::this_thread::sleep_for(100ms);
+    }
 
-		zmq_close(pub_sock);
-		zmq_ctx_destroy(context);
-		return 0;
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << "**Exception**: " << e.what() << std::endl;
-		return -1;
-	}
+    zmq_close(pub_sock);
+    zmq_ctx_destroy(context);
+    return 0;
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "**Exception**: " << e.what() << std::endl;
+    return -1;
+  }
 }
--- scripts/replace-header/main.cpp
+++ scripts/replace-header/main.cpp
@@ -43,105 +43,108 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	if (argc != 2)
-	{
-		printf("usage: %s <C++ source file>\n", argv[0]);
-		return 1;
-	}
+  if (argc != 2)
+  {
+    printf("usage: %s <C++ source file>\n", argv[0]);
+    return 1;
+  }
 
-	const char* in_file_name = argv[1];
+  const char* in_file_name = argv[1];
 
-	const char* out_file_name = "replace-header.tmp";
+  const char* out_file_name = "replace-header.tmp";
 
-	printf("replace-header: working on %s...", in_file_name);
+  printf("replace-header: working on %s...", in_file_name);
 
-	ifstream f(in_file_name);
-	if (f.fail())
-	{
-		cerr << "ERROR: cannot open " << in_file_name << endl;
-		return 1;
-	}
+  ifstream f(in_file_name);
+  if (f.fail())
+  {
+    cerr << "ERROR: cannot open " << in_file_name << endl;
+    return 1;
+  }
 
-	ofstream of(out_file_name);
-	if (of.fail())
-	{
-		cerr << "ERROR: cannot create " << out_file_name << endl;
-		return 1;
-	}
+  ofstream of(out_file_name);
+  if (of.fail())
+  {
+    cerr << "ERROR: cannot create " << out_file_name << endl;
+    return 1;
+  }
 
-	int nLines = 0;
-	string inLine;
-	bool copyThisLine, insertMyHeading, lookingForHeadEnd = false;
-	while (!f.eof() && !f.fail())
-	{
-		copyThisLine = true;
-		insertMyHeading = false;
+  int nLines = 0;
+  string inLine;
+  bool copyThisLine, insertMyHeading, lookingForHeadEnd = false;
+  while (!f.eof() && !f.fail())
+  {
+    copyThisLine = true;
+    insertMyHeading = false;
 
-		std::getline(f, inLine);
-		if (!f.fail())
-		{
-			// Still looking for header start?
-			if (nLines == 0)
-			{
-				// Started??
-				if (inLine.find("/*") != string::npos)
-				{
-					lookingForHeadEnd = true;
-					copyThisLine = false;
-				}
-				else
-				{
-					// Seems there is no heading: add ours:
-					insertMyHeading = true;
-				}
-			}
+    std::getline(f, inLine);
+    if (!f.fail())
+    {
+      // Still looking for header start?
+      if (nLines == 0)
+      {
+        // Started??
+        if (inLine.find("/*") != string::npos)
+        {
+          lookingForHeadEnd = true;
+          copyThisLine = false;
+        }
+        else
+        {
+          // Seems there is no heading: add ours:
+          insertMyHeading = true;
+        }
+      }
 
-			// Looking for end?
-			if (lookingForHeadEnd)
-			{
-				copyThisLine = false;
+      // Looking for end?
+      if (lookingForHeadEnd)
+      {
+        copyThisLine = false;
 
-				if (inLine.find("*/") != string::npos)
-				{
-					lookingForHeadEnd = false;
-					insertMyHeading = true;
-				}
-			}
+        if (inLine.find("*/") != string::npos)
+        {
+          lookingForHeadEnd = false;
+          insertMyHeading = true;
+        }
+      }
 
-			// Insert comment block:
-			if (insertMyHeading)
-			{
-				of << "/* "
-					  "+-------------------------------------------------------"
-					  "-----------------+\n";
-				of << "   |                Mobile Robot Programming Toolkit "
-					  "(MRPT)                 |\n";
-				of << "   |                         https://www.mrpt.org/      "
-					  "                     |\n";
-				of << "   |                                                    "
-					  "                    |\n";
-				of << "   | Copyright (c) 2005-2024, Individual contributors, "
-					  "see AUTHORS file     |\n";
-				of << "   | See: https://www.mrpt.org/Authors - All rights "
-					  "reserved.               |\n";
-				of << "   | Released under BSD License. See: "
-					  "https://www.mrpt.org/License          |\n";
-				of << "   "
-					  "+-------------------------------------------------------"
-					  "-----------------+ */\n";
-			}
+      // Insert comment block:
+      if (insertMyHeading)
+      {
+        of << "/* "
+              "+-------------------------------------------------------"
+              "-----------------+\n";
+        of << "   |                Mobile Robot Programming Toolkit "
+              "(MRPT)                 |\n";
+        of << "   |                         https://www.mrpt.org/      "
+              "                     |\n";
+        of << "   |                                                    "
+              "                    |\n";
+        of << "   | Copyright (c) 2005-2026, Individual contributors, "
+              "see AUTHORS file     |\n";
+        of << "   | See: https://www.mrpt.org/Authors - All rights "
+              "reserved.               |\n";
+        of << "   | Released under BSD License. See: "
+              "https://www.mrpt.org/License          |\n";
+        of << "   "
+              "+-------------------------------------------------------"
+              "-----------------+ */\n";
+      }
 
-			// Normal copy:
-			if (copyThisLine) { of << inLine << "\n"; }
-		}
-		nLines++;
-	}
+      // Normal copy:
+      if (copyThisLine)
+      {
+        of << inLine << "\n";
+      }
+    }
+    nLines++;
+  }
 
-	f.close();
-	of.close();
+  f.close();
+  of.close();
 
-	system((string("mv replace-header.tmp ") + string(in_file_name)).c_str());
-	printf("done (%i lines)\n", nLines);
+  system((string("mv replace-header.tmp ") + string(in_file_name)).c_str());
+  printf("done (%i lines)\n", nLines);
 
-	return 0;
+  return 0;
 }
