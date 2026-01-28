28732f384b354579073e04ed725da8fb036c2c9e
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-01-28 10:40:06 +0100

    Remove alloca(), replaced by custom StackAlloc wrapper

--- modules/mrpt_system/include/mrpt/system/memory.h
+++ modules/mrpt_system/include/mrpt/system/memory.h
@@ -13,8 +13,6 @@
 */
 #pragma once
 
-#include <mrpt/system/config.h>  // HAVE_ALLOCA
-
 #include <cstring>
 #include <type_traits>
 
@@ -28,43 +26,6 @@ namespace mrpt::system
 
 /** Returns the memory occupied by this process, in bytes */
 unsigned long getMemoryUsage();
-
-/** In platforms and compilers with support to "alloca", allocate a memory block
- * on the stack; if alloca is not supported, it is emulated as a normal "malloc"
- * - NOTICE: Since in some platforms alloca will be emulated with malloc,
- * alloca_free MUST BE ALWAYS CALLED to avoid memory leaks.
- *   This method MUST BE a macro rather than a function in order to operate on
- * the caller's stack.
- *  \sa mrpt_alloca_free
- */
-#if defined(_MSC_VER) && (_MSC_VER >= 1400)
-// Visual Studio 2005, 2008
-#define mrpt_alloca(nBytes) _malloca(nBytes)
-#elif defined(HAVE_ALLOCA)
-// GCC
-#define mrpt_alloca(nBytes) ::alloca(nBytes)
-#else
-// Default: Emulate with memory in the heap:
-#define mrpt_alloca(nBytes) ::malloc(nBytes)
-#endif
-
-/** This method must be called to "free" each memory block allocated with
- * "system::alloca": If the block was really allocated in the stack, no
- * operation is actually performed, otherwise it will be freed from the heap.
- *   This method MUST BE a macro rather than a function in order to operate on
- * the caller's stack.
- * \sa mrpt_alloca
- */
-#if defined(_MSC_VER) && (_MSC_VER >= 1400)
-// Visual Studio 2005, 2008
-#define mrpt_alloca_free(mem_block) _freea(mem_block)
-#elif defined(HAVE_ALLOCA)
-// GCC
-#define mrpt_alloca_free(mem_block)
-#else
-// Default: Emulate with memory in the heap:
-#define mrpt_alloca_free(mem_block) free(mem_block)
-#endif
 
 /** @} */
 
