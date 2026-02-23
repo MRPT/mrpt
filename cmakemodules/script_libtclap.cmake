ad33a8d24d944b66911519dfd6cb9ce5002bd169
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-02-23 01:22:39 +0100

    Remove now obsolete cmakemodules dir

--- cmakemodules/script_libtclap.cmake
+++
@@ -1,5 +0,0 @@
-# Check for libtclap, a headers-only lib
-# ===================================================
-# We need our customized version of TClap, not a system version. See sources for the modifications.
-set(CMAKE_MRPT_HAS_TCLAP 1)
-
