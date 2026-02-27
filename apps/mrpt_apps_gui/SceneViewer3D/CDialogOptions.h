15b7cc4443b317acd5ec2622362a707318c81c38
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-02-27 21:59:15 +0100

    Fix SceneViewer3D

--- apps/mrpt_apps_gui/SceneViewer3D/CDialogOptions.h
+++ apps/mrpt_apps_gui/SceneViewer3D/CDialogOptions.h
@@ -70,36 +70,36 @@ class CDialogOptions : public wxDialog
 
  protected:
   //(*Identifiers(CDialogOptions)
-  static const long ID_STATICTEXT1;
-  static const long ID_SPINCTRL1;
-  static const long ID_CHECKBOX1;
-  static const long ID_CHECKBOX2;
-  static const long ID_CHECKBOX3;
-  static const long ID_PANEL1;
-  static const long ID_PANEL2;
-  static const long ID_STATICTEXT6;
-  static const long ID_SPINCTRL2;
-  static const long ID_STATICTEXT3;
-  static const long ID_SPINCTRL3;
-  static const long ID_STATICTEXT7;
-  static const long ID_SPINCTRL4;
-  static const long ID_STATICTEXT2;
-  static const long ID_SPINCTRL6;
-  static const long ID_STATICTEXT5;
-  static const long ID_SPINCTRL8;
-  static const long ID_STATICTEXT4;
-  static const long ID_SPINCTRL5;
-  static const long ID_STATICTEXT10;
-  static const long ID_SPINCTRL10;
-  static const long ID_STATICTEXT11;
-  static const long ID_SPINCTRL9;
-  static const long ID_STATICTEXT8;
-  static const long ID_STATICTEXT9;
-  static const long ID_SPINCTRL7;
-  static const long ID_PANEL3;
-  static const long ID_NOTEBOOK1;
-  static const long ID_BUTTON1;
-  static const long ID_BUTTON2;
+  static const wxWindowID ID_STATICTEXT1;
+  static const wxWindowID ID_SPINCTRL1;
+  static const wxWindowID ID_CHECKBOX1;
+  static const wxWindowID ID_CHECKBOX2;
+  static const wxWindowID ID_CHECKBOX3;
+  static const wxWindowID ID_PANEL1;
+  static const wxWindowID ID_PANEL2;
+  static const wxWindowID ID_STATICTEXT6;
+  static const wxWindowID ID_SPINCTRL2;
+  static const wxWindowID ID_STATICTEXT3;
+  static const wxWindowID ID_SPINCTRL3;
+  static const wxWindowID ID_STATICTEXT7;
+  static const wxWindowID ID_SPINCTRL4;
+  static const wxWindowID ID_STATICTEXT2;
+  static const wxWindowID ID_SPINCTRL6;
+  static const wxWindowID ID_STATICTEXT5;
+  static const wxWindowID ID_SPINCTRL8;
+  static const wxWindowID ID_STATICTEXT4;
+  static const wxWindowID ID_SPINCTRL5;
+  static const wxWindowID ID_STATICTEXT10;
+  static const wxWindowID ID_SPINCTRL10;
+  static const wxWindowID ID_STATICTEXT11;
+  static const wxWindowID ID_SPINCTRL9;
+  static const wxWindowID ID_STATICTEXT8;
+  static const wxWindowID ID_STATICTEXT9;
+  static const wxWindowID ID_SPINCTRL7;
+  static const wxWindowID ID_PANEL3;
+  static const wxWindowID ID_NOTEBOOK1;
+  static const wxWindowID ID_BUTTON1;
+  static const wxWindowID ID_BUTTON2;
   //*)
 
  private:
