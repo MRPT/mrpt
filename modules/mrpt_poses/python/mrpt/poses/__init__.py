9c3449b53cc365275dad9cd7015be3d20cb80d9b
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-03-02 03:14:35 +0100

    progress generating pybind11

--- modules/mrpt_poses/python/mrpt/poses/__init__.py
+++ modules/mrpt_poses/python/mrpt/poses/__init__.py
@@ -12,27 +12,48 @@ CPose2D = _b.CPose2D
 # 3D Poses
 CPose3D = _b.CPose3D
 
+# Quaternion pose
+CPose3DQuat = _b.CPose3DQuat
+
+# Points
+CPoint2D = _b.CPoint2D
+CPoint3D = _b.CPoint3D
+
 # PDFs
 CPosePDF = _b.CPosePDF
 CPose3DPDF = _b.CPose3DPDF
 CPose3DPDFGaussian = _b.CPose3DPDFGaussian
 CPose3DPDFGaussianInf = _b.CPose3DPDFGaussianInf
+CPosePDFGaussian = _b.CPosePDFGaussian
+CPosePDFGaussianInf = _b.CPosePDFGaussianInf
+
+# Interpolators
+CPose2DInterpolator = _b.CPose2DInterpolator
+CPose3DInterpolator = _b.CPose3DInterpolator
+
+# Random sampling
+CPoseRandomSampler = _b.CPoseRandomSampler
 
 # Averaging
 SE_average2 = _b.SE_average2
 SE_average3 = _b.SE_average3
 
-# ----- Global Functions -----
-# (None exported explicitly in this batch, but operators act as functions)
-
 # ----- Optional __all__ -----
 __all__ = [
     "CPose2D",
     "CPose3D",
+    "CPose3DQuat",
+    "CPoint2D",
+    "CPoint3D",
     "CPosePDF",
     "CPose3DPDF",
     "CPose3DPDFGaussian",
     "CPose3DPDFGaussianInf",
+    "CPosePDFGaussian",
+    "CPosePDFGaussianInf",
+    "CPose2DInterpolator",
+    "CPose3DInterpolator",
+    "CPoseRandomSampler",
     "SE_average2",
     "SE_average3",
 ]
--- modules/mrpt_viz/python_bindings/mrpt_viz_py.cpp
+++ modules/mrpt_viz/python_bindings/mrpt_viz_py.cpp
@@ -18,14 +18,32 @@
 #include <pybind11/stl.h>
 
 // MRPT headers
+#include <mrpt/math/CMatrixFixed.h>
+#include <mrpt/math/TPoint3D.h>
+#include <mrpt/math/TSegment3D.h>
+#include <mrpt/viz/CArrow.h>
 #include <mrpt/viz/CAssimpModel.h>
+#include <mrpt/viz/CAxis.h>
+#include <mrpt/viz/CBox.h>
 #include <mrpt/viz/CCamera.h>
+#include <mrpt/viz/CCylinder.h>
+#include <mrpt/viz/CEllipsoid2D.h>
+#include <mrpt/viz/CEllipsoid3D.h>
+#include <mrpt/viz/CFrustum.h>
+#include <mrpt/viz/CGridPlaneXY.h>
+#include <mrpt/viz/CGridPlaneXZ.h>
 #include <mrpt/viz/CPointCloud.h>
 #include <mrpt/viz/CPointCloudColoured.h>
+#include <mrpt/viz/CSetOfLines.h>
 #include <mrpt/viz/CSetOfObjects.h>
+#include <mrpt/viz/CSimpleLine.h>
+#include <mrpt/viz/CSphere.h>
+#include <mrpt/viz/CText.h>
+#include <mrpt/viz/CText3D.h>
 #include <mrpt/viz/CVisualObject.h>
 #include <mrpt/viz/Scene.h>
 #include <mrpt/viz/Viewport.h>
+#include <mrpt/viz/stock_objects.h>
 
 namespace py = pybind11;
 using namespace mrpt::viz;
@@ -129,4 +147,181 @@ PYBIND11_MODULE(_bindings, m)
       .value("IgnoreMaterialColor", CAssimpModel::LoadFlags::IgnoreMaterialColor)
       .value("Verbose", CAssimpModel::LoadFlags::Verbose)
       .export_values();
+
+  // =========================================================================
+  // Phase 0.4 Extensions — additional viz classes
+  // =========================================================================
+
+  // 8. CGridPlaneXY
+  py::class_<CGridPlaneXY, CVisualObject, std::shared_ptr<CGridPlaneXY>>(m, "CGridPlaneXY")
+      .def(
+          py::init<float, float, float, float, float, float>(), py::arg("xmin") = -10.f,
+          py::arg("xmax") = 10.f, py::arg("ymin") = -10.f, py::arg("ymax") = 10.f,
+          py::arg("z") = 0.f, py::arg("frequency") = 1.f)
+      .def(
+          "setPlaneLimits", &CGridPlaneXY::setPlaneLimits, py::arg("xmin"), py::arg("xmax"),
+          py::arg("ymin"), py::arg("ymax"))
+      .def("setPlaneZcoord", &CGridPlaneXY::setPlaneZcoord)
+      .def("setGridFrequency", &CGridPlaneXY::setGridFrequency);
+
+  // 9. CGridPlaneXZ
+  py::class_<CGridPlaneXZ, CVisualObject, std::shared_ptr<CGridPlaneXZ>>(m, "CGridPlaneXZ")
+      .def(
+          py::init<float, float, float, float, float, float>(), py::arg("xmin") = -10.f,
+          py::arg("xmax") = 10.f, py::arg("zmin") = -10.f, py::arg("zmax") = 10.f,
+          py::arg("y") = 0.f, py::arg("frequency") = 1.f)
+      .def(
+          "setPlaneLimits", &CGridPlaneXZ::setPlaneLimits, py::arg("xmin"), py::arg("xmax"),
+          py::arg("zmin"), py::arg("zmax"))
+      .def("setPlaneYcoord", &CGridPlaneXZ::setPlaneYcoord)
+      .def("setGridFrequency", &CGridPlaneXZ::setGridFrequency);
+
+  // 10. CAxis
+  py::class_<CAxis, CVisualObject, std::shared_ptr<CAxis>>(m, "CAxis")
+      .def(
+          py::init<float, float, float, float, float, float, float, float, bool>(),
+          py::arg("xmin") = -1.f, py::arg("ymin") = -1.f, py::arg("zmin") = -1.f,
+          py::arg("xmax") = 1.f, py::arg("ymax") = 1.f, py::arg("zmax") = 1.f,
+          py::arg("frequency") = 1.f, py::arg("lineWidth") = 3.f, py::arg("marks") = true)
+      .def("setAxisLimits", &CAxis::setAxisLimits)
+      .def("setFrequency", &CAxis::setFrequency)
+      .def("getFrequency", &CAxis::getFrequency)
+      .def("setTextScale", &CAxis::setTextScale)
+      .def("getTextScale", &CAxis::getTextScale)
+      .def("enableTickMarks", py::overload_cast<bool>(&CAxis::enableTickMarks));
+
+  // 11. CBox
+  py::class_<CBox, CVisualObject, std::shared_ptr<CBox>>(m, "CBox")
+      .def(py::init<>())
+      .def(
+          py::init<const mrpt::math::TPoint3D&, const mrpt::math::TPoint3D&, bool, float>(),
+          py::arg("corner1"), py::arg("corner2"), py::arg("is_wireframe") = false,
+          py::arg("lineWidth") = 1.0f)
+      .def("setBoxCorners", &CBox::setBoxCorners)
+      .def(
+          "getBoxCorners",
+          [](const CBox& self)
+          {
+            mrpt::math::TPoint3D c1, c2;
+            self.getBoxCorners(c1, c2);
+            return py::make_tuple(c1, c2);
+          })
+      .def("setWireframe", &CBox::setWireframe)
+      .def("isWireframe", &CBox::isWireframe)
+      .def("enableBoxBorder", &CBox::enableBoxBorder);
+
+  // 12. CSphere
+  py::class_<CSphere, CVisualObject, std::shared_ptr<CSphere>>(m, "CSphere")
+      .def(py::init<float, int>(), py::arg("radius") = 1.0f, py::arg("nDivs") = 20)
+      .def("setRadius", &CSphere::setRadius)
+      .def("getRadius", &CSphere::getRadius)
+      .def("setNumberDivs", &CSphere::setNumberDivs);
+
+  // 13. CCylinder
+  py::class_<CCylinder, CVisualObject, std::shared_ptr<CCylinder>>(m, "CCylinder")
+      .def(py::init<>())
+      .def(
+          py::init<float, float, float, int>(), py::arg("baseRadius"), py::arg("topRadius"),
+          py::arg("height") = 1.0f, py::arg("slices") = 20)
+      .def("setRadius", &CCylinder::setRadius)
+      .def("setRadii", &CCylinder::setRadii)
+      .def("setHeight", &CCylinder::setHeight)
+      .def("getHeight", &CCylinder::getHeight);
+
+  // 14. CArrow
+  py::class_<CArrow, CVisualObject, std::shared_ptr<CArrow>>(m, "CArrow")
+      .def(py::init<>())
+      .def(
+          "setArrowEnds",
+          [](CArrow& self, float x0, float y0, float z0, float x1, float y1, float z1)
+          { self.setArrowEnds(x0, y0, z0, x1, y1, z1); },
+          py::arg("x0"), py::arg("y0"), py::arg("z0"), py::arg("x1"), py::arg("y1"), py::arg("z1"))
+      .def("setHeadRatio", &CArrow::setHeadRatio)
+      .def("setSmallRadius", &CArrow::setSmallRadius);
+
+  // 15. CText
+  py::class_<CText, CVisualObject, std::shared_ptr<CText>>(m, "CText")
+      .def(py::init<>())
+      .def(py::init<const std::string&>(), py::arg("text"))
+      .def("setString", &CText::setString)
+      .def("getString", &CText::getString)
+      .def("setFont", &CText::setFont);
+
+  // 16. CText3D
+  py::class_<CText3D, CVisualObject, std::shared_ptr<CText3D>>(m, "CText3D")
+      .def(
+          py::init<const std::string&, const std::string&, float>(), py::arg("text") = "",
+          py::arg("fontName") = "sans", py::arg("scale") = 1.0f)
+      .def("setString", &CText3D::setString)
+      .def("getString", &CText3D::getString);
+
+  // 17. CSetOfLines
+  py::class_<CSetOfLines, CVisualObject, std::shared_ptr<CSetOfLines>>(m, "CSetOfLines")
+      .def(py::init<>())
+      .def("clear", &CSetOfLines::clear)
+      .def(
+          "appendLine",
+          py::overload_cast<double, double, double, double, double, double>(
+              &CSetOfLines::appendLine),
+          py::arg("x0"), py::arg("y0"), py::arg("z0"), py::arg("x1"), py::arg("y1"), py::arg("z1"))
+      .def("appendLine", py::overload_cast<const mrpt::math::TSegment3D&>(&CSetOfLines::appendLine))
+      .def("__len__", [](const CSetOfLines& self) { return self.size(); });
+
+  // 18. CSimpleLine
+  py::class_<CSimpleLine, CVisualObject, std::shared_ptr<CSimpleLine>>(m, "CSimpleLine")
+      .def(py::init<>())
+      .def(
+          "setLineCoords",
+          py::overload_cast<float, float, float, float, float, float>(&CSimpleLine::setLineCoords),
+          py::arg("x0"), py::arg("y0"), py::arg("z0"), py::arg("x1"), py::arg("y1"), py::arg("z1"))
+      .def("getLineStart", &CSimpleLine::getLineStart)
+      .def("getLineEnd", &CSimpleLine::getLineEnd);
+
+  // 19. CEllipsoid3D
+  py::class_<CEllipsoid3D, CVisualObject, std::shared_ptr<CEllipsoid3D>>(m, "CEllipsoid3D")
+      .def(py::init<>())
+      .def(
+          "setCovMatrix", [](CEllipsoid3D& self, const mrpt::math::CMatrixDouble33& cov)
+          { self.setCovMatrix(cov); })
+      .def("setQuantiles", &CEllipsoid3D::setQuantiles)
+      .def("set3DsegmentsCount", &CEllipsoid3D::set3DsegmentsCount);
+
+  // 20. CEllipsoid2D
+  py::class_<CEllipsoid2D, CVisualObject, std::shared_ptr<CEllipsoid2D>>(m, "CEllipsoid2D")
+      .def(py::init<>())
+      .def(
+          "setCovMatrix", [](CEllipsoid2D& self, const mrpt::math::CMatrixDouble22& cov)
+          { self.setCovMatrix(cov); })
+      .def("setQuantiles", &CEllipsoid2D::setQuantiles);
+
+  // 21. CPointCloudColoured (complement to existing CPointCloud)
+  py::class_<CPointCloudColoured, CVisualObject, std::shared_ptr<CPointCloudColoured>>(
+      m, "CPointCloudColoured")
+      .def(py::init<>())
+      .def("clear", &CPointCloudColoured::clear)
+      .def(
+          "push_back",
+          [](CPointCloudColoured& self, float x, float y, float z, float r, float g, float b,
+             float a) { self.push_back(x, y, z, r, g, b, a); },
+          py::arg("x"), py::arg("y"), py::arg("z"), py::arg("r") = 1.0f, py::arg("g") = 1.0f,
+          py::arg("b") = 1.0f, py::arg("a") = 1.0f)
+      .def("size", &CPointCloudColoured::size)
+      .def("__len__", [](const CPointCloudColoured& self) { return self.size(); });
+
+  // 22. stock_objects submodule
+  auto stock = m.def_submodule("stock_objects", "Pre-built 3D objects");
+  stock.def("CornerXYZ", &mrpt::viz::stock_objects::CornerXYZ, py::arg("scale") = 1.0f);
+  stock.def(
+      "CornerXYZSimple", &mrpt::viz::stock_objects::CornerXYZSimple, py::arg("scale") = 1.0f,
+      py::arg("lineWidth") = 1.0f);
+  stock.def("CornerXYZEye", &mrpt::viz::stock_objects::CornerXYZEye);
+  stock.def(
+      "CornerXYSimple", &mrpt::viz::stock_objects::CornerXYSimple, py::arg("scale") = 1.0f,
+      py::arg("lineWidth") = 1.0f);
+  stock.def("RobotPioneer", &mrpt::viz::stock_objects::RobotPioneer);
+  stock.def("RobotRhodon", &mrpt::viz::stock_objects::RobotRhodon);
+  stock.def("RobotGiraff", &mrpt::viz::stock_objects::RobotGiraff);
+  stock.def("BumblebeeCamera", &mrpt::viz::stock_objects::BumblebeeCamera);
+  stock.def("Hokuyo_URG", &mrpt::viz::stock_objects::Hokuyo_URG);
+  stock.def("Hokuyo_UTM", &mrpt::viz::stock_objects::Hokuyo_UTM);
 }
