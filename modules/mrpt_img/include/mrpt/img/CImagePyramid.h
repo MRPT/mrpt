d08107c55c32875d6a4038dba904f1b8425c38a7
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-03-11 16:37:00 +0100

    bring back from old mrpt:vision into mrpt::img: image pyramids, rectify maps

---
+++ modules/mrpt_img/include/mrpt/img/CImagePyramid.h
@@ -0,0 +1,103 @@
+/*                    _
+                     | |    Mobile Robot Programming Toolkit (MRPT)
+ _ __ ___  _ __ _ __ | |_
+| '_ ` _ \| '__| '_ \| __|          https://www.mrpt.org/
+| | | | | | |  | |_) | |_
+|_| |_| |_|_|  | .__/ \__|     https://github.com/MRPT/mrpt/
+               | |
+               |_|
+
+ Copyright (c) 2005-2026, Individual contributors, see AUTHORS file
+ See: https://www.mrpt.org/Authors - All rights reserved.
+ SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#pragma once
+
+#include <mrpt/img/CImage.h>
+
+namespace mrpt::img
+{
+/** Holds and builds a pyramid of images: starting with an image at full
+ * resolution (octave=1), it builds
+ *  a number of half-resolution images: octave=2 at 1/2 , octave=3 at 1/2^2,
+ * octave=N at 1/2^(N-1).
+ *
+ *  Color (RGB) or grayscale pyramids can be built from color input images;
+ * only grayscale pyramids can be built from
+ *   grayscale images.
+ *
+ *  The algorithm to halve the images can be either a 1:2 decimation or a
+ * smooth filter (arithmetic mean of every 4 pixels).
+ *
+ *  Pyramids are built by invoking the method \a buildPyramid() or \a
+ * buildPyramidFast()
+ *
+ * Example of usage:
+ * \code
+ *   CImagePyramid  pyr;
+ *
+ *   CImage img = ...
+ *
+ *   pyr.buildPyramid(
+ *      img,
+ *      4,    // num. of octaves
+ *      true  // smooth
+ *      );
+ *
+ *   pyr.images[0].saveToFile("pyr0.jpg");
+ *   pyr.images[1].saveToFile("pyr1.jpg");
+ *   ...
+ * \endcode
+ *
+ *  \note Both converting to grayscale and building the octave images have
+ * SSE2-optimized implementations (if available).
+ *
+ * \sa mrpt::img::CImage
+ * \ingroup mrpt_img_grp
+ */
+class CImagePyramid
+{
+ public:
+  CImagePyramid() = default;
+
+  /** Fills the vector \a images with the different octaves built from the
+   * input image.
+   *  \param[in] img The input image. Can be either color or grayscale.
+   *  \param[in] nOctaves Number of octaves to build. 1 means just the
+   * original image, 2 means the original plus the 1/2 image, etc.
+   *  \param[in] smooth_halves If true, use an arithmetic mean of every 2x2
+   * pixel block when downsampling.
+   *  \param[in] convert_grayscale If true, the pyramid is built in grayscale
+   * even for color input images.
+   * \return true if SSE2-optimized versions of CImage::scaleHalf() was used
+   * to build **all** the scales in the pyramid.
+   * \sa buildPyramidFast
+   */
+  bool buildPyramid(
+      const mrpt::img::CImage& img,
+      size_t nOctaves,
+      bool smooth_halves = true,
+      bool convert_grayscale = false);
+
+  /**  Exactly like \a buildPyramid(), but if the input image has not to be
+   * converted from RGB to grayscale, the image data buffer is *reutilized*
+   *   for the 1st octave in \a images[0], emptying the input image.
+   * \sa buildPyramid
+   */
+  bool buildPyramidFast(
+      mrpt::img::CImage& img,
+      size_t nOctaves,
+      bool smooth_halves = true,
+      bool convert_grayscale = false);
+
+  /** The individual images:
+   *  - images[0]: 1st octave (full-size)
+   *  - images[1]: 2nd octave (1/2 size)
+   *  - images[2]: 3rd octave (1/4 size)
+   *  - ...
+   *  - images[i]: (i+1)-th octave (1/2^i size)
+   */
+  std::vector<mrpt::img::CImage> images;
+};
+}  // namespace mrpt::img
---
+++ modules/mrpt_img/include/mrpt/img/CUndistortMap.h
@@ -0,0 +1,92 @@
+/*                    _
+                     | |    Mobile Robot Programming Toolkit (MRPT)
+ _ __ ___  _ __ _ __ | |_
+| '_ ` _ \| '__| '_ \| __|          https://www.mrpt.org/
+| | | | | | |  | |_) | |_
+|_| |_| |_|_|  | .__/ \__|     https://github.com/MRPT/mrpt/
+               | |
+               |_|
+
+ Copyright (c) 2005-2026, Individual contributors, see AUTHORS file
+ See: https://www.mrpt.org/Authors - All rights reserved.
+ SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#pragma once
+
+#include <mrpt/img/CImage.h>
+#include <mrpt/img/TCamera.h>
+
+namespace mrpt::img
+{
+/** Use this class to undistort monocular images if the same distortion map is
+ * used over and over again.
+ *  Using this class is much more efficient that calling
+ * mrpt::img::CImage::rectifyImage or OpenCV's cvUndistort2(), since
+ *  the remapping data is computed only once for the camera parameters (typical
+ * times: 640x480 image -> 70% build map / 30% actual undistort).
+ *
+ *  Works with grayscale or color images.
+ *
+ * Example of usage:
+ * \code
+ *   CUndistortMap   unmap;
+ *   mrpt::img::TCamera  cam_params;
+ *
+ *   unmap.setFromCamParams( cam_params );
+ *
+ *   mrpt::img::CImage  img, img_out;
+ *
+ *   while (true) {
+ *     unmap.undistort(img, img_out);  // or:
+ *     unmap.undistort(img);  // output in place
+ *   }
+ *
+ * \endcode
+ *
+ * \sa CStereoRectifyMap, mrpt::img::TCamera, the application <a
+ * href="http://www.mrpt.org/Application:camera-calib" >camera-calib</a> for
+ * calibrating a camera.
+ * \ingroup mrpt_img_grp
+ */
+class CUndistortMap
+{
+ public:
+  /** Default ctor */
+  CUndistortMap() = default;
+
+  /** Prepares the mapping from the distortion parameters of a camera.
+   * Must be called before invoking \a undistort().
+   */
+  void setFromCamParams(const mrpt::img::TCamera& params);
+
+  /** Undistort the input image and saves the result in the output one - \a
+   * setFromCamParams() must have been set prior to calling this.
+   */
+  void undistort(const mrpt::img::CImage& in_img, mrpt::img::CImage& out_img) const;
+
+  /** Undistort the input image and saves the result in-place- \a
+   * setFromCamParams() must have been set prior to calling this.
+   */
+  void undistort(mrpt::img::CImage& in_out_img) const;
+
+  /** Returns the camera parameters which were used to generate the distortion
+   * map, as passed by the user to \a setFromCamParams */
+  inline const mrpt::img::TCamera& getCameraParams() const { return m_camera_params; }
+
+  /** Returns true if \a setFromCamParams() has been already called, false
+   * otherwise.
+   *  Can be used within loops to determine the first usage of the object and
+   * when it needs to be initialized.
+   */
+  inline bool isSet() const { return !m_dat_mapx.empty(); }
+
+ private:
+  std::vector<int16_t> m_dat_mapx;
+  std::vector<uint16_t> m_dat_mapy;
+
+  /** A copy of the data provided by the user */
+  mrpt::img::TCamera m_camera_params;
+
+};  // end class
+}  // namespace mrpt::img
